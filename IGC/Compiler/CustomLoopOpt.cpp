intel/intel-graphics-compiler

commit 1b5a29989cbe7959183d98eb4f6b01ddbaa35666

Switch to use llvm intrinsic for min/max/sqrt, this will benefit more LLVM optimization passes (e.g. LICM). Recheck-in of 70c407116b2646997a9e9c7eba1cbf5291af522f after fixing functional regressions
Change-Id: Id2879e03c6b4f15e99b7fd37edafef7ce15266de

IGC/Compiler/CustomLoopOpt.cpp
@@ -33,6 +33,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "common/LLVMUtils.h"
 
 #include "Compiler/CISACodeGen/ShaderCodeGen.hpp"
+#include "Compiler/CISACodeGen/helper.h"
 #include "Compiler/CustomLoopOpt.hpp"
 #include "Compiler/IGCPassSupport.h"
 #include "Compiler/MetaDataUtilsWrapper.h"
@@ -177,21 +178,20 @@ bool CustomLoopVersioning::detectLoop(Loop* loop,
     Instruction* i0 = body->getFirstNonPHI();
     Instruction* i1 = GetNextInstruction(i0);
 
-    GenIntrinsicInst* imax = dyn_cast<GenIntrinsicInst>(i0);
-    GenIntrinsicInst* imin = i1 ? dyn_cast<GenIntrinsicInst>(i1) : nullptr;
+    CallInst* imax = dyn_cast<CallInst>(i0);
+    CallInst* imin = i1 ? dyn_cast<CallInst>(i1) : nullptr;
 
-    if (!(imax && imax->getIntrinsicID() == GenISAIntrinsic::GenISA_max &&
-          imin && imin->getIntrinsicID() == GenISAIntrinsic::GenISA_min))
+    if (!(imax && GetOpCode(imax) == llvm_max &&
+          imin && GetOpCode(imin) == llvm_min))
     {
         return false;
     }
 
-    GenIntrinsicInst* interval_x = dyn_cast<GenIntrinsicInst>(
-        imax->getArgOperand(0), GenISAIntrinsic::GenISA_max);
-    GenIntrinsicInst* interval_y = dyn_cast<GenIntrinsicInst>(
-        imin->getArgOperand(0), GenISAIntrinsic::GenISA_min);
+    CallInst* interval_x = dyn_cast<CallInst>(imax->getArgOperand(0));
+    CallInst* interval_y = dyn_cast<CallInst>(imin->getArgOperand(0));
 
-    if (!interval_x || !interval_y)
+    if (!(interval_x && GetOpCode(interval_x) == llvm_max) ||
+        !(interval_y && GetOpCode(interval_y) == llvm_min))
     {
         return false;
     }

IGC/Compiler/CISACodeGen/CoalescingEngine.cpp
@@ -1303,8 +1303,6 @@ void CoalescingEngine::visitCallInst(llvm::CallInst &I)
         case GenISAIntrinsic::GenISA_ROUNDNE:
         case GenISAIntrinsic::GenISA_imulH:
         case GenISAIntrinsic::GenISA_umulH:
-        case GenISAIntrinsic::GenISA_min:
-        case GenISAIntrinsic::GenISA_max:
         case GenISAIntrinsic::GenISA_uaddc:
         case GenISAIntrinsic::GenISA_usubb:
         case GenISAIntrinsic::GenISA_bfrev:
@@ -1346,6 +1344,8 @@ void CoalescingEngine::visitCallInst(llvm::CallInst &I)
         case Intrinsic::floor:
         case Intrinsic::ceil:
         case Intrinsic::trunc:
+        case Intrinsic::maxnum:
+        case Intrinsic::minnum:
             break;
         case Intrinsic::exp2:
             break;

IGC/Compiler/CISACodeGen/EmitVISAPass.cpp
@@ -7183,9 +7183,6 @@ void EmitPass::EmitGenIntrinsicMessage(llvm::GenIntrinsicInst* inst)
     case GenISAIntrinsic::GenISA_PHASE_INPUT:
         emitPhaseInput(inst);
         break;
-    case GenISAIntrinsic::GenISA_sqrt:
-        emitSqrt(inst);
-        break;
     case GenISAIntrinsic::GenISA_ldrawvector_indexed:
     case GenISAIntrinsic::GenISA_ldraw_indexed:
         emitLoadRawIndexed(inst);
@@ -7306,6 +7303,11 @@ void EmitPass::EmitIntrinsicMessage(llvm::IntrinsicInst* inst)
     case Intrinsic::bswap:
         emitLLVMbswap(inst);
         break;
+
+    case Intrinsic::sqrt:
+        emitSqrt(inst);
+        break;
+
     default:
         inst->print(IGC::Debug::ods());
         assert(0 && "unknown intrinsic");

IGC/Compiler/CISACodeGen/GenIRLowering.cpp
@@ -90,10 +90,10 @@ private:
 
   Value *rearrangeAdd(Value *, Loop *) const;
 
-  bool combineFMaxFMin(GenIntrinsicInst *GII, BasicBlock::iterator &BBI) const;
+  bool combineFMaxFMin(CallInst *GII, BasicBlock::iterator &BBI) const;
   bool combineSelectInst(SelectInst *Sel, BasicBlock::iterator &BBI) const;
 
-  bool constantFoldFMaxFMin(GenIntrinsicInst *GII,
+  bool constantFoldFMaxFMin(CallInst *GII,
                             BasicBlock::iterator &BBI) const;
 };
 
@@ -198,13 +198,12 @@ template <typename Op_t, typename ConstTy> struct ClampWithConstants_match {
       : Op(OpMatch), CMin(Min), CMax(Max) {}
 
   template <typename OpTy> bool match(OpTy *V) {
-    GenIntrinsicInst *GII = dyn_cast<GenIntrinsicInst>(V);
+    CallInst *GII = dyn_cast<CallInst>(V);
     if (!GII)
       return false;
 
-    GenISAIntrinsic::ID GIID = GII->getIntrinsicID();
-    if (GIID != GenISAIntrinsic::GenISA_max &&
-        GIID != GenISAIntrinsic::GenISA_min)
+    EOPCODE GIID = GetOpCode(GII);
+    if (GIID != llvm_max && GIID != llvm_min)
       return false;
 
     Value *X = GII->getOperand(0);
@@ -216,15 +215,13 @@ template <typename Op_t, typename ConstTy> struct ClampWithConstants_match {
     if (!C0)
       return false;
 
-    GenIntrinsicInst *GII2 = dyn_cast<GenIntrinsicInst>(X);
+    CallInst *GII2 = dyn_cast<CallInst>(X);
     if (!GII2)
       return false;
 
-    GenISAIntrinsic::ID GIID2 = GII2->getIntrinsicID();
-    if (!(GIID == GenISAIntrinsic::GenISA_min &&
-          GIID2 == GenISAIntrinsic::GenISA_max) &&
-        !(GIID == GenISAIntrinsic::GenISA_max &&
-          GIID2 == GenISAIntrinsic::GenISA_min))
+    EOPCODE GIID2 = GetOpCode(GII2);
+    if (!(GIID == llvm_min && GIID2 == llvm_max) &&
+        !(GIID == llvm_max && GIID2 == llvm_min))
       return false;
 
     X = GII2->getOperand(0);
@@ -239,8 +236,8 @@ template <typename Op_t, typename ConstTy> struct ClampWithConstants_match {
     if (!Op.match(X))
       return false;
 
-    CMin = (GIID2 == GenISAIntrinsic::GenISA_min) ? C0 : C1;
-    CMax = (GIID2 == GenISAIntrinsic::GenISA_min) ? C1 : C0;
+    CMin = (GIID2 == llvm_min) ? C0 : C1;
+    CMax = (GIID2 == llvm_min) ? C1 : C0;
     return true;
   }
 };
@@ -286,10 +283,10 @@ bool GenIRLowering::runOnFunction(Function &F) {
       default: // By default, DO NOTHING
         break;
       case Instruction::Call:
-        if (GenIntrinsicInst *GII = dyn_cast<GenIntrinsicInst>(Inst)) {
-          switch (GII->getIntrinsicID()) {
-          case GenISAIntrinsic::GenISA_max:
-          case GenISAIntrinsic::GenISA_min:
+        if (CallInst *GII = dyn_cast<CallInst>(Inst)) {
+          switch (GetOpCode(GII)) {
+          case llvm_max:
+          case llvm_min:
             Changed |= combineFMaxFMin(GII, BI);
             break;
           default:
@@ -724,12 +721,11 @@ bool GenIRLowering::lowerGetElementPtrInst(GetElementPtrInst *GEP,
   return true;
 }
 
-bool GenIRLowering::constantFoldFMaxFMin(GenIntrinsicInst *GII,
+bool GenIRLowering::constantFoldFMaxFMin(CallInst *GII,
                                          BasicBlock::iterator &BBI) const {
   // Constant fold fmax/fmin only.
-  GenISAIntrinsic::ID GIID = GII->getIntrinsicID();
-  if (GIID != GenISAIntrinsic::GenISA_max &&
-      GIID != GenISAIntrinsic::GenISA_min)
+  EOPCODE GIID = GetOpCode(GII);
+  if (GIID != llvm_max && GIID != llvm_min)
     return false;
 
   // Skip fmax/fmin with non-constant operand.
@@ -742,7 +738,7 @@ bool GenIRLowering::constantFoldFMaxFMin(GenIntrinsicInst *GII,
   const APFloat &A = CFP0->getValueAPF();
   const APFloat &B = CFP1->getValueAPF();
   APFloat Result =
-      (GIID == GenISAIntrinsic::GenISA_min) ? minnum(A, B) : maxnum(A, B);
+      (GIID == llvm_min) ? minnum(A, B) : maxnum(A, B);
   Constant *C = ConstantFP::get(GII->getContext(), Result);
 
   GII->replaceAllUsesWith(C);
@@ -751,7 +747,7 @@ bool GenIRLowering::constantFoldFMaxFMin(GenIntrinsicInst *GII,
   return true;
 }
 
-bool GenIRLowering::combineFMaxFMin(GenIntrinsicInst *GII,
+bool GenIRLowering::combineFMaxFMin(CallInst *GII,
                                     BasicBlock::iterator &BBI) const {
   using namespace llvm::PatternMatch; // Scoped namespace using.
 
@@ -833,9 +829,9 @@ bool GenIRLowering::combineSelectInst(SelectInst *Sel,
       return false;
   }
 
-  GenISAIntrinsic::ID IID =
-      IsMax ? GenISAIntrinsic::GenISA_max : GenISAIntrinsic::GenISA_min;
-  Function *IFunc = GenISAIntrinsic::getDeclaration(
+  Intrinsic::ID IID =
+      IsMax ? Intrinsic::maxnum : Intrinsic::minnum;
+  Function *IFunc = Intrinsic::getDeclaration(
       Sel->getParent()->getParent()->getParent(), IID, LHS->getType());
 
   Instruction *I = Builder->CreateCall2(IFunc, LHS, RHS);

IGC/Compiler/CISACodeGen/HalfPromotion.cpp
@@ -92,7 +92,9 @@ void IGC::HalfPromotion::handleLLVMIntrinsic(llvm::IntrinsicInst &I)
         id == Intrinsic::ceil ||
         id == Intrinsic::fabs ||
         id == Intrinsic::pow ||
-        id == Intrinsic::fma)
+        id == Intrinsic::fma ||
+        id == Intrinsic::maxnum ||
+        id == Intrinsic::minnum)
     {
         Module* M = I.getParent()->getParent()->getParent();
         llvm::IGCIRBuilder<> builder(&I);
@@ -105,8 +107,15 @@ void IGC::HalfPromotion::handleLLVMIntrinsic(llvm::IntrinsicInst &I)
 
         for(unsigned i = 0; i < I.getNumArgOperands(); ++i)
         {
-            Value* op = builder.CreateFPExt(I.getOperand(i), builder.getFloatTy());
-            arguments.push_back(op);
+            if (I.getOperand(i)->getType()->isHalfTy())
+            {
+                Value* op = builder.CreateFPExt(I.getOperand(i), builder.getFloatTy());
+                arguments.push_back(op);
+            }
+            else
+            {
+                arguments.push_back(I.getOperand(i));
+            }
         }
 
         Value* f32Val = builder.CreateCall(
@@ -122,12 +131,9 @@ void IGC::HalfPromotion::handleGenIntrinsic(llvm::GenIntrinsicInst &I)
 {
     GenISAIntrinsic::ID id = I.getIntrinsicID();
     if(id == GenISAIntrinsic::GenISA_fsat ||
-        id == GenISAIntrinsic::GenISA_sqrt ||
         id == GenISAIntrinsic::GenISA_rsq ||
         id == GenISAIntrinsic::GenISA_GradientX ||
         id == GenISAIntrinsic::GenISA_GradientY ||
-        id == GenISAIntrinsic::GenISA_min ||
-        id == GenISAIntrinsic::GenISA_max ||
         id == GenISAIntrinsic::GenISA_WaveShuffleIndex)
     {
         Module* M = I.getParent()->getParent()->getParent();

IGC/Compiler/CISACodeGen/OpenCLKernelCodeGen.cpp
@@ -1228,13 +1228,9 @@ void COpenCLKernel::ParseShaderSpecificOpcode( llvm::Instruction* inst )
     case Instruction::Call:
         if(inst->getType()->isDoubleTy())
         {
-            if(GenIntrinsicInst* I = dyn_cast<GenIntrinsicInst>(inst))
+            if (GetOpCode(inst) == llvm_sqrt)
             {
-                GenISAIntrinsic::ID id = I->getIntrinsicID();
-                if(id == GenISAIntrinsic::GenISA_sqrt)
-                {
-                     SetDisableMidthreadPreemption();
-                }
+                SetDisableMidthreadPreemption();
             }
         }
         break;

IGC/Compiler/CISACodeGen/PatternMatchPass.cpp
@@ -338,13 +338,13 @@ struct ClampWithConstants_match {
 
     template<typename OpTy>
     bool match(OpTy *V) {
-        GenIntrinsicInst *GII = dyn_cast<GenIntrinsicInst>(V);
+        CallInst *GII = dyn_cast<CallInst>(V);
         if (!GII)
             return false;
 
-        GenISAIntrinsic::ID GIID = GII->getIntrinsicID();
-        if (GIID != GenISAIntrinsic::GenISA_max &&
-            GIID != GenISAIntrinsic::GenISA_min)
+        EOPCODE op = GetOpCode(GII);
+
+        if (op != llvm_max && op != llvm_min)
             return false;
 
         Value *X = GII->getOperand(0);
@@ -360,11 +360,9 @@ struct ClampWithConstants_match {
         if (!GII2)
             return false;
 
-        GenISAIntrinsic::ID GIID2 = GII2->getIntrinsicID();
-        if (!(GIID == GenISAIntrinsic::GenISA_min &&
-              GIID2 == GenISAIntrinsic::GenISA_max) &&
-            !(GIID == GenISAIntrinsic::GenISA_max &&
-              GIID2 == GenISAIntrinsic::GenISA_min))
+        EOPCODE op2 = GetOpCode(GII2);
+        if (!(op == llvm_min && op2 == llvm_max) &&
+            !(op == llvm_max && op2 == llvm_min))
             return false;
 
         X = GII2->getOperand(0);
@@ -379,8 +377,8 @@ struct ClampWithConstants_match {
         if (!Op.match(X))
             return false;
 
-        CMin = (GIID2 == GenISAIntrinsic::GenISA_min) ? C0 : C1;
-        CMax = (GIID2 == GenISAIntrinsic::GenISA_min) ? C1 : C0;
+        CMin = (op2 == llvm_min) ? C0 : C1;
+        CMax = (op2 == llvm_min) ? C1 : C0;
         return true;
     }
 };
@@ -990,11 +988,6 @@ void CodeGenPatternMatch::visitCallInst(CallInst &I)
     {
         switch(CI->getIntrinsicID())
         {
-        case GenISAIntrinsic::GenISA_min:
-        case GenISAIntrinsic::GenISA_max:
-            match = MatchSatModifier(I) ||
-                MatchModifier(I);
-            break;
         case GenISAIntrinsic::GenISA_ROUNDNE:
         case GenISAIntrinsic::GenISA_imulH:
         case GenISAIntrinsic::GenISA_umulH:
@@ -1118,6 +1111,11 @@ void CodeGenPatternMatch::visitIntrinsicInst(llvm::IntrinsicInst &I)
         break;
     case Intrinsic::fma:
         match = MatchFMA(I);
+        break;
+    case Intrinsic::maxnum:
+    case Intrinsic::minnum:
+        match = MatchSatModifier(I) ||
+            MatchModifier(I);
         break;
     default:
         match = MatchSingleInstruction(I);
@@ -2857,16 +2855,6 @@ bool CodeGenPatternMatch::MatchRsqrt(llvm::BinaryOperator& I)
                     }
                 }
             }
-            // OCL needs to emit a special sqrt because the LLVM intrinsic has undefined
-            // behavior for negative numbers.
-            else if (llvm::GenIntrinsicInst* sqrt = dyn_cast<GenIntrinsicInst>(I.getOperand(1)))
-            {
-                if (sqrt->getIntrinsicID() == GenISAIntrinsic::GenISA_sqrt)
-                {
-                    source = sqrt->getOperand(0);
-                    found = true;
-                }
-            }
         }
     }
     if(found)
@@ -3475,20 +3463,19 @@ inline bool isMinOrMax(llvm::Value* inst, llvm::Value*& source0, llvm::Value*& s
 {
     bool found = false;
     llvm::Instruction* max = llvm::dyn_cast<llvm::Instruction>(inst);
-    if(GenIntrinsicInst* intrMinMax = dyn_cast<GenIntrinsicInst>(inst))
+    if (!max)
+        return false;
+
+    EOPCODE op = GetOpCode(max);
+    if (op == llvm_min || op == llvm_max)
     {
-        GenISAIntrinsic::ID IID = intrMinMax->getIntrinsicID();
-        if(IID == GenISAIntrinsic::GenISA_max ||
-            IID == GenISAIntrinsic::GenISA_min)
-        {
-            source0 = intrMinMax->getOperand(0);
-            source1 = intrMinMax->getOperand(1);
-            isUnsigned = false;
-            isMin = (IID == GenISAIntrinsic::GenISA_min);
-            return true;
-        }
+        source0 = max->getOperand(0);
+        source1 = max->getOperand(1);
+        isUnsigned = false;
+        isMin = (op == llvm_min);
+        return true;
     }
-    else if(max && GetOpCode(max)==llvm_select)
+    else if(op == llvm_select)
     {
         if (llvm::CmpInst *cmp = llvm::dyn_cast<llvm::CmpInst>(max->getOperand(0)))
         {

IGC/Compiler/CISACodeGen/helper.cpp
@@ -623,14 +623,14 @@ void getTextureAndSamplerOperands(llvm::GenIntrinsicInst *pIntr, llvm::Value*& p
     }
 }
 
-EOPCODE GetOpCode(llvm::Instruction* inst)
+EOPCODE GetOpCode(const llvm::Instruction* inst)
 {
-    if(GenIntrinsicInst *CI = dyn_cast<GenIntrinsicInst>( inst ))
+    if(const GenIntrinsicInst *CI = dyn_cast<GenIntrinsicInst>( inst ))
     {
         unsigned ID = CI->getIntrinsicID();
         return (EOPCODE)(OPCODE(ID,e_Intrinsic));
     }
-    else if(llvm::IntrinsicInst *CI = llvm::dyn_cast<llvm::IntrinsicInst>( inst ))
+    else if(const IntrinsicInst *CI = llvm::dyn_cast<llvm::IntrinsicInst>( inst ))
     {
         unsigned ID = CI->getIntrinsicID();
         return (EOPCODE)(OPCODE(ID,e_Intrinsic));

commit 12c138854fa154a65a9b6393399e445cb0055f8b

Fix IGC SA test failure caused by 1b5a29989cbe7959183d98eb4f6b01ddbaa35666
Change-Id: I51d231ab4ca67b7a025402779d236cbc508d57a6

IGC/Compiler/CustomLoopOpt.cpp
@@ -157,8 +157,8 @@ bool CustomLoopVersioning::runOnFunction(Function& F)
 //   br i1 %cond, label %break_cont, label %after_loop
 //
 // loop_body:
-//   %206 = call float @genx.GenISA.max.f32(float %loop_range_x, float %t)
-//   %207 = call float @genx.GenISA.min.f32(float %loop_range_y, float %nextT)
+//   %206 = call float @llvm.maxnum.f32(float %loop_range_x, float %t)
+//   %207 = call float @llvm.minnum.f32(float %loop_range_y, float %nextT)
 //   ...
 //   %258 = load float, float addrspace(65538)* ...
 //   %res_s588 = fmul float %nextT, %258
@@ -308,8 +308,8 @@ void CustomLoopVersioning::rewriteLoopSeg1(Loop* loop,
     Instruction* i0 = body->getFirstNonPHI();
     Instruction* i1 = GetNextInstruction(i0);
 
-    GenIntrinsicInst* imax = cast<GenIntrinsicInst>(i0);
-    GenIntrinsicInst* imin = cast<GenIntrinsicInst>(i1);
+    IntrinsicInst* imax = cast<IntrinsicInst>(i0);
+    IntrinsicInst* imin = cast<IntrinsicInst>(i1);
     assert(imax && imin);
 
     imax->replaceAllUsesWith(interval_x);
@@ -422,13 +422,13 @@ void CustomLoopVersioning::rewriteLoopSeg2(Loop* loop,
     Instruction* i0 = body->getFirstNonPHI();
     Instruction* i1 = GetNextInstruction(i0);
 
-    GenIntrinsicInst* imax = cast<GenIntrinsicInst>(i0);
-    GenIntrinsicInst* imin = cast<GenIntrinsicInst>(i1);
+    IntrinsicInst* imax = cast<IntrinsicInst>(i0);
+    IntrinsicInst* imin = cast<IntrinsicInst>(i1);
     assert(imax && imin);
 
     // find
-    //   %206 = call float @genx.GenISA.max.f32()
-    //   %207 = call float @genx.GenISA.min.f32()
+    //   %206 = call float @llvm.maxnum.f32()
+    //   %207 = call float @llvm.minnum.f32()
     //   %209 = fdiv float 1.000000e+00, % 206
     //   %210 = fmul float %207, % 209
     Instruction* fmul = nullptr;
@@ -475,8 +475,8 @@ void CustomLoopVersioning::rewriteLoopSeg3(BasicBlock* bb,
     Instruction* i0 = bb->getFirstNonPHI();
     Instruction* i1 = GetNextInstruction(i0);
 
-    GenIntrinsicInst* imax = cast<GenIntrinsicInst>(i0);
-    GenIntrinsicInst* imin = cast<GenIntrinsicInst>(i1);
+    IntrinsicInst* imax = cast<IntrinsicInst>(i0);
+    IntrinsicInst* imin = cast<IntrinsicInst>(i1);
     assert(imax && imin);
 
     imax->replaceAllUsesWith(imax->getArgOperand(1));