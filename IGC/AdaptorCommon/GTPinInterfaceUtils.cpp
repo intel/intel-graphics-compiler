f4a94389d3a4bb076d16bd2d727efabf48e31876
xlei3 <xiao.lei@intel.com>
2018-06-07 16:57:34 -0700

    Fixed to using aligned_malloc and aligned_free for buffer allocation/free.
    Change-Id: Iaee66f9985d55e94414fc89a0df372de2c8e892f

--- IGC/AdaptorCommon/GTPinInterfaceUtils.cpp
+++ IGC/AdaptorCommon/GTPinInterfaceUtils.cpp
@@ -65,7 +65,7 @@ void* AllocGTPinDataBuffer(const IGC::SKernelProgram* program, const IGC::SProgr
         }
 
         // Allocate the buffer
-        buffer = (char*)malloc(allocSize);
+        buffer = (char*)IGC::aligned_malloc(allocSize, sizeof(QWORD));
     }
 
     // Update the gtpin return status
