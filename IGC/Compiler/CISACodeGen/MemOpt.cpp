a6fe3b0ed4c9ba76dff71ec16d087a2c3f297b72
sys_hudsonse <hudson_server@intel.com>
2018-07-02 15:47:17 -0700

    Backout of 0fe716afd995908b19d833ee192dcc82a308bf24 due to Functional Regression
    Change-Id: I52a0cd0fe7e7786c4ab6c4eb7873518cbe7d0a4c

--- IGC/Compiler/CISACodeGen/MemOpt.cpp
+++ IGC/Compiler/CISACodeGen/MemOpt.cpp
@@ -176,7 +176,7 @@ namespace {
           bool DirectIndex;
           unsigned BufID;
           BufferType BufTy = DecodeAS4GFXResource(AS, DirectIndex, BufID);
-          if (BufTy == CONSTANT_BUFFER && CGC->m_DriverInfo.UsesTypedConstantBuffers())
+          if (BufTy == CONSTANT_BUFFER)
             return true;
         }
         return false;
