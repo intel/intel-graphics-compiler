d447bc8df7f62d4f5983477da4a6bbaca6ebf1b7
Szymichowski, Pawel <pawel.szymichowski@intel.com>
2019-03-01 21:45:58 -0800

    Changes in code.
    Change-Id: Ibabe9082f171bb9e0aed7b7ce273d6c5c7acc0f9

--- IGC/common/SystemThread.cpp
+++ IGC/common/SystemThread.cpp
@@ -282,8 +282,8 @@ CGenSystemInstructionKernelProgram* CGenSystemInstructionKernelProgram::Create(
             {
                 SIPIndex = GEN11_HP_SIP_CSR;
             }
-            if ((platform.getPlatformInfo().eProductFamily == IGFX_LAKEFIELD) ||
-                (platform.getPlatformInfo().eProductFamily == IGFX_JASPERLAKE))
+            if ((platform.getPlatformInfo().eProductFamily == IGFX_LAKEFIELD)
+            )
             {
                 SIPIndex = GEN11_LKF_SIP_CSR;
             }
