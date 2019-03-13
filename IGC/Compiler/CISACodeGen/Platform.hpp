077209b1d954e02a900a7393aa5492cbfb41f269
Mochocki, Piotr <piotr.mochocki@intel.com>
2019-03-13 15:59:48 -0700

    Revert of commit 952d4d1f210cfb5573aa73c7a7a4d1119237f901
    Change-Id: I7ad203ab8dd3a8336f051b80b172aed20bfda849

--- IGC/Compiler/CISACodeGen/Platform.hpp
+++ IGC/Compiler/CISACodeGen/Platform.hpp
@@ -422,12 +422,6 @@ public:
     }
 
     const SCompilerHwCaps& GetCaps(){return m_caps;}
-
-    bool WaClampLowerCosineValue() const
-    {
-        return m_WaTable.WaPreventCosineOverShoot != 0;
-    }
-
 };
 
 }//namespace IGC
