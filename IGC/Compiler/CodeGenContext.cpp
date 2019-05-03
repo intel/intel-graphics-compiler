ad0e41291503cc6600cb517d4dd8378484d91f37
fftzeng <frances.tzeng@intel.com>
2019-05-03 03:23:42 -0700

    Allow small spills instead of recompile
    Change-Id: Ib6472b4f1bc67ff57c22a1b590245b8a657af3ed

--- IGC/Compiler/CodeGenContext.cpp
+++ IGC/Compiler/CodeGenContext.cpp
@@ -102,6 +102,7 @@ bool RetryManager::IsLastTry(CodeGenContext* cgCtx) {
     return (!enabled ||
         IGC_IS_FLAG_ENABLED(DisableRecompilation) ||
         (cgCtx->getModuleMetaData()->csInfo.forcedSIMDSize != 0) ||
+        lastSpillSize < IGC_GET_FLAG_VALUE(AllowedSpillRegCount) ||
         (stateId < getStateCnt() && RetryTable[stateId].nextState >= getStateCnt()));
 }
 unsigned RetryManager::GetRetryId() const { return stateId; }
