e461328d8a1616b21564a217385e08d0145250ce
Irene Wu <irene.w.wu@intel.com>
2019-06-04 03:45:24 -0700

    Support Pattern Match on Predicate Add
    PredAdd takes precedence over Mad pattern matching
    
    Change-Id: Ib9d7366ae0f26b943beefb1ff676847d1b35ea3f

--- IGC/Compiler/CISACodeGen/CISABuilder.cpp
+++ IGC/Compiler/CISACodeGen/CISABuilder.cpp
@@ -577,6 +577,13 @@ void CEncoder::Select(CVariable* flag, CVariable* dst, CVariable* src0, CVariabl
     }
 }
 
+void CEncoder::PredAdd(CVariable* flag, CVariable* dst, CVariable* src0, CVariable* src1)
+{
+    m_encoderState.m_flag.var = flag;
+
+    Arithmetic(ISA_ADD, dst, src0, src1);
+}
+
 void CEncoder::SetDstSubVar(uint subVar)
 {
     m_encoderState.m_dstOperand.subVar = int_cast<uint8_t>(subVar);
