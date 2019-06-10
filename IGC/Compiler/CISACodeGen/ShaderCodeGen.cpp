0d72dd62e2d7dcd0690a4bcdd4c3c35e20135678
Marcin Naczk <marcin.naczk@intel.com>
2019-06-09 23:03:59 -0700

    Revert of: Add pass which removes unnecessary operations on local memory
    Change-Id: If33cd42386d43e2cee76d9eee3719b6db8440497

--- IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
+++ IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
@@ -1376,7 +1376,6 @@ void OptimizeIR(CodeGenContext* pContext)
             }
             mpm.add(llvm::createCFGSimplificationPass());
             mpm.add(llvm::createEarlyCSEPass());
-
             if(pContext->m_instrTypes.hasNonPrimitiveAlloca)
             {
                 mpm.add(createSROAPass());
@@ -1462,9 +1461,6 @@ void OptimizeIR(CodeGenContext* pContext)
                 mpm.add(createSROAPass());
             }
         }
-#if LLVM_VERSION_MAJOR >= 7
-        mpm.add(new TrivialLocalMemoryOpsElimination());
-#endif
         mpm.add(createGenSimplificationPass());
 
         if (pContext->m_instrTypes.hasLoadStore)
