200ce44509703c826012bcc041bf55f92d9382f5
Gu, Junjie <junjie.gu@intel.com>
2019-07-12 17:29:30 -0700

    Peephole legalizer assumes i8 must be legal.
    Change-Id: I64f9475fea3e1878a8842fe11cb02b73b840e8f2

--- IGC/AdaptorOCL/UnifyIROCL.cpp
+++ IGC/AdaptorOCL/UnifyIROCL.cpp
@@ -201,13 +201,7 @@ static void CommonOCLBasedPasses(
         "-f32:32:32-f64:64:64-v16:16:16-v24:32:32"\
         "-v32:32:32-v48:64:64-v64:64:64-v96:128:128"\
         "-v128:128:128-v192:256:256-v256:256:256"\
-        "-v512:512:512-v1024:1024:1024";
-    if (pContext->platform.supportByteALUOperation()) {
-        layoutstr += "-n8:16:32";
-    }
-    else {
-        layoutstr += "-n16:32";
-    }
+        "-v512:512:512-v1024:1024:1024-n8:16:32";
 
     StringRef dataLayout = layoutstr;
     pContext->getModule()->setDataLayout(dataLayout);
@@ -216,7 +210,7 @@ static void CommonOCLBasedPasses(
     {
         BuiltinSizeModule->setDataLayout(dataLayout);
     }
-        
+
     MetaDataUtils *pMdUtils = pContext->getMetaDataUtils();
     
     //extracting OCL version major before SPIRMetadataTranslation pass deletes its metadata node
