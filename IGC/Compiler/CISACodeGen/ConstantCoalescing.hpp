83f17481ee34084ce28cfdc5d0154528e2927694
Tzeng, Frances <frances.tzeng@intel.com>
2019-09-17 12:56:00 -0700

    [Autobackout][FuncReg]Revert of change: 4cb07b1e50680f50a32649252e4270d7c310dca5 Improve CB merge for better performance
    Change-Id: Ifb339be305dbbd0e0abd69c3425f771eb49ce515

--- IGC/Compiler/CISACodeGen/ConstantCoalescing.hpp
+++ IGC/Compiler/CISACodeGen/ConstantCoalescing.hpp
@@ -58,7 +58,6 @@ namespace IGC
         uint         elementSize;       // size in bytes of the basic data element
         uint         chunkStart;        // offset of the first data element in chunk in units of elementSize
         uint         chunkSize;         // chunk size in elements
-        uint         loadOrder;
         llvm::Instruction* chunkIO;     // coalesced load
     };
 
@@ -85,10 +84,6 @@ namespace IGC
             std::vector<BufChunk*>& dircb_owlds,
             std::vector<BufChunk*>& indcb_owlds,
             std::vector<BufChunk*>& indcb_gathers);
-
-        void FindAllDirectCB(llvm::BasicBlock* blk,
-            std::vector<BufChunk*>& dircb_owloads);
-
         void ProcessFunction(llvm::Function* function);
 
         virtual bool runOnFunction(llvm::Function& func) override;
