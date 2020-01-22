739d255cefc29e0065e8a421476a8dedb991aef3
Gu, Junjie <junjie.gu@intel.com>
2020-01-22 09:26:55 -0800

    Re-enable ifcvt again after ifcvt fix has been submitted. This affects code under NoMask WA only.
    Change-Id: I17d41006e5feffc670e517926ae7924777c2bd5b

--- IGC/Compiler/CISACodeGen/CISABuilder.cpp
+++ IGC/Compiler/CISACodeGen/CISABuilder.cpp
@@ -3872,15 +3872,11 @@ namespace IGC
 
         if (IGC_IS_FLAG_ENABLED(ForceNoMaskWA)) {
             SaveOption(vISA_forceNoMaskWA, true);
-            // temporary disable ifcvt until ifcvt is fixed
-            SaveOption(vISA_ifCvt, false);
         }
         if (m_program->m_Platform->getWATable().Wa_1407528679 != 0 &&
             IGC_GET_FLAG_VALUE(NoMaskWA) > 0)
         {
             SaveOption(vISA_noMaskWA, IGC_GET_FLAG_VALUE(NoMaskWA));
-            // temporary disable ifcvt until ifcvt is fixed
-            SaveOption(vISA_ifCvt, false);
         }
 
         if (IGC_IS_FLAG_ENABLED(DisableCSEL))
