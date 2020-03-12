d52ec7acd0824b831e3b7f8b0e891f77d89fb767
Wei Chen <weiyu.chen@intel.com>
2020-03-12 05:23:33 -0700

    [Autobackout][Manual]Revert of change: 04b2cdacc208f660c61f59f97a42042e2d92f857 Refactor spill generator to reduce the number of explicit send message descriptor construction.
    Change-Id: Idc533ce3e2e8a8f2a4f52c1253af5f9d62c513ec

--- visa/Gen4_IR.cpp
+++ visa/Gen4_IR.cpp
@@ -3669,14 +3669,6 @@ void G4_INST::emit_inst(std::ostream& output, bool symbol_dst, bool *symbol_srcs
         if (isIntrinsic())
         {
             output << "." << asIntrinsicInst()->getName();
-            if (isSpillIntrinsic())
-            {
-                output << "." << asSpillIntrinsic()->getNumRows();
-            }
-            else if (isFillIntrinsic())
-            {
-                output << "." << asFillIntrinsic()->getNumRows();
-            }
         }
 
         if (mod)
@@ -3693,12 +3685,7 @@ void G4_INST::emit_inst(std::ostream& output, bool symbol_dst, bool *symbol_srcs
         {// no need to emit size for nop, wait
             output << '(' << static_cast<int>(execSize) << ") ";
         }
-
-        if (isSpillIntrinsic())
-        {
-            output << "Scratch[" << asSpillIntrinsic()->getOffset() << "] ";
-        }
-        else if (dst)
+        if (dst)
         {
             dst->emit(output, symbol_dst);  // emit symbolic/physical register depends on the flag
             output << ' ';
@@ -3719,10 +3706,6 @@ void G4_INST::emit_inst(std::ostream& output, bool symbol_dst, bool *symbol_srcs
                 }
                 output << ' ';
             }
-        }
-        if (isFillIntrinsic())
-        {
-            output << "Scratch[" << asFillIntrinsic()->getOffset() << "] ";
         }
 
         if( isMath() && asMathInst()->getMathCtrl() != MATH_RESERVED )
