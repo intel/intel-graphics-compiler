049782d2fa1bdafd238a1bb8141a5c1840c729bc
Srividya Karumuri <srividya.karumuri@intel.com>
2020-03-17 15:00:30 -0700

    Not limiting the loop unroll to small loops for high register pressure shaders if constant folding
    Change-Id: I1932c885722a3351058acf7109aed7dddf734a4e

--- IGC/Compiler/GenTTI.cpp
+++ IGC/Compiler/GenTTI.cpp
@@ -137,7 +137,8 @@ namespace llvm {
         // For OCL shaders, do a two-step loop unrolling. The first
         // unrolling is simple and full, and the second runs after
         // LICM, which allows partial unrolling. Same for other APIs?
-        if (lowPressure || (ctx->type == ShaderType::OPENCL_SHADER))
+        if (lowPressure || (ctx->type == ShaderType::OPENCL_SHADER) ||
+            (!IGC_IS_FLAG_ENABLED(DisableDynamicConstantFolding) && (!ctx->getModuleMetaData()->inlineDynConstants.empty())))
         {
             UP.Threshold = LoopUnrollThreshold;
             UP.PartialThreshold = LoopUnrollThreshold;
