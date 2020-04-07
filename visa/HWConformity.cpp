18c56e0c80acd6515f380128cc550ec787d4fc74
gbsbuild <gbsbuild@intel.com>
2020-04-07 00:37:05 -0700

    Changes in code.
    Change-Id: I81bf3c75d08315fa7e8952449f325f1cf12b55c3

--- visa/HWConformity.cpp
+++ visa/HWConformity.cpp
@@ -5981,7 +5981,8 @@ void HWConformity::conformBB(G4_BB* bb)
         G4_INST *inst = *i;
         G4_opcode opcode = inst->opcode();
 
-        if (opcode == G4_nop ||
+        if ((inst->mayExceedTwoGRF() && !inst->isSend()) ||
+            opcode == G4_nop ||
             opcode == G4_label)
         {
             continue;
