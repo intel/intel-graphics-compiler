50b85917772e5ed5836327682b55ef4ac30dd436
Jurek, Pawel <pawel.jurek@intel.com>
2020-04-29 11:54:12 -0700

    [Autobackout][FuncReg]Revert of change: 0afbb19ee0c9daf0c3179432f38d38ff1e518987 Update ICBE_VERSION
    Change-Id: I1e08ad464546e1f1d4b08f8fe01acde9213dd6c9

--- IGC/AdaptorOCL/ocl_igc_shared/executable_format/patch_shared.h
+++ IGC/AdaptorOCL/ocl_igc_shared/executable_format/patch_shared.h
@@ -209,13 +209,12 @@ struct SPatchExecutionEnvironment :
     uint32_t    NumGRFRequired;
     uint32_t    WorkgroupWalkOrderDims; // dim0 : [0 : 1]; dim1 : [2 : 3]; dim2 : [4 : 5]
     uint32_t    HasGlobalAtomics;
-    uint32_t    reserved1;
-    uint32_t    reserved2;
+    uint32_t    reserved;
     uint32_t    StatelessWritesCount;
 };
 
 // Update CURRENT_ICBE_VERSION when modifying the patch list
-static_assert(sizeof(SPatchExecutionEnvironment) == (104 + sizeof(SPatchItemHeader)), "The size of SPatchExecutionEnvironment is not what is expected");
+static_assert(sizeof(SPatchExecutionEnvironment) == (100 + sizeof(SPatchItemHeader)), "The size of SPatchExecutionEnvironment is not what is expected");
 
 /*****************************************************************************\
 STRUCT: SPatchString
