765c254ffdf3e61fd3d4d2602d6ebedccebb6753
Alex <alexander.paige@intel.com>
2020-06-16 15:16:10 -0700

    Adding new CustomSafeOpt pattern for Ldrawvector.
    This change tries to convert ldrawvectors that only use one element into ldraw. This saves register space and in some cases can improve performance.
    
    Change-Id: I41b0c17647cada00192e700511af1ac0dc1c3b7b

--- IGC/Compiler/CustomSafeOptPass.hpp
+++ IGC/Compiler/CustomSafeOptPass.hpp
@@ -80,6 +80,7 @@ namespace IGC
         void visitFPTruncInst(llvm::FPTruncInst& I);
         void visitExtractElementInst(llvm::ExtractElementInst& I);
         void visitLdptr(llvm::CallInst* inst);
+        void visitLdRawVec(llvm::CallInst* inst);
         void visitLoadInst(llvm::LoadInst& I);
 
         //
