a7973ca962b54465013c97c5438fc3b109d7bb4f
Mariusz Merecki <mariusz.merecki@intel.com>
2020-07-15 06:15:39 -0700

    Improve URB merging, detect and merge OWROD URB writes in the following patterns: 1. shl + or     urbOffset = urbOffset << 1;     UrbWrite(urbOffset, ...);     urbOffset = urbOffset | 1;     UrbWrite(urbOffset, ...); 2. mul + or     urbOffset = urbOffset * 2;     UrbWrite(urbOffset, ...);     urbOffset = urbOffset | 1;     UrbWrite(urbOffset, ...); 3. two oword urb writes in loop     urbOffset = urbOffset * 2;     for(...) {       {...}       UrbWrite(urbOffset, ...);       urbOffset = urbOffset | 1;       UrbWrite(urbOffset, ...);       urbOffset = urbOffset + 2;       {...}     }
    Change-Id: I706e7fe5ea50c23c7713d0f729f0f18108f75b3f

--- IGC/Compiler/CISACodeGen/MergeURBWrites.cpp
+++ IGC/Compiler/CISACodeGen/MergeURBWrites.cpp
@@ -379,6 +379,86 @@ std::pair<Value*, unsigned int> MergeURBWrites::GetBaseAndOffset(Value* pUrbOffs
                 offset = GetConstant(src0) + baseAndOffset.second;
             }
         }
+        else if (pInstr->getOpcode() == Instruction::Or)
+        {
+            // Examples of patterns matched below:
+            // 1. shl + or
+            //    urbOffset = urbOffset << 1;
+            //    urbOffset = urbOffset | 1;
+            // 2. mul + or
+            //    urbOffset = urbOffset * 2;
+            //    urbOffset = urbOffset | 1;
+            // 3. two oword urb writes in loop
+            //    urbOffset = urbOffset * 2;
+            //    for(...) {
+            //      {...}
+            //      urbOffset = urbOffset | 1;
+            //      urbOffset = urbOffset + 2;
+            //      {...}
+            //    }
+            //
+            //
+
+            std::function<unsigned int(Value*)> GetAlignment =
+                [&GetAlignment, &GetConstant](Value* pUrbOffset)->unsigned int
+            {
+                unsigned int alignment = 1;
+                Instruction* pInstr = dyn_cast<Instruction>(pUrbOffset);
+                if (pInstr &&
+                    pInstr->getOpcode() == Instruction::Shl &&
+                    isa<ConstantInt>(pInstr->getOperand(1)))
+                {
+                    alignment = GetAlignment(pInstr->getOperand(0)) *
+                        (1u << GetConstant(pInstr->getOperand(1)));
+                }
+                else if (pInstr &&
+                    pInstr->getOpcode() == Instruction::Mul &&
+                    isa<ConstantInt>(pInstr->getOperand(1)) &&
+                    iSTD::IsPowerOfTwo(GetConstant(pInstr->getOperand(1))))
+                {
+                    alignment = GetAlignment(pInstr->getOperand(0)) *
+                        GetConstant(pInstr->getOperand(1));
+                }
+                return alignment;
+            };
+
+            Value* src0 = pInstr->getOperand(0);
+            Value* src1 = pInstr->getOperand(1);
+            unsigned int alignment = 1;
+            if (isa<PHINode>(src0) && isa<ConstantInt>(src1))
+            {
+                // pattern 3
+                PHINode* pPhi = cast<PHINode>(src0);
+                alignment = std::numeric_limits<unsigned int>::max();
+                for (unsigned int i = 0; i < pPhi->getNumIncomingValues(); i++)
+                {
+                    Instruction* pIncoming = dyn_cast<Instruction>(pPhi->getIncomingValue(i));
+                    if (pIncoming &&
+                        pIncoming->getOpcode() == Instruction::Add &&
+                        pPhi == pIncoming->getOperand(0) &&
+                        isa<ConstantInt>(pIncoming->getOperand(1)) &&
+                        iSTD::IsPowerOfTwo(GetConstant(pIncoming->getOperand(1))))
+                    {
+                        alignment = std::min(alignment, GetConstant(pIncoming->getOperand(1)));
+                    }
+                    else
+                    {
+                        alignment = std::min(alignment, GetAlignment(pIncoming));
+                    }
+                }
+            }
+            else
+            {
+                // patterns 1 and 2
+                alignment = GetAlignment(src0);
+            }
+            if (alignment > GetConstant(src1))
+            {
+                IGC_ASSERT(iSTD::IsPowerOfTwo(alignment));
+                pBase = src0;
+                offset = GetConstant(src1);
+            }
+        }
     }
 
     return std::make_pair(pBase, offset);
