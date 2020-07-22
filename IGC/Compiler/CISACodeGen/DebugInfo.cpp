f52097c175b0637b73c734602d8f34c3f32a323a
Pratik J Ashar <pratik.j.ashar@intel.com>
2020-07-22 03:53:05 -0700

    Emit Output attribute when -g -cl-opt-disable are both passed.
    Change-Id: I34a1bea7f40652421e9139983ad99d25a3c3a217

--- IGC/Compiler/CISACodeGen/DebugInfo.cpp
+++ IGC/Compiler/CISACodeGen/DebugInfo.cpp
@@ -359,8 +359,13 @@ void DebugInfoData::markOutputVars(const llvm::Instruction* pInst)
         // values anywhere in the code till they are in scope.
         if (m_outputVals.find(pVar) == m_outputVals.end())
         {
-            //m_pShader->GetEncoder().GetVISAKernel()->AddAttributeToVar(pVar->visaGenVariable[0], "Output", 0, nullptr);
-            (void)m_outputVals.insert(pVar);
+            if (m_pShader->GetContext()->getModuleMetaData()->compOpt.OptDisable)
+            {
+                // Emit "Output" attribute only when -g and -cl-opt-disable are both passed
+                // -g by itself shouldnt alter generated code
+                m_pShader->GetEncoder().GetVISAKernel()->AddAttributeToVar(pVar->visaGenVariable[0], "Output", 0, nullptr);
+                (void)m_outputVals.insert(pVar);
+            }
         }
     }
 }
