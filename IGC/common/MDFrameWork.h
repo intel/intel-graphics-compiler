be47a205faac45f67ae68129ae3a86dfa66f254d
Alex <alexander.paige@intel.com>
2020-07-23 17:15:36 -0700

    Adding metadata for computedDepthMode
    Change-Id: If3c376649b049c7bff22fb95fee2086d3fd14fb2

--- IGC/common/MDFrameWork.h
+++ IGC/common/MDFrameWork.h
@@ -373,6 +373,7 @@ namespace IGC
 
         unsigned int privateMemoryPerWI = 0;
         std::array<uint64_t, NUM_SHADER_RESOURCE_VIEW_SIZE> m_ShaderResourceViewMcsMask{};
+        unsigned int computedDepthMode = 0; //Defaults to 0 meaning depth mode is off
     };
     void serialize(const IGC::ModuleMetaData &moduleMD, llvm::Module* module);
     void deserialize(IGC::ModuleMetaData &deserializedMD, const llvm::Module* module);
