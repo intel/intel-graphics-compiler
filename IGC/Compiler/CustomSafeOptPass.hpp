009e47b0770c22038c94a572db32a8f549d597a4
Bartlomiej Gajda <bartlomiej.gajda@intel.com>
2020-07-29 11:20:18 -0700

    Add pass to Split Indirect EE to sel to avoid VxH mov.
    It's a separate pass, as it wants to capture result of LowerGEPIntoPrivMem.
    
    Heuristic currently handles calculating cost cases:
    - less than <4 x type> vec size
    - when EEI index is a multiply(+add) of some ConstantInt as in below example.
    
    From:
      %268 = mul nuw i32 %res.i2.i, 3
      %269 = extractelement <12 x float> %234, i32 %268
      %274 = extractelement <12 x float> %198, i32 %268
      %270 = add i32 %268, 1
      %271 = extractelement <12 x float> %234, i32 %270
      %275 = extractelement <12 x float> %198, i32 %270
      %272 = add i32 %268, 2
      %273 = extractelement <12 x float> %234, i32 %272
      %276 = extractelement <12 x float> %198, i32 %272
    
    To:
      %250 = icmp eq i32 %res.i2.i, i16 1
      %251 = select i1 %250, float %206, float %200
      %252 = select i1 %250, float %208, float %202
      %253 = select i1 %250, float %210, float %204
      %254 = select i1 %250, float %48, float %32
      %255 = select i1 %250, float %49, float %33
      %256 = select i1 %250, float %50, float %34
      %257 = icmp eq i32 %res.i2.i, i16 2
      %258 = select i1 %257, float %214, float %251
      %259 = select i1 %257, float %215, float %252
      %260 = select i1 %257, float %216, float %253
      %261 = select i1 %257, float %64, float %254
      %262 = select i1 %257, float %65, float %255
      %263 = select i1 %257, float %66, float %256
    
    Change-Id: I787c336aab0846c0d5d978718b5cd44d460499a9

--- IGC/Compiler/CustomSafeOptPass.hpp
+++ IGC/Compiler/CustomSafeOptPass.hpp
@@ -249,6 +249,7 @@ namespace IGC
     llvm::FunctionPass* createGenStrengthReductionPass();
     llvm::FunctionPass* createNanHandlingPass();
     llvm::FunctionPass* createFlattenSmallSwitchPass();
+    llvm::FunctionPass* createSplitIndirectEEtoSelPass();
     llvm::FunctionPass* createIGCIndirectICBPropagaionPass();
     llvm::FunctionPass* createBlendToDiscardPass();
     llvm::FunctionPass* createMarkReadOnlyLoadPass();
