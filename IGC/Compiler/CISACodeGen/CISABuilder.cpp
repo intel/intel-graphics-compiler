b981827f0f2b8e0a476898929dea3b6a47fc1534
Diana Chen <po-yu.chen@intel.com>
2020-08-24 12:39:41 -0700

    Minor fix
    Change-Id: I793357b38127fe6591926ad9a6e8b51003b2d306

--- IGC/Compiler/CISACodeGen/CISABuilder.cpp
+++ IGC/Compiler/CISACodeGen/CISABuilder.cpp
@@ -4161,7 +4161,8 @@ namespace IGC
         {
             SaveOption(vISA_dumpNewSyntax, false);
         }
-        if (IGC_IS_FLAG_ENABLED(disableCompaction))
+        if (IGC_IS_FLAG_ENABLED(disableCompaction)
+            )
         {
             SaveOption(vISA_Compaction, false);
         }
