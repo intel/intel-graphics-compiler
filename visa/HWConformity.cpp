e2e4d7796e0ae071d1388e2505f584d52e2b0a64
Wei Chen <weiyu.chen@intel.com>
2020-09-16 11:56:44 -0700

    Swap src0 and src1 for pseudo-mad if src1 is scalar but src0 is not.
    Change-Id: I77a1ea2f15cc96d6671b47288c89f555bdba933b

--- visa/HWConformity.cpp
+++ visa/HWConformity.cpp
@@ -3478,9 +3478,11 @@ bool HWConformity::isGoodAlign1TernaryDst(G4_INST* inst) const
     auto dstTySize = getTypeSize(dst->getType());
 
     int alignInBytes = std::max((int) dstTySize, builder.get3SrcDstAlign());
-    // if src2 is not a scalar, then align it to 32 bytes.
+
     if (builder.noSrc2Regioning())
     {
+        // src2 is required to have the same subreg as dst if src2 is not a scalar
+        // If we can't guarantee this we have to align both of them to 32 byte
         unsigned src2Pos = inst->opcode() == G4_pseudo_mad ? 0 : 2;
         auto src2 = inst->getSrc(src2Pos);
         if (src2->isSrcRegRegion() && !src2->asSrcRegRegion()->isScalar())
@@ -3937,18 +3939,6 @@ bool HWConformity::generateAlign1Mad(G4_BB* bb, INST_LIST_ITER iter)
     MUST_BE_TRUE(inst->opcode() == G4_pseudo_mad, "expect pseudo mad");
     bool mustDoMad = IS_TYPE_FLOAT_ALL(inst->getDst()->getType());
 
-    if (!isGoodAlign1TernaryDst(inst))
-    {
-        if (mustDoMad)
-        {
-            auto alignment = builder.noSrc2Regioning() ? GRFALIGN : Four_Word;
-            inst->setDest(insertMovAfter(iter, inst->getDst(), inst->getDst()->getType(), bb, alignment));
-        }
-        else
-        {
-            return false;
-        }
-    }
 
     // try swapping src0 (really src2) and src1 to see if we can save a move
     // some conditions where swap may help:
@@ -3956,6 +3946,7 @@ bool HWConformity::generateAlign1Mad(G4_BB* bb, INST_LIST_ITER iter)
     // -- if src1 is imm, as MAD src2 supports 16-bit imm
     // -- if src0 is HF in a mix mode MAD, as MAD src1 supports HF
     // -- if src1 is scalar, as MAD src2 has more region restrictions
+    // We perform the swapping before the dst checks as some platforms require dst and src2 to have the same subreg
     {
         G4_Operand* src0 = inst->getSrc(0);
         G4_Operand* src1 = inst->getSrc(1);
@@ -3968,17 +3959,29 @@ bool HWConformity::generateAlign1Mad(G4_BB* bb, INST_LIST_ITER iter)
             //swap src0 and src1 as src0 supports imm
             inst->swapSrc(0, 1);
         }
-        else if (!isGoodAlign1TernarySrc(inst, 0, true) &&
+        else if (src0->isSrcRegRegion() && !src0->asSrcRegRegion()->isScalar() &&
             src1->isSrcRegRegion() &&
             src1->asSrcRegRegion()->isScalar())
         {
-            // Swap src0 and src1 if src1 is scalar but src0 is not a good Align1TernarySrc
-            // when src2 regioning support is quite limited.
+            // Swap src0 and src1 if src1 is scalar but src0 is not, as src2 regioning support is quite limited.
             inst->swapSrc(0, 1);
         }
         else if (isLowPrecisionFloatTy(src0->getType()) && src1->getType() == Type_F)
         {
             inst->swapSrc(0, 1);
+        }
+    }
+
+    if (!isGoodAlign1TernaryDst(inst))
+    {
+        if (mustDoMad)
+        {
+            auto alignment = builder.noSrc2Regioning() ? GRFALIGN : Four_Word;
+            inst->setDest(insertMovAfter(iter, inst->getDst(), inst->getDst()->getType(), bb, alignment));
+        }
+        else
+        {
+            return false;
         }
     }
 
