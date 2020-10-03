99fee671bc53894402ec99ae3c2fa8625ede732d
Wei Chen <weiyu.chen@intel.com>
2020-10-02 18:40:19 -0700

    [Autobackout][FuncReg]Revert of change: 556db31680c36099f8ae12a4d2094899eed644e8 Minor cleanup
    Change-Id: Ibf7b10df5a7d058133b6b90b7e22a75909d27a30

--- visa/Gen4_IR.cpp
+++ visa/Gen4_IR.cpp
@@ -2573,9 +2573,9 @@ bool G4_INST::canHoistTo(const G4_INST *defInst, bool simdBB) const
 {
     bool indirect_dst = (dst->getRegAccess() != Direct);
 
-    auto def_dst = defInst->getDst();
+    G4_Operand *def_dst = defInst->getDst();
 
-    if (!def_dst)
+    if (def_dst == NULL)
     {
         // can this actually happen?
         return false;
