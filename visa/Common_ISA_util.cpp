9322c40b170ad171e8d64cf79724eb628917da64
Pratik J Ashar <pratik.j.ashar@intel.com>
2020-10-14 06:31:05 -0700

    Change name length field type in VISA debug info. Remove IGC restriction on kernel name length in CISA binary.
    Change-Id: Ie7313e42dc97c06eac8e8d7b708a4432b055a1d2

--- visa/Common_ISA_util.cpp
+++ visa/Common_ISA_util.cpp
@@ -1090,7 +1090,7 @@ int vector_opnd::getSizeInBinary() const
 /*
 function_info {
     ub linkage; // MBZ
-    ub name_len;
+    uw name_len;
     ub name[name_len];
     ud offset;
     ud size;
@@ -1111,7 +1111,7 @@ uint32_t function_info_t::getSizeInBinary() const
 
 /*
     kernel_info {
-    ub name_len;
+    uw name_len;
     ub name[name_len];
     ud offset;
     ud size;
