47e82b9dd16bcb6e1b6aaf6647133ba7341ab889
Aleksandr Bezzubikov <aleksandr.bezzubikov@intel.com>
2020-10-23 12:13:59 -0700

    Consider SIMD mask while constfolding
    Change-Id: I9d1fb5b2466c61d9a593f65e40c76c245ec74448

--- IGC/VectorCompiler/lib/GenXOpts/CMAnalysis/ConstantFoldingGenX.cpp
+++ IGC/VectorCompiler/lib/GenXOpts/CMAnalysis/ConstantFoldingGenX.cpp
@@ -135,6 +135,7 @@ static Constant *constantFoldWrRegion(Type *RetTy,
                                       const CMRegion &R, const DataLayout &DL) {
   Constant *OldValue = Operands[GenXIntrinsic::GenXRegion::OldValueOperandNum];
   Constant *NewValue = Operands[GenXIntrinsic::GenXRegion::NewValueOperandNum];
+  Constant *Mask = Operands[GenXIntrinsic::GenXRegion::PredicateOperandNum];
   // The inputs can be ConstantExpr if we are being called from
   // CallAnalyzer.
   if (isa<ConstantExpr>(OldValue) || isa<ConstantExpr>(NewValue))
@@ -147,7 +148,8 @@ static Constant *constantFoldWrRegion(Type *RetTy,
 
   const int RetElemSize = DL.getTypeSizeInBits(RetTy->getScalarType()) / 8;
   unsigned Offset = OffsetC->getSExtValue() / RetElemSize;
-  if (isa<UndefValue>(OldValue) && R.isContiguous() && (Offset == 0)) {
+  if (isa<UndefValue>(OldValue) && R.isContiguous() && Offset == 0 &&
+      Mask->isAllOnesValue()) {
     // If old value is undef and new value is splat, and the result vector
     // is no bigger than 2 GRFs, then just return a splat of the right type.
     Constant *Splat = NewValue;
@@ -172,7 +174,7 @@ static Constant *constantFoldWrRegion(Type *RetTy,
     return UndefValue::get(RetTy); // out of range index
   if (!isa<VectorType>(NewValue->getType()))
     Values[Offset] = NewValue;
-  else {
+  else if (!Mask->isZeroValue()) {
     unsigned RowIdx = Offset;
     unsigned Idx = RowIdx;
     unsigned NextRow = R.Width;
@@ -185,7 +187,10 @@ static Constant *constantFoldWrRegion(Type *RetTy,
       if (Idx >= WholeNumElements)
         // return collected values even if idx is out of bounds
         return ConstantVector::get(Values);
-      Values[Idx] = NewValue->getAggregateElement(i);
+      if (Mask->isAllOnesValue() ||
+          (Mask->getType()->isVectorTy() &&
+           !cast<ConstantVector>(Mask)->getAggregateElement(i)->isZeroValue()))
+        Values[Idx] = NewValue->getAggregateElement(i);
       Idx += R.Stride;
     }
   }
