776459c9e42d2f0270820e1f0d589f391475d933
AnBodrova <anastasia.bodrova@intel.com>
2020-11-30 16:11:17 +0000

    Changes in code.

--- IGC/common/MDFrameWork.h
+++ IGC/common/MDFrameWork.h
@@ -170,7 +170,6 @@ namespace IGC
         int privateMemoryPerWI = 0;
         bool globalIDPresent = false;
         bool isUniqueEntry = false;
-        bool openCLNoLocalAddresses = false;
 
         std::vector<std::string> UserAnnotations;
 
