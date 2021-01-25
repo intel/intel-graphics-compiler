ec74f9f4865aca2dfec3fb0c57c50726b70cb4db
Aleksander Us <aleksander.us@intel.com>
2021-01-25 16:40:19 +0000

    Filter components, not libraries in get_llvm_targets
    LLVM_DYLIB_COMPONENTS contains components. These are similar to
    targets, but do not have LLVM prefix. Use these components as input to
    filtering function. This is unlikely that libLLVM will contain things
    other than "all" but if it will, previous version will break.

--- IGC/cmake/utils.cmake
+++ IGC/cmake/utils.cmake
@@ -4,15 +4,27 @@
 # lib. Otherwise, just return passed libraries.
 # ret -- name of variable with returned targets list. All other
 # arguments are targets to process.
-function(vc_get_llvm_targets RET)
+function(igc_get_llvm_targets RET)
   set(TARGETS ${ARGN})
+
   if (LLVM_LINK_LLVM_DYLIB)
+    # Drop all components, though it is probably not right
+    # and llvm_map_components_to_libnames should be used as filter.
+    # However, in external build it returns empty list for "all"
+    # so filtering is not really done.
     if ("${LLVM_DYLIB_COMPONENTS}" STREQUAL "all")
       set(TARGETS "")
     else()
       list(REMOVE_ITEM TARGETS ${LLVM_DYLIB_COMPONENTS})
     endif()
+  endif()
+
+  # Expand rest of components names to target names.
+  llvm_map_components_to_libnames(TARGETS ${TARGETS})
+
+  if (LLVM_LINK_LLVM_DYLIB)
     set(TARGETS ${TARGETS} LLVM)
   endif()
+
   set(${RET} ${TARGETS} PARENT_SCOPE)
 endfunction()
