56151cd2e682c3ef75d27f8db412e15f3651e3df
Wei Chen <weiyu.chen@intel.com>
2021-01-27 00:22:48 +0000

    Take SpillMemOffset into consideration when reporting spill size.

--- visa/GraphColor.cpp
+++ visa/GraphColor.cpp
@@ -9496,7 +9496,7 @@ int GlobalRA::coloringRegAlloc()
     const unsigned maxRAIterations = 10;
     unsigned iterationNo = 0;
 
-    int globalScratchOffset = kernel.getInt32KernelAttr(Attributes::ATTR_SpillMemOffset);
+    const int globalScratchOffset = kernel.getInt32KernelAttr(Attributes::ATTR_SpillMemOffset);
     bool useScratchMsgForSpill = !hasStackCall && (globalScratchOffset < (int)(SCRATCH_MSG_LIMIT * 0.6)
     );
     bool enableSpillSpaceCompression = builder.getOption(vISA_SpillSpaceCompression);
@@ -9932,7 +9932,9 @@ int GlobalRA::coloringRegAlloc()
     }
 
     // this includes vISA's scratch space use only and does not include whatever IGC may use for private memory
-    uint32_t spillMemUsed = std::max(this->actualSpillSize, nextSpillOffset);
+
+    auto spillSizeFromMsg = (this->actualSpillSize > (uint32_t) globalScratchOffset) ? this->actualSpillSize - globalScratchOffset : this->actualSpillSize;
+    uint32_t spillMemUsed = std::max(spillSizeFromMsg, nextSpillOffset);
     if (spillMemUsed)
     {
         builder.criticalMsgStream() << "Spill memory used = " << spillMemUsed << " bytes for kernel " <<
