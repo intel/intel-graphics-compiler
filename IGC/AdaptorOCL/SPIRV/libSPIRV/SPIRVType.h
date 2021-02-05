92bcba920da4180fe540ac38ddaa7adbd13e0bd2
lgotszal <lukasz.gotszald@intel.com>
2021-02-05 12:38:41 +0000

    update copyright headers in IGC/AdaptorOCL

--- IGC/AdaptorOCL/SPIRV/libSPIRV/SPIRVType.h
+++ IGC/AdaptorOCL/SPIRV/libSPIRV/SPIRVType.h
@@ -1,28 +1,26 @@
-/*===================== begin_copyright_notice ==================================
+/*========================== begin_copyright_notice ============================
 
-Copyright (c) 2017 Intel Corporation
+Copyright (c) 2000-2021 Intel Corporation
 
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom
+the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included
 in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
 
-
-======================= end_copyright_notice ==================================*/
+============================= end_copyright_notice ===========================*/
 
 //===- SPIRVType.h - Class to represent a SPIR-V Type ------------*- C++ -*-===//
 //
@@ -518,7 +516,7 @@ public:
 
   SPIRVWord getMemberCount() const { return MemberTypeVec.size();}
   SPIRVType *getMemberType(size_t I) const { return MemberTypeVec[I];}
-  void setMemberType(size_t I, SPIRVType* pTy) 
+  void setMemberType(size_t I, SPIRVType* pTy)
   {
       if (I >= MemberTypeVec.size() && !ContinuedInstructions.empty()) {
           const size_t MaxNumElements = MaxWordCount - FixedWC;
