d2d04d0ea1401269a30a03bb8ec95657ad40013d
Xiao Lei <xiao.lei@intel.com>
2021-02-09 19:56:25 +0000

    This reverts "[IGC BugFix][IGC]: For optnone builtins, allow -O0 flag to determine if we should call them as subroutines or stackcalls (#9430)"

--- IGC/AdaptorCommon/ProcessFuncAttributes.cpp
+++ IGC/AdaptorCommon/ProcessFuncAttributes.cpp
@@ -426,6 +426,19 @@ bool ProcessFuncAttributes::runOnModule(Module& M)
         if (isKernel)
             continue;
 
+        // OptNone builtins are special versions of builtins assuring that all
+        // theirs parameters are constant values.
+        if(isOptNoneBuiltin(F->getName()))
+        {
+            // OptimizeNone attribute was only required to prevent clang optimizations.
+            // We can remove it now to unblock IGC optimizations.
+            F->removeFnAttr(llvm::Attribute::OptimizeNone);
+            // Treat optnone builtins as subroutines to avoid kernel bloat.
+            // Optnone attribute implies noinline attribute being present, so we don't need to
+            // set noinline attribute explicitly here.
+            continue;
+        }
+
         // Flag for function calls where alwaysinline must be true
         bool mustAlwaysInline = false;
 
@@ -433,20 +446,7 @@ bool ProcessFuncAttributes::runOnModule(Module& M)
         if (F->hasFnAttribute(llvm::Attribute::Builtin) ||
             F->getName().startswith(igc_spv::kLLVMName::builtinPrefix))
         {
-            // OptNone builtins are special versions of builtins assuring that all
-            // theirs parameters are constant values.
-            if (isOptNoneBuiltin(F->getName()))
-            {
-                // OptimizeNone attribute was only required to prevent clang optimizations.
-                // We can remove it now to unblock IGC optimizations.
-                // Optnone attribute implies noinline attribute being present, so we don't need to
-                // set noinline attribute explicitly here.
-                F->removeFnAttr(llvm::Attribute::OptimizeNone);
-            }
-            else
-            {
-                mustAlwaysInline = true;
-            }
+            mustAlwaysInline = true;
         }
         // inline all OCL math functions if __FastRelaxedMath is set
         else if (fastMathFunct.find(F) != fastMathFunct.end())
