85bbf2b350e5fd68c540bec4ea7297aaf7377663
DmitryBushev <dmitry.bushev@intel.com>
2021-02-19 17:23:33 +0000

    Promoting feature refactoring '--version' option display in cmc

--- IGC/CMFE/AdaptorCM/Interface.h
+++ IGC/CMFE/AdaptorCM/Interface.h
@@ -1,3 +1,27 @@
+/*========================== begin_copyright_notice ============================
+
+Copyright (c) 2019-2021 Intel Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom
+the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+
+============================= end_copyright_notice ===========================*/
+
 #ifndef LLVM_FRONTEND_WRAPPER_INTERFACE_H
 #define LLVM_FRONTEND_WRAPPER_INTERFACE_H
 
@@ -80,6 +104,9 @@ static const int InterfaceVersion = 9;
 } // namespace ClangFE
 } // namespace CM
 } // namespace Intel
+
+extern "C" INTEL_CM_CLANGFE_DLL_DECL bool IntelCMClangFEIsShowVersionInvocation(
+    Intel::CM::ClangFE::IDriverInvocation const *DriverInvocPtr);
 
 extern "C" INTEL_CM_CLANGFE_DLL_DECL int
 IntelCMClangFEGetInterfaceVersion();
