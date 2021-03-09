a1a03dc33324500fbf51e6e65cb22b68555492ba
Pratik J Ashar <pratik.j.ashar@intel.com>
2021-03-09 12:41:22 +0000

    Dont add fill for spilled variables that have a single def in program and are not live-in to the BB, hence not loop carried.
    definition. Such variables dont need read-modify-write when spilled.

--- visa/SpillManagerGMRF.h
+++ visa/SpillManagerGMRF.h
@@ -593,6 +593,8 @@ private:
         }
     }
 
+    void populateDefsTable();
+
     // Data
     GlobalRA&                gra;
     IR_Builder *             builder_;
@@ -622,6 +624,8 @@ private:
 
     const Interference *     spillIntf_;
     vISA::Mem_Manager              mem_;
+
+    std::unordered_map<G4_Declare*, unsigned int> VarDefs;
 
     // The number of GRF spill.
     unsigned numGRFSpill = 0;
