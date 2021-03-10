97e2dac1936f85aed9df164dda867ba0cb7f98e5
Aleksandr Bezzubikov <aleksandr.bezzubikov@intel.com>
2021-03-10 13:16:33 -0800

    [Autobackout][FuncReg]Revert of change: 04a66cbf9b60936f5189098618161ac8b8435869 Switch TPM to SVM entirely

--- IGC/VectorCompiler/include/vc/GenXCodeGen/GenXInternalMetadata.h
+++ IGC/VectorCompiler/include/vc/GenXCodeGen/GenXInternalMetadata.h
@@ -38,14 +38,6 @@ namespace FunctionMD {
 inline constexpr const char GenXKernelInternal[] = "genx.kernel.internal";
 }
 
-namespace InstMD {
-inline constexpr const char SVMBlockType[] = "SVMBlockType";
-}
-
-namespace ModuleMD {
-inline constexpr const char UseSVMStack[] = "genx.useGlobalMem";
-}
-
 namespace internal {
 
 namespace KernelMDOp {
--- IGC/VectorCompiler/lib/GenXCodeGen/GenXCisaBuilder.cpp
+++ IGC/VectorCompiler/lib/GenXCodeGen/GenXCisaBuilder.cpp
@@ -3633,10 +3633,7 @@ void GenXKernelBuilder::buildIntrinsic(CallInst *CI, unsigned IntrinID,
     Value *V = CI;
     if (!AI.isRet())
       V = CI->getArgOperand(AI.getArgIdx());
-    auto *EltType = V->getType()->getScalarType();
-    if (auto *MDType = CI->getMetadata(InstMD::SVMBlockType))
-      EltType = cast<ValueAsMetadata>(MDType->getOperand(0).get())->getType();
-    unsigned ElBytes = getResultedTypeSize(EltType, DL);
+    unsigned ElBytes = getResultedTypeSize(V->getType()->getScalarType(), DL);
     switch (ElBytes) {
       // For N = 2 byte data type, use block size 1 and block count 2.
       // Otherwise, use block size N and block count 1.
