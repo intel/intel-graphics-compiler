125603e6142c7574d405a3ffe3ca21d3d810f4fc
Arkady Shlykov <arkady.shlykov@intel.com>
2021-03-12 18:59:46 +0000

    Revert "[IGC Performance][IGC]: Preprocess vector bitcasts to enable memory operations simplification."
    This reverts commit a3b0a66933d6297c8aca4badc4e877558c4be7c2.

--- IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
+++ IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
@@ -651,8 +651,6 @@ static void AddLegalizationPasses(CodeGenContext& ctx, IGCPassManager& mpm, PSSi
         mpm.add(createSplitIndirectEEtoSelPass());
     }
 
-    // Preprocess vector bitcasts to enable memory operations simplification.
-    mpm.add(createVectorBitCastOptPass());
     // Split big vector & 3-element load/store, etc.
     mpm.add(createVectorPreProcessPass());
 
