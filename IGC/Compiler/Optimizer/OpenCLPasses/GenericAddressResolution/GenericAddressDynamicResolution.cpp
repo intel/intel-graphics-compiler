24e5a7c149f1d82e055bf73d3cc8c9e54844d080
Wei Chen <weiyu.chen@intel.com>
2021-03-24 22:28:30 -0700

    [Autobackout][FuncReg]Revert of change: 442fd3cf121b916b344f88e311471396020e8079 Force private memory to global buffer when generic pointers are present.

--- IGC/Compiler/Optimizer/OpenCLPasses/GenericAddressResolution/GenericAddressDynamicResolution.cpp
+++ IGC/Compiler/Optimizer/OpenCLPasses/GenericAddressResolution/GenericAddressDynamicResolution.cpp
@@ -226,8 +226,7 @@ bool GenericAddressDynamicResolution::visitLoadStoreInst(Instruction& I)
     }
 
     if (pointerAddressSpace == ADDRESS_SPACE_GENERIC) {
-        bool allocatePrivateAsGlobal = m_ctx->forceGlobalMemoryAllocation() || IGC_IS_FLAG_ENABLED(ForcePrivateMemoryToGlobalOnGeneric);
-        if (allocatePrivateAsGlobal && m_ctx->hasNoLocalToGenericCast())
+        if (m_ctx->forceGlobalMemoryAllocation() && m_ctx->hasNoLocalToGenericCast())
         {
             resolveGASWithoutBranches(I, pointerOperand);
         }
