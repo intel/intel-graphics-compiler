57e4883065731572b7c7885bd6ee77a04f82342d
Jurek, Pawel <pawel.jurek@intel.com>
2021-04-06 11:44:26 -0700

    [Autobackout][FuncReg]Revert of change: 74aa5632fd86aeb0c6ab7fdfbc33ef556c61e0e0 Respect per instruction contraction flag in mad pattern match.
    SPV_INTEL_fp_fast_math_mode introduces AllowContractFastINTEL fast math flag. When instruction is decorated with this flag, contractions are allowed even if it may violate language standard. This enum maps to LLVM fast math flags, but was not respected in "mad" instruction pattern match.

--- IGC/Compiler/CISACodeGen/PatternMatchPass.cpp
+++ IGC/Compiler/CISACodeGen/PatternMatchPass.cpp
@@ -2030,18 +2030,9 @@ namespace IGC
         {
             return false;
         }
-
-        bool isFpMadWithContractionOverride = false;
         if (isFpMad(I) && m_AllowContractions == false)
         {
-            if (I.hasAllowContract())
-            {
-                isFpMadWithContractionOverride = true;
-            }
-            else
-            {
-                return false;
-            }
+            return false;
         }
         if (!isFpMad(I) && !(m_Platform.doIntegerMad() && m_ctx->m_DriverInfo.EnableIntegerMad()))
         {
@@ -2081,10 +2072,6 @@ namespace IGC
                     // in case we know we won't be able to remove the mul we don't merge it
                     if (!m_PosDep->PositionDependsOnInst(mul) && NeedInstruction(*mul))
                         continue;
-
-                    if (isFpMadWithContractionOverride && !mul->hasAllowContract())
-                        continue;
-
                     sources[2] = I.getOperand(1 - i);
                     sources[1] = mul->getOperand(0);
                     sources[0] = mul->getOperand(1);
