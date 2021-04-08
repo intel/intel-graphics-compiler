1e4cf949677924b48bc7fbef84be9f456cb0d28d
Igor Tsimbalist <igor.v.tsimbalist@intel.com>
2021-04-08 17:23:02 +0000

    When promoting arrays to registers wrong assumption regarding fp64 and int64 is made.

--- IGC/Compiler/CISACodeGen/LowerGEPForPrivMem.cpp
+++ IGC/Compiler/CISACodeGen/LowerGEPForPrivMem.cpp
@@ -258,11 +258,6 @@ static void GetAllocaLiverange(Instruction* I, unsigned int& liverangeStart, uns
 
 bool LowerGEPForPrivMem::IsNativeType(Type* type)
 {
-    if ((type->isDoubleTy() && m_ctx->platform.hasNoFP64Inst()) ||
-        (type->isIntegerTy(64) && m_ctx->platform.hasNoFullI64Support()))
-    {
-        return false;
-    }
     return true;
 }
 
