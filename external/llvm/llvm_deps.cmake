383601f2874b875ab8e1aa7354bec3f25c651280
Lukasz Wesierski <lukasz.wesierski@intel.com>
2021-04-23 08:36:19 -0700

    [Autobackout][FuncReg]Revert of change: 1c54754ea979c299c239450ae101d52a5b8ce049 Included lldELF library to IGC solution

--- external/llvm/llvm_deps.cmake
+++ external/llvm/llvm_deps.cmake
@@ -38,7 +38,6 @@ cmake_policy(VERSION 3.13.4)
 include_guard(DIRECTORY)
 
 set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR} ${CMAKE_MODULE_PATH})
-set(IGC_OPTION__LLVM_LLD OFF)
 
 # Get preferred version of LLVM.
 include(llvm_preferred_version)
@@ -56,11 +55,6 @@ include(llvm_clang_source_hook)
 
 # SPIRV translator source hook.
 include(llvm_spirv_source_hook)
-
-# LLD source hook.
-if(IGC_OPTION__LLVM_LLD)
-  include(llvm_lld_source_hook)
-endif()
 
 # Process LLVM.
 include(llvm)
