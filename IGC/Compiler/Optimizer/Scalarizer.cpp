b091f925f058b497453b392446f15e0116f598ab
Dmitry Kostin <dmitry.kostin@intel.com>
2021-05-09 23:03:04 -0700

    [Autobackout][FuncReg]Revert of change: 1af3fb3e3cca9222416fd05b394b053736dd0052 Sync change

--- IGC/Compiler/Optimizer/Scalarizer.cpp
+++ IGC/Compiler/Optimizer/Scalarizer.cpp
@@ -72,7 +72,6 @@ ScalarizeFunction::~ScalarizeFunction()
 {
     releaseAllSCMEntries();
     delete[] m_SCMAllocationArray;
-    destroyDummyFunc();
     V_PRINT(scalarizer, "ScalarizeFunction destructor\n");
 }
 
@@ -155,6 +154,8 @@ bool ScalarizeFunction::runOnFunction(Function& F)
         IGC_ASSERT_MESSAGE((*index)->use_empty(), "Unable to remove used instruction");
         (*index)->eraseFromParent();
     }
+
+    destroyDummyFunc();
 
     V_PRINT(scalarizer, "\nCompleted scalarizing function: " << m_currFunc->getName() << "\n");
     return true;
