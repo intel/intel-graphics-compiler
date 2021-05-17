581517a81ca52a8763983ca7d52fb1f84434c8e9
lgotszal <lukasz.gotszald@intel.com>
2021-05-17 21:48:15 +0000

    update copyright headers in Source/skuwa

--- skuwa/ixehp_sdv_hw_wa.c
+++ skuwa/ixehp_sdv_hw_wa.c
@@ -1,22 +1,10 @@
-/*****************************************************************************
-** Copyright (c) Intel Corporation (2012-2020).                              *
-**                                                                           *
-** INTEL MAKES NO WARRANTY OF ANY KIND REGARDING THE CODE.  THIS CODE IS     *
-** LICENSED ON AN "AS IS" BASIS AND INTEL WILL NOT PROVIDE ANY SUPPORT,      *
-** ASSISTANCE, INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL DOES NOT     *
-** PROVIDE ANY UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY      *
-** DISCLAIMS ANY WARRANTY OF MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR   *
-** ANY PARTICULAR PURPOSE, OR ANY OTHER WARRANTY.  Intel disclaims all       *
-** liability, including liability for infringement of any proprietary        *
-** rights, relating to use of the code. No license, express or implied, by   *
-** estoppel or otherwise, to any intellectual property rights is             *
-** granted herein.                                                           *
-**                                                                           *
-** Contains Workaround table initializers for XeHP_SDV                       *
-**                                                                           *
-** AUTO-GENERATED FILE. DO NOT EDIT.                                         *
-**                                                                           *
-*****************************************************************************/
+/*========================== begin_copyright_notice ============================
+
+Copyright (C) 2021 Intel Corporation
+
+SPDX-License-Identifier: MIT
+
+============================= end_copyright_notice ===========================*/
 
 #include "wa_def.h"
 
--- skuwa/ixehp_sdv_sw_wa.c
+++ skuwa/ixehp_sdv_sw_wa.c
@@ -1,20 +1,11 @@
-/*****************************************************************************
-** Copyright (c) Intel Corporation (2013-2016).                              *
-**                                                                           *
-** INTEL MAKES NO WARRANTY OF ANY KIND REGARDING THE CODE.  THIS CODE IS     *
-** LICENSED ON AN "AS IS" BASIS AND INTEL WILL NOT PROVIDE ANY SUPPORT,      *
-** ASSISTANCE, INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL DOES NOT     *
-** PROVIDE ANY UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY      *
-** DISCLAIMS ANY WARRANTY OF MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR   *
-** ANY PARTICULAR PURPOSE, OR ANY OTHER WARRANTY.  Intel disclaims all       *
-** liability, including liability for infringement of any proprietary        *
-** rights, relating to use of the code. No license, express or implied, by   *
-** estoppel or otherwise, to any intellectual property rights is             *
-** granted herein.                                                           *
-**                                                                           *
-** Contains Workaround table initializers for XeHP_SDV.                      *
-**                                                                           *
-*****************************************************************************/
+/*========================== begin_copyright_notice ============================
+
+Copyright (C) 2021 Intel Corporation
+
+SPDX-License-Identifier: MIT
+
+============================= end_copyright_notice ===========================*/
+
 #include "wa_def.h"
 
 #define XE_HP_SDV_GT_REV_ID_A0   SI_REV_ID(0,0)
