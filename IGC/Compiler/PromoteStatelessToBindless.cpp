4fdfb2201dbea0eb4c24d9d62cb2d143f1c20fce
Marcin Naczk <marcin.naczk@intel.com>
2021-05-19 15:57:25 +0000

    Add missing debug line info in PromoteStatelessToBlindless

--- IGC/Compiler/PromoteStatelessToBindless.cpp
+++ IGC/Compiler/PromoteStatelessToBindless.cpp
@@ -206,6 +206,7 @@ void PromoteStatelessToBindless::PromoteStatelessToBindlessBuffers(Function& F) 
                     GenISAIntrinsic::GenISA_simdBlockReadBindless,
                     { accessInst->getType(), basePointer->getType(),Type::getInt32Ty(accessInst->getContext()) });
                 Instruction* newBlockRead = CallInst::Create(newBlockReadFunc, { basePointer, bufferOffset }, "", accessInst);
+                newBlockRead->setDebugLoc(pIntr->getDebugLoc());
                 accessInst->replaceAllUsesWith(newBlockRead);
                 accessInst->eraseFromParent();
             }
@@ -215,6 +216,7 @@ void PromoteStatelessToBindless::PromoteStatelessToBindlessBuffers(Function& F) 
                     GenISAIntrinsic::GenISA_simdBlockWriteBindless,
                     { basePointer->getType(), pIntr->getOperand(1)->getType(), Type::getInt32Ty(accessInst->getContext()) });
                 Instruction* newBlockWrite = CallInst::Create(newBlockWriteFunc, { basePointer, pIntr->getOperand(1), bufferOffset }, "", accessInst);
+                newBlockWrite->setDebugLoc(pIntr->getDebugLoc());
                 accessInst->replaceAllUsesWith(newBlockWrite);
                 accessInst->eraseFromParent();
             }
