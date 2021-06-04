beaea61901c2d9380ed4da2ca1ce5e3a152b82f5
Dmitry Ryabtsev <dmitry.ryabtsev1@intel.com>
2021-06-04 15:55:57 +0000

    fix copy-paste in lld libraries search

--- IGC/cmake/igc_find_liblldELF.cmake
+++ IGC/cmake/igc_find_liblldELF.cmake
@@ -9,31 +9,26 @@
 #   IGC_OPTION__LLDELF_LIB_DIR - Specify additional directories for searching lldELF library
 #   IGC_OPTION__LLDELF_H_DIR - Specify additional directories for searching lldELF headers
 
+function(find_lld_library VAR LIB_NAME)
+  find_library(${VAR}
+    ${LIB_NAME}
+    PATHS "${IGC_OPTION__LLDELF_LIB_DIR}"
+    PATH_SUFFIXES "llvm-${LLVM_VERSION_MAJOR}/lib")
+
+  if(${VAR}-NOTFOUND)
+    message(FATAL_ERROR
+    "Cannot find ${LIB_NAME} library, please install missing library or provide the path by IGC_OPTION__LLDELF_LIB_DIR")
+  endif()
+endfunction()
+
 if(IGC_BUILD__LLVM_SOURCES)
   set(LLD_ELF_LIB lldELF)
   set(LLD_COM_LIB lldCommon)
   get_target_property(lldELF_SRC_DIR lldELF SOURCE_DIR)
   set(LLD_INCLUDE_DIR "${lldELF_SRC_DIR}/../include")
 elseif(IGC_BUILD__LLVM_PREBUILDS)
-  find_library(LLD_ELF_LIB_TMP
-    lldELF
-    PATHS "${IGC_OPTION__LLDELF_LIB_DIR}"
-    PATH_SUFFIXES "llvm-${LLVM_VERSION_MAJOR}/lib")
-  find_library(LLD_COM_LIB_TMP
-    lldCommon
-    PATHS "${IGC_OPTION__LLDELF_LIB_DIR}"
-    PATH_SUFFIXES "llvm-${LLVM_VERSION_MAJOR}/lib")
-
-  if(LLD_ELF_LIB_TMP-NOTFOUND)
-    message(FATAL_ERROR
-    "Cannot find lldELF library, please install missing library or provide the path by IGC_OPTION__LLDELF_LIB_DIR")
-  endif()
-  if(LLD_COM_LIB_TMP-NOTFOUND)
-    message(FATAL_ERROR
-    "Cannot find lldCommon library, please install missing library or provide the path by IGC_OPTION__LLDELF_LIB_DIR")
-  endif()
-  set(LLD_ELF_LIB ${LLD_ELF_LIB_TMP})
-  set(LLD_COM_LIB ${LLD_COM_LIB_TMP})
+  find_lld_library(LLD_ELF_LIB lldELF)
+  find_lld_library(LLD_COM_LIB lldCommon)
 
   find_path(
     LLD_INCLUDE_DIR
