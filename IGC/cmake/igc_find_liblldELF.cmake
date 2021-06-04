d91ecc617b0d9e9f2430286460364dbc68dd6fea
Dmitry Ryabtsev <dmitry.ryabtsev1@intel.com>
2021-06-04 18:11:11 +0000

    Add lld libraries dependencies for prebuilt llvm compilation

--- IGC/cmake/igc_find_liblldELF.cmake
+++ IGC/cmake/igc_find_liblldELF.cmake
@@ -9,26 +9,53 @@
 #   IGC_OPTION__LLDELF_LIB_DIR - Specify additional directories for searching lldELF library
 #   IGC_OPTION__LLDELF_H_DIR - Specify additional directories for searching lldELF headers
 
-function(find_lld_library VAR LIB_NAME)
-  find_library(${VAR}
+function(add_lld_library LIB_NAME)
+  find_library(${LIB_NAME}_PATH
     ${LIB_NAME}
     PATHS "${IGC_OPTION__LLDELF_LIB_DIR}"
     PATH_SUFFIXES "llvm-${LLVM_VERSION_MAJOR}/lib")
 
-  if(${VAR}-NOTFOUND)
+  if(${LIB_NAME}_PATH-NOTFOUND)
     message(FATAL_ERROR
     "Cannot find ${LIB_NAME} library, please install missing library or provide the path by IGC_OPTION__LLDELF_LIB_DIR")
   endif()
+  add_library(${LIB_NAME} UNKNOWN IMPORTED GLOBAL)
+  set_target_properties(${LIB_NAME} PROPERTIES IMPORTED_LOCATION ${${LIB_NAME}_PATH})
 endfunction()
 
 if(IGC_BUILD__LLVM_SOURCES)
-  set(LLD_ELF_LIB lldELF)
-  set(LLD_COM_LIB lldCommon)
   get_target_property(lldELF_SRC_DIR lldELF SOURCE_DIR)
   set(LLD_INCLUDE_DIR "${lldELF_SRC_DIR}/../include")
 elseif(IGC_BUILD__LLVM_PREBUILDS)
-  find_lld_library(LLD_ELF_LIB lldELF)
-  find_lld_library(LLD_COM_LIB lldCommon)
+  add_lld_library(lldELF)
+  add_lld_library(lldCommon)
+  igc_get_llvm_targets(LLD_COMMON_LLVM_DEPS
+    Codegen
+    Core
+    DebugInfoDWARF
+    Demangle
+    MC
+    Option
+    Support
+    Target)
+  igc_get_llvm_targets(LLD_ELF_LLVM_DEPS
+    ${LLVM_TARGETS_TO_BUILD}
+    BinaryFormat
+    BitWriter
+    Core
+    DebugInfoDWARF
+    Demangle
+    LTO
+    MC
+    Object
+    Option
+    Passes
+    Support)
+  target_link_libraries(lldCommon INTERFACE
+    ${LLD_COMMON_LLVM_DEPS})
+  target_link_libraries(lldELF INTERFACE
+    ${LLD_ELF_LLVM_DEPS}
+    lldCommon)
 
   find_path(
     LLD_INCLUDE_DIR
@@ -45,7 +72,6 @@ elseif(IGC_BUILD__LLVM_PREBUILDS)
 endif()
 
 list(APPEND IGC_BUILD__LLVM_LIBS_TO_LINK
-  ${LLD_ELF_LIB}
-  ${LLD_COM_LIB})
+  lldELF)
 
 include_directories(${LLD_INCLUDE_DIR})
