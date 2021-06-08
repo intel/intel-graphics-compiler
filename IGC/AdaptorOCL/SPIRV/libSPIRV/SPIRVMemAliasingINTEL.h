c0b068e3098ef78fbf915efeaba77b24298d8b21
lgotszal <lukasz.gotszald@intel.com>
2021-06-08 16:55:56 +0000

    update copyright headers in gfx-driver

--- IGC/AdaptorOCL/SPIRV/libSPIRV/SPIRVMemAliasingINTEL.h
+++ IGC/AdaptorOCL/SPIRV/libSPIRV/SPIRVMemAliasingINTEL.h
@@ -1,42 +1,21 @@
-//===- SPIRVMemAliasingINTEL.h -                               --*- C++ -*-===//
-//
-//                     The LLVM/SPIR-V Translator
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
-//
-// Copyright (c) 2021 Intel Corporation. All rights reserved.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal with the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// Redistributions of source code must retain the above copyright notice,
-// this list of conditions and the following disclaimers.
-// Redistributions in binary form must reproduce the above copyright notice,
-// this list of conditions and the following disclaimers in the documentation
-// and/or other materials provided with the distribution.
-// Neither the names of Intel Corporation, nor the names of its contributors
-// may be used to endorse or promote products derived from this Software without
-// specific prior written permission.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH
-// THE SOFTWARE.
-//
-//===----------------------------------------------------------------------===//
-/// \file
-///
-/// This file defines the memory aliasing entries defined in SPIRV spec with op
-/// codes.
-///
-//===----------------------------------------------------------------------===//
+/*========================== begin_copyright_notice ============================
+
+Copyright (C) 2021 Intel Corporation
+
+SPDX-License-Identifier: MIT
+
+============================= end_copyright_notice ===========================*/
+
+/*========================== begin_copyright_notice ============================
+
+This file is distributed under the University of Illinois Open Source License.
+See LICENSE.TXT for details.
+
+============================= end_copyright_notice ===========================*/
+
+// file SPIRVMemAliasingINTEL.h from the LLVM/SPIR-V Translator
+// This file defines the memory aliasing entries defined in SPIRV spec with op
+// codes.
 
 #ifndef SPIRV_LIBSPIRV_SPIRVMEMALIASINGINTEL_H
 #define SPIRV_LIBSPIRV_SPIRVMEMALIASINGINTEL_H
