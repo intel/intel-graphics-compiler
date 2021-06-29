a8d4752a44b21720cad06fac9ef21669abc3aadf
Pranav Tendulkar <pranav.tendulkar@intel.com>
2021-06-29 12:51:02 +0000

    Fixed state save header structure offset and bits field.
    Related-to: DOQG-2371
    
    Signed-off-by: Pranav Tendulkar <pranav.tendulkar@intel.com>

--- IGC/common/StateSaveAreaHeader.h
+++ IGC/common/StateSaveAreaHeader.h
@@ -8,66 +8,66 @@ SPDX-License-Identifier: MIT
 
 #pragma once
 
-namespace SIP
-{
-    struct alignas(4) version {
-        uint8_t major;
-        uint8_t minor;
-        uint16_t patch;
-    };
+namespace SIP {
+struct alignas(4) version {
+    uint8_t major;
+    uint8_t minor;
+    uint16_t patch;
+};
 
-    struct alignas(8) StateSaveArea {
-        char magic[8] = "tssarea";
-        uint64_t reserved1;
-        struct version version;
-        uint32_t size;
-        uint8_t reserved2[3];
-    };
+struct alignas(8) StateSaveArea {
+    char magic[8] = "tssarea";
+    uint64_t reserved1;
+    struct version version;
+    uint8_t size;
+    uint8_t reserved2[3];
+};
 
-    // Debug SIP will dump this data in debug surface.
-    struct sr_ident {
-        char magic[8] = "srmagic";
-        struct version version;
-    };
+// Debug SIP will dump this data in debug surface.
+struct sr_ident {
+    char magic[8] = "srmagic";
+    struct version version;
+};
 
-    struct alignas(8) regset_desc {
-        uint32_t offset;
-        uint16_t num;
-        uint16_t bits;
-        uint16_t bytes;
-    };
+struct alignas(8) regset_desc {
+    uint32_t offset;
+    uint16_t num;
+    uint16_t bits;
+    uint16_t bytes;
+};
 
-    struct alignas(8) intelgt_state_save_area {
-        uint32_t num_slices;
-        uint32_t num_subslices_per_slice;
-        uint32_t num_eus_per_subslice;
-        uint32_t num_threads_per_eu;
-        uint32_t state_area_offset;
-        uint32_t state_save_size;
-        uint32_t slm_area_offset;
-        uint32_t slm_bank_size;
-        uint32_t slm_bank_valid;
-        uint32_t sr_magic_offset;
-        struct regset_desc grf;
-        struct regset_desc addr;
-        struct regset_desc flag;
-        struct regset_desc emask;
-        struct regset_desc sr;
-        struct regset_desc cr;
-        struct regset_desc notification;
-        struct regset_desc tdr;
-        struct regset_desc acc;
-        struct regset_desc mme;
-        struct regset_desc ce;
-        struct regset_desc sp;
-        struct regset_desc cmd;
-        struct regset_desc tm0;
-        struct regset_desc fc;
-        struct regset_desc dbg;
-    };
+struct alignas(8) intelgt_state_save_area {
+    uint32_t num_slices;
+    uint32_t num_subslices_per_slice;
+    uint32_t num_eus_per_subslice;
+    uint32_t num_threads_per_eu;
+    uint32_t state_area_offset;
+    uint32_t state_save_size;
+    uint32_t slm_area_offset;
+    uint32_t slm_bank_size;
+    uint32_t slm_bank_valid;
+    uint32_t sr_magic_offset;
+    struct regset_desc grf;
+    struct regset_desc addr;
+    struct regset_desc flag;
+    struct regset_desc emask;
+    struct regset_desc sr;
+    struct regset_desc cr;
+    struct regset_desc notification;
+    struct regset_desc tdr;
+    struct regset_desc acc;
+    struct regset_desc mme;
+    struct regset_desc ce;
+    struct regset_desc sp;
+    struct regset_desc cmd;
+    struct regset_desc tm;
+    struct regset_desc fc;
+    struct regset_desc dbg;
+};
 
-    struct StateSaveAreaHeader {
-        struct StateSaveArea versionHeader;
-        struct intelgt_state_save_area regHeader;
-    };
-} // namespace SIP
+struct StateSaveAreaHeader {
+    struct StateSaveArea versionHeader;
+    struct intelgt_state_save_area regHeader;
+};
+
+}  // namespace SIP
--- IGC/common/SystemThread.cpp
+++ IGC/common/SystemThread.cpp
@@ -8,6 +8,7 @@ SPDX-License-Identifier: MIT
 
 #include "SystemThread.h"
 #include "common/allocator.h"
+#include "common/StateSaveAreaHeader.h"
 #include "common/igc_regkeys.hpp"
 #include "common/secure_mem.h"
 #include "common/debug/Dump.hpp"
@@ -30,7 +31,6 @@ SPDX-License-Identifier: MIT
 #include "common/SIPKernels/Gen12LPSIPCSR.h"
 #include "common/SIPKernels/Gen12LPSIPCSRDebug.h"
 #include "common/SIPKernels/Gen12LPSIPCSRDebugBindless.h"
-#include "common/SIPKernels/Gen12LPSIPCSRDebugBindlessDebugHeader.h"
 #include "Probe/Assertion.h"
 
 using namespace llvm;
@@ -38,6 +38,163 @@ using namespace USC;
 
 namespace SIP
 {
+
+// Debug surface area for all GEN8+ architectures
+struct DebugSurfaceLayout
+{
+    // The *_ALIGN fields below are padding of the SIP between
+    // the registers set.
+    static constexpr size_t GR_COUNT = 128;
+    static constexpr size_t GR_ELEMENTS = 1;
+    static constexpr size_t GR_ELEMENT_SIZE = 32;
+    static constexpr size_t GR_ALIGN = 0;
+
+    static constexpr size_t A0_COUNT = 1;
+    static constexpr size_t A0_ELEMENTS = 16;
+    static constexpr size_t A0_ELEMENT_SIZE = 2;
+    static constexpr size_t A0_ALIGN = 0;
+
+    static constexpr size_t F_COUNT = 2;
+    static constexpr size_t F_ELEMENTS = 2;
+    static constexpr size_t F_ELEMENT_SIZE = 2;
+    static constexpr size_t F_ALIGN = 20;
+
+    static constexpr size_t EXEC_MASK_COUNT = 1;
+    static constexpr size_t EXEC_MASK_ELEMENTS = 1;
+    static constexpr size_t EXEC_MASK_ELEMENT_SIZE = 4;
+    static constexpr size_t EXEC_MASK_ALIGN = 0;
+
+    static constexpr size_t SR_COUNT = 2;
+    static constexpr size_t SR_ELEMENTS = 4;
+    static constexpr size_t SR_ELEMENT_SIZE = 4;
+    static constexpr size_t SR_ALIGN = 0;
+
+    static constexpr size_t CR_COUNT = 1;
+    static constexpr size_t CR_ELEMENTS = 4;
+    static constexpr size_t CR_ELEMENT_SIZE = 4;
+    static constexpr size_t CR_ALIGN = 16;
+
+    static constexpr size_t IP_COUNT = 1;
+    static constexpr size_t IP_ELEMENTS = 1;
+    static constexpr size_t IP_ELEMENT_SIZE = 4;
+    static constexpr size_t IP_ALIGN = 28;
+
+    static constexpr size_t N_COUNT = 1;
+    static constexpr size_t N_ELEMENTS = 3;
+    static constexpr size_t N_ELEMENT_SIZE = 4;
+    static constexpr size_t N_ALIGN = 20;
+
+    static constexpr size_t TDR_COUNT = 1;
+    static constexpr size_t TDR_ELEMENTS = 8;
+    static constexpr size_t TDR_ELEMENT_SIZE = 2;
+    static constexpr size_t TDR_ALIGN = 16;
+
+    static constexpr size_t ACC_COUNT = 10;
+    static constexpr size_t ACC_ELEMENTS = 8;
+    static constexpr size_t ACC_ELEMENT_SIZE = 4;
+    static constexpr size_t ACC_ALIGN = 0;
+
+    static constexpr size_t TM_COUNT = 1;
+    static constexpr size_t TM_ELEMENTS = 4;
+    static constexpr size_t TM_ELEMENT_SIZE = 4;
+    static constexpr size_t TM_ALIGN = 16;
+
+    static constexpr size_t CE_COUNT = 1;
+    static constexpr size_t CE_ELEMENTS = 1;
+    static constexpr size_t CE_ELEMENT_SIZE = 4;
+    static constexpr size_t CE_ALIGN = 28;
+
+    static constexpr size_t SP_COUNT = 1;
+    static constexpr size_t SP_ELEMENTS = 2;
+    static constexpr size_t SP_ELEMENT_SIZE = 8;
+    static constexpr size_t SP_ALIGN = 0;
+
+    static constexpr size_t DBG_COUNT = 1;
+    static constexpr size_t DBG_ELEMENTS = 1;
+    static constexpr size_t DBG_ELEMENT_SIZE = 4;
+    static constexpr size_t DBG_ALIGN = 0;
+
+    static constexpr size_t VERSION_COUNT = 1;
+    static constexpr size_t VERSION_ELEMENTS = 1;
+    static constexpr size_t VERSION_ELEMENT_SIZE = 12;
+    static constexpr size_t VERSION_ALIGN = 0;
+
+    uint8_t grf[GR_COUNT * GR_ELEMENTS * GR_ELEMENT_SIZE + GR_ALIGN];
+    uint8_t a0[A0_COUNT * A0_ELEMENTS * A0_ELEMENT_SIZE + A0_ALIGN];
+    uint8_t f[F_COUNT * F_ELEMENTS * F_ELEMENT_SIZE + F_ALIGN];
+    uint8_t execmask[EXEC_MASK_COUNT * EXEC_MASK_ELEMENTS * EXEC_MASK_ELEMENT_SIZE + EXEC_MASK_ALIGN];
+    uint8_t sr[SR_COUNT * SR_ELEMENTS * SR_ELEMENT_SIZE + SR_ALIGN];
+    uint8_t cr[CR_COUNT * CR_ELEMENTS * CR_ELEMENT_SIZE + CR_ALIGN];
+    uint8_t ip[IP_COUNT * IP_ELEMENTS * IP_ELEMENT_SIZE + IP_ALIGN];
+    uint8_t n[N_COUNT * N_ELEMENTS * N_ELEMENT_SIZE + N_ALIGN];
+    uint8_t tdr[TDR_COUNT * TDR_ELEMENTS * TDR_ELEMENT_SIZE + TDR_ALIGN];
+    uint8_t acc[ACC_COUNT * ACC_ELEMENTS * ACC_ELEMENT_SIZE + ACC_ALIGN];
+    uint8_t tm[TM_COUNT * TM_ELEMENTS * TM_ELEMENT_SIZE + TM_ALIGN];
+    uint8_t ce[CE_COUNT * CE_ELEMENTS * CE_ELEMENT_SIZE + CE_ALIGN];
+    uint8_t sp[SP_COUNT * SP_ELEMENTS * SP_ELEMENT_SIZE + SP_ALIGN];
+    uint8_t dbg[DBG_COUNT * DBG_ELEMENTS * DBG_ELEMENT_SIZE + DBG_ALIGN];
+    uint8_t version[VERSION_COUNT * VERSION_ELEMENTS * VERSION_ELEMENT_SIZE + VERSION_ALIGN];
+};
+
+static struct StateSaveAreaHeader Gen12LPSIPCSRDebugBindlessDebugHeader =
+{
+    {"tssarea", 0, {1, 0, 0}, sizeof(StateSaveAreaHeader) / 8, {0, 0, 0}},  // versionHeader
+    {
+        // regHeader
+        0,                               // num_slices
+        0,                               // num_subslices_per_slice
+        0,                               // num_eus_per_subslice
+        0,                               // num_threads_per_eu
+        0,                               // state_area_offset
+        0x1800,                          // state_save_size
+        0,                               // slm_area_offset
+        0,                               // slm_bank_size
+        0,                               // slm_bank_valid
+        offsetof(struct DebugSurfaceLayout, version),  // sr_magic_offset
+        {offsetof(struct DebugSurfaceLayout, grf), DebugSurfaceLayout::GR_COUNT,
+         DebugSurfaceLayout::GR_ELEMENTS* DebugSurfaceLayout::GR_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::GR_ELEMENTS* DebugSurfaceLayout::GR_ELEMENT_SIZE},  // grf
+        {offsetof(struct DebugSurfaceLayout, a0), DebugSurfaceLayout::A0_COUNT,
+         DebugSurfaceLayout::A0_ELEMENTS* DebugSurfaceLayout::A0_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::A0_ELEMENTS* DebugSurfaceLayout::A0_ELEMENT_SIZE},  // addr
+        {offsetof(struct DebugSurfaceLayout, f), DebugSurfaceLayout::F_COUNT,
+         DebugSurfaceLayout::F_ELEMENTS* DebugSurfaceLayout::F_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::F_ELEMENTS* DebugSurfaceLayout::F_ELEMENT_SIZE},  // flag
+        {offsetof(struct DebugSurfaceLayout, execmask), DebugSurfaceLayout::EXEC_MASK_COUNT,
+         DebugSurfaceLayout::EXEC_MASK_ELEMENTS* DebugSurfaceLayout::EXEC_MASK_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::EXEC_MASK_ELEMENTS* DebugSurfaceLayout::EXEC_MASK_ELEMENT_SIZE},  // emask
+        {offsetof(struct DebugSurfaceLayout, sr), DebugSurfaceLayout::SR_COUNT,
+         DebugSurfaceLayout::SR_ELEMENTS* DebugSurfaceLayout::SR_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::SR_ELEMENTS* DebugSurfaceLayout::SR_ELEMENT_SIZE},  // sr
+        {offsetof(struct DebugSurfaceLayout, cr), DebugSurfaceLayout::CR_COUNT,
+         DebugSurfaceLayout::CR_ELEMENTS* DebugSurfaceLayout::CR_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::CR_ELEMENTS* DebugSurfaceLayout::CR_ELEMENT_SIZE},  // cr
+        {offsetof(struct DebugSurfaceLayout, n), DebugSurfaceLayout::N_COUNT,
+         DebugSurfaceLayout::N_ELEMENTS* DebugSurfaceLayout::N_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::N_ELEMENTS* DebugSurfaceLayout::N_ELEMENT_SIZE},  // notification
+        {offsetof(struct DebugSurfaceLayout, tdr), DebugSurfaceLayout::TDR_COUNT,
+         DebugSurfaceLayout::TDR_ELEMENTS* DebugSurfaceLayout::TDR_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::TDR_ELEMENTS* DebugSurfaceLayout::TDR_ELEMENT_SIZE},  // tdr
+        {offsetof(struct DebugSurfaceLayout, acc), DebugSurfaceLayout::ACC_COUNT,
+         DebugSurfaceLayout::ACC_ELEMENTS* DebugSurfaceLayout::ACC_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::ACC_ELEMENTS* DebugSurfaceLayout::ACC_ELEMENT_SIZE},  // acc
+        {0, 0, 0, 0},                                  // mme
+        {offsetof(struct DebugSurfaceLayout, ce), DebugSurfaceLayout::CE_COUNT,
+         DebugSurfaceLayout::CE_ELEMENTS* DebugSurfaceLayout::CE_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::CE_ELEMENTS* DebugSurfaceLayout::CE_ELEMENT_SIZE},  // ce
+        {offsetof(struct DebugSurfaceLayout, sp), DebugSurfaceLayout::SP_COUNT,
+         DebugSurfaceLayout::SP_ELEMENTS* DebugSurfaceLayout::SP_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::SP_ELEMENTS* DebugSurfaceLayout::SP_ELEMENT_SIZE},  // sp
+        {0, 0, 0, 0},                                // cmd
+        {offsetof(struct DebugSurfaceLayout, tm), DebugSurfaceLayout::TM_COUNT,
+         DebugSurfaceLayout::TM_ELEMENTS* DebugSurfaceLayout::TM_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::TM_ELEMENTS* DebugSurfaceLayout::TM_ELEMENT_SIZE},  // tm
+        {0, 0, 0, 0},                                  // FC
+        {offsetof(struct DebugSurfaceLayout, dbg), DebugSurfaceLayout::DBG_COUNT,
+         DebugSurfaceLayout::DBG_ELEMENTS* DebugSurfaceLayout::DBG_ELEMENT_SIZE * 8,
+         DebugSurfaceLayout::DBG_ELEMENTS* DebugSurfaceLayout::DBG_ELEMENT_SIZE}  // dbg
+    }
+};
 
 MemoryBuffer* LoadFile(const std::string &FileName)
 {
@@ -233,7 +390,7 @@ void populateSIPKernelInfo(const IGC::CPlatform &platform,
 
     // Match SIP alignment of debug surface
     IGC_ASSERT_MESSAGE(((Gen12LPSIPCSRDebugBindlessDebugHeader.regHeader.state_area_offset +
-	  Gen12LPSIPCSRDebugBindlessDebugHeader.versionHeader.size) / 16 == 0x15),
+	  Gen12LPSIPCSRDebugBindlessDebugHeader.versionHeader.size * 8) / 16 == 0x14),
 	"Gen12 Bindless SIP header size alignment mismatch.");
 
 }
