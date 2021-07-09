605a3ab1aa76e4a9a00679690eef522e2cde87bb
bcheng0127 <bu.qi.cheng@intel.com>
2021-07-08 21:21:30 -0700

    [Autobackout][FuncReg]Revert of change: 73811c7b5652e0b44611ba96706efeaae2766876 Changes in code.

--- IGC/Compiler/CISACodeGen/CISABuilder.cpp
+++ IGC/Compiler/CISACodeGen/CISABuilder.cpp
@@ -4329,11 +4329,6 @@ namespace IGC
             SaveOption(vISA_SWSBTokenNum, IGC_GET_FLAG_VALUE(SWSBTokenNum));
         }
 
-        if (IGC_IS_FLAG_ENABLED(EnableAccSubBeforeRA))
-        {
-            SaveOption(vISA_accSubBeforeRA, true);
-        }
-
         if (IGC_IS_FLAG_ENABLED(EnableAccSub))
         {
             SaveOption(vISA_accSubstitution, true);
--- IGC/common/igc_flags.h
+++ IGC/common/igc_flags.h
@@ -443,7 +443,6 @@ DECLARE_IGC_REGKEY(bool, SkipTREarlyExitCheck, false, "Skip SIMD16 early exit ch
 DECLARE_IGC_REGKEY(bool, EnableTCSHWBarriers, false,  "Enable TCS pass with HW barriers support. Default TCS pass is TCS pass with multiple continuation functions.", false)
 DECLARE_IGC_REGKEY(bool, ForceMCFBarriers, false,  "Force TCS pass with MCF (SW) barriers support. Default TCS pass is TCS pass with multiple continuation functions.", false)
 DECLARE_IGC_REGKEY(bool, EnableAccSub, true, "Enable accumulator substitution", false)
-DECLARE_IGC_REGKEY(bool, EnableAccSubBeforeRA, true, "Enable accumulator substitution", false)
 DECLARE_IGC_REGKEY(DWORD, NumGeneralAcc, 0, "set the number [1-8] of general acc for accumulator substitution. 0 means using the platform-default value", false)
 DECLARE_IGC_REGKEY(bool, ForceSWCoalescingOfAtomicCounter, false, "Force software coalescing of atomic counter", false)
 DECLARE_IGC_REGKEY(bool, ForceMixMode, false, "force enable mix mode even on platforms that do not support it", false)
