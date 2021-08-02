f1411494aeacdfa5aae7f8c670ed1ab496ef111a
Mateusz Borzyszkowski <mateusz.borzyszkowski@intel.com>
2021-08-02 15:39:42 +0000

    Replace invalid characters. Try2
    LLVM sometimes creates the function with characters which are incorrect for vISA (e.g. name with dot).
    The change checks LLVM function name and replaces chars to underscore.

--- IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
+++ IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
@@ -92,6 +92,7 @@ SPDX-License-Identifier: MIT
 #if defined(_DEBUG) && !defined(ANDROID)
 #include "Compiler/VerificationPass.hpp"
 #endif
+#include "Compiler/FixInvalidFuncNamePass.hpp"
 #include "Compiler/LegalizationPass.hpp"
 #include "Compiler/LowPrecisionOptPass.hpp"
 #include "Compiler/WorkaroundAnalysisPass.h"
@@ -318,6 +319,7 @@ static void AddAnalysisPasses(CodeGenContext& ctx, IGCPassManager& mpm)
     }
 
     mpm.add(new Layout());
+    mpm.add(createFixInvalidFuncNamePass());
 
     mpm.add(createTimeStatsCounterPass(&ctx, TIME_CG_Analysis, STATS_COUNTER_END));
 
