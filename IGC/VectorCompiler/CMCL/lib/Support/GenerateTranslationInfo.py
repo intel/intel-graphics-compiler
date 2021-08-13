800d23e2c88bc5e20046d6be87f6c171363468a7
lgotszal <lukasz.gotszald@intel.com>
2021-08-13 07:38:03 +0000

    update copyright headers in IGC
    update copyright headers in IGC

--- IGC/VectorCompiler/CMCL/lib/Support/GenerateTranslationInfo.py
+++ IGC/VectorCompiler/CMCL/lib/Support/GenerateTranslationInfo.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-#=========================== begin_copyright_notice ============================
+# ========================== begin_copyright_notice ============================
 #
 # Copyright (C) 2021 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 #
-#============================ end_copyright_notice =============================
+# =========================== end_copyright_notice =============================
 
 import argparse
 import json
