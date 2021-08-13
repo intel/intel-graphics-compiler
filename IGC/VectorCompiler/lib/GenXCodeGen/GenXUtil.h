8b695b9d305ce5ab70d8edd45e224237a8f5b5f5
Sergei Kachkov <sergei.kachkov@intel.com>
2021-08-13 09:31:54 -0700

    [Autobackout][FuncReg]Revert of change: 2b32be4def36fc0061729f9655f9cccecf91d7bc  Move functions to separate addrspace(5)
    Since size of function pointer is fixed and can be different from size
    of generic pointer, we separate them to change data layout of function
    pointer to 32-bit size

--- IGC/VectorCompiler/lib/GenXCodeGen/GenXUtil.h
+++ IGC/VectorCompiler/lib/GenXCodeGen/GenXUtil.h
@@ -69,7 +69,16 @@ template <typename T> inline T roundedVal(T Val, T RoundUp) {
 template<unsigned UnitBitSize = 1>
 unsigned getTypeSize(Type *Ty, const DataLayout *DL = nullptr) {
   IGC_ASSERT(Ty && Ty->isSized());
-  unsigned BitTypeSize = DL ? DL->getTypeSizeInBits(Ty) : Ty->getPrimitiveSizeInBits();
+  unsigned BitTypeSize = 0;
+  // FIXME: it's better to use DataLayout to get function pointers type size, so
+  // we should remove it when such pointers will be in separate address space.
+  // Size of function pointers is always 32 bit.
+  if (auto PT = dyn_cast<PointerType>(Ty->getScalarType());
+      PT && PT->getPointerElementType()->isFunctionTy()) {
+    BitTypeSize = 32 * isa<VectorType>(Ty) ? cast<VectorType>(Ty)->getNumElements()
+                                           : 1;
+  } else
+    BitTypeSize = DL ? DL->getTypeSizeInBits(Ty) : Ty->getPrimitiveSizeInBits();
   IGC_ASSERT_MESSAGE(BitTypeSize, "Consider using DataLayout for retrieving this type size");
   return 1 + (BitTypeSize - 1) / UnitBitSize;
 }
