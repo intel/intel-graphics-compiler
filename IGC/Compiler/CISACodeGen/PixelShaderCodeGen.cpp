8a02a6d6b61a2116de93724914ec89b87577df35
Hou-Jen Ko <hou-jen.ko@intel.com>
2021-08-16 21:33:04 +0000

    Cleanup unnecessary R1Lo
    Remove workaround from the code patching

--- IGC/Compiler/CISACodeGen/PixelShaderCodeGen.cpp
+++ IGC/Compiler/CISACodeGen/PixelShaderCodeGen.cpp
@@ -145,10 +145,6 @@ void CPixelShader::AllocatePSPayload()
         for (uint i = 0; i < GetR1()->GetNumberInstance(); i++)
         {
             AllocateInput(GetR1(), offset, i, forceLiveOut);
-            for (auto R1Lo: GetR1Lo()) {
-                AllocateInput(R1Lo, offset, i, forceLiveOut);
-            }
-
             offset += getGRFSize();
         }
     }
