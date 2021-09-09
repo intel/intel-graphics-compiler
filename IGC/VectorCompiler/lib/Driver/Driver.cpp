82f8c79345b18059edd3c78f2ac7641cd9abf33e
Aleksander Us <aleksander.us@intel.com>
2021-09-09 12:14:26 +0000

    Make -cmc option unknown for SPIRV path
    Report -cmc option as unknown in preparation for complete removal of
    deprecated compilation mode.

--- IGC/VectorCompiler/lib/Driver/Driver.cpp
+++ IGC/VectorCompiler/lib/Driver/Driver.cpp
@@ -532,19 +532,11 @@ parseApiOptions(StringSaver &Saver, StringRef ApiOptions, bool IsStrictMode) {
     return parseOptions<ID, OPT_UNKNOWN, OPT_INPUT>(Argv, FlagsToInclude,
                                                     Options, IsStrictMode);
   }
-  // Deprecated -cmc parsing just for compatibility.
+  // Treat -cmc option as unknown.
   const std::string IgcmcOptName =
       Options.getOption(OPT_igcmc).getPrefixedName();
-  if (HasOption(IgcmcOptName)) {
-    llvm::errs()
-        << "'" << IgcmcOptName
-        << "' option is deprecated and will be removed in the future release. "
-           "Use -vc-codegen instead for compiling from SPIRV.\n";
-    const unsigned FlagsToInclude =
-        IGC::options::IgcmcApiOption | IGC::options::IGCApiOption;
-    return parseOptions<ID, OPT_UNKNOWN, OPT_INPUT>(Argv, FlagsToInclude,
-                                                    Options, IsStrictMode);
-  }
+  if (HasOption(IgcmcOptName))
+    return make_error<vc::OptionError>(IgcmcOptName, /*IsInternal=*/false);
 
   return make_error<vc::NotVCError>();
 }
