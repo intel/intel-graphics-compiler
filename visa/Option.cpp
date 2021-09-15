0404b07d156ce2c841c93c27023dced0d2bc6275
Gu, Junjie <junjie.gu@intel.com>
2021-09-15 19:59:58 +0000

    Minor refactoring
    Added names for some of visa unnamed flags.
    Make sure the options list printout in .asm does not have unamed flags.

--- visa/Option.cpp
+++ visa/Option.cpp
@@ -538,7 +538,8 @@ std::string Options::getFullArgString() const
     // This is for igc. When igc invokes vISA, it sets options
     // via setOption() api instead of options string, thus leave
     // argString empty. Here, we re-generate this options strings
-    // (This is for debugging)
+    //
+    // If a flag has no name (it happens), skip it!
     for (int i = vISA_OPTIONS_UNINIT + 1; i < vISA_NUM_OPTIONS; ++i)
     {
         vISAOptions o = (vISAOptions)i;
@@ -551,30 +552,53 @@ std::string Options::getFullArgString() const
                 {
                     // Boolean option means the reverse of the default!
                     // (Probably should avoid such reverse handling)
-                    args << m_vISAOptions.getArgStr(o) << " ";
+                    const char* argName = m_vISAOptions.getArgStr(o);
+                    if (argName && argName[0] != 0)
+                    {
+                        args << argName << " ";
+                    }
                 }
                 break;
             case ET_INT32:
-                args << m_vISAOptions.getArgStr(o) << " "
-                    << m_vISAOptions.getUint32(o) << " ";
+            {
+                const char* argName = m_vISAOptions.getArgStr(o);
+                if (argName && argName[0] != 0)
+                {
+                    args << argName << " " << m_vISAOptions.getUint32(o) << " ";
+                }
                 break;
+            }
             case ET_INT64:
-                args << m_vISAOptions.getArgStr(o) << " "
-                    << m_vISAOptions.getUint64(o) << " ";
+            {
+                const char* argName = m_vISAOptions.getArgStr(o);
+                if (argName && argName[0] != 0)
+                {
+                    args << argName << " " << m_vISAOptions.getUint64(o) << " ";
+                }
                 break;
+            }
             case ET_2xINT32:
             {
                 uint32_t lo32, hi32;
                 uint64_t val = m_vISAOptions.getUint64(o);
                 lo32 = (uint32_t)val;
                 hi32 = (uint32_t)(val >> 32);
-                args << m_vISAOptions.getArgStr(o) << " "
-                    << hi32 << " " << lo32 << " ";
+
+                const char* argName = m_vISAOptions.getArgStr(o);
+                if (argName && argName[0] != 0)
+                {
+                    args << argName << " " << hi32 << " " << lo32 << " ";
+                }
             }
             break;
             case ET_CSTR:
             {
-                args << m_vISAOptions.getArgStr(o) << " ";
+                const char* argName = m_vISAOptions.getArgStr(o);
+                if (!argName || argName[0] == 0)
+                {
+                    break;
+                }
+                args << argName << " ";
                 const char *sval = m_vISAOptions.getCstr(o);
                 // Careful not to emit empty strings
                 // ... -string_opt   -next_opt
