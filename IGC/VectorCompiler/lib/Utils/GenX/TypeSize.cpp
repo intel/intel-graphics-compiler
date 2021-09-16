724e7e78fd970ad8b57c92ca9a40424bc2e85719
Anatoly Parshintsev <anatoly.parshintsev@intel.com>
2021-09-16 14:24:25 +0000

    added missing license
    added missing license

--- IGC/VectorCompiler/lib/Utils/GenX/TypeSize.cpp
+++ IGC/VectorCompiler/lib/Utils/GenX/TypeSize.cpp
@@ -1,3 +1,11 @@
+/*========================== begin_copyright_notice ============================
+
+Copyright (C) 2021 Intel Corporation
+
+SPDX-License-Identifier: MIT
+
+============================= end_copyright_notice ===========================*/
+
 #include "vc/Utils/GenX/TypeSize.h"
 
 vc::TypeSizeWrapper vc::getTypeSize(llvm::Type *Ty,
