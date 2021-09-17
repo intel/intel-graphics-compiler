45174cd17a7bc848fb1e3e38eac74544ff90afe5
Gang Y Chen <gang.y.chen@intel.com>
2021-09-17 01:16:14 -0700

    [Autobackout][FuncReg]Revert of change: ff7cbee12384ab3e80b9a9054c9a480eec6417b0  remove dead load after type legalization
    remove dead load after type legalization

--- IGC/AdaptorCommon/TypesLegalizationPass.cpp
+++ IGC/AdaptorCommon/TypesLegalizationPass.cpp
@@ -7,7 +7,6 @@ SPDX-License-Identifier: MIT
 ============================= end_copyright_notice ===========================*/
 
 #include "llvmWrapper/Support/Alignment.h"
-#include <llvm/IR/InstIterator.h>
 #include "TypesLegalizationPass.hpp"
 #include "../Compiler/IGCPassSupport.h"
 #include "Probe/Assertion.h"
@@ -355,21 +354,5 @@ bool TypesLegalizationPass::runOnFunction( Function &function ) {
   m_StoreInst.clear();
   m_ExtractValueInst.clear();
   m_PhiNodes.clear();
-  // type-legalization may create dead load, clean up those loads
-  SmallVector<LoadInst*, 32> DeadLoads;
-  for (inst_iterator I = inst_begin(function), E = inst_end(function);
-      I != E; ++I) {
-    Instruction* inst = &*I;
-    if (LoadInst* Load = dyn_cast<LoadInst>(inst)) {
-      if (!Load->isVolatile() && Load->use_empty() &&
-           Load->getType()->isAggregateType()) {
-        DeadLoads.push_back(Load);
-      }
-    }
-  }
-  for (LoadInst* ld : DeadLoads) {
-    ld->eraseFromParent();
-  }
-
   return shaderModified;
 }
