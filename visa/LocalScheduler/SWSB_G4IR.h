5fdf340b473de1f7fc5a593b15943632eed51545
bcheng0127 <bu.qi.cheng@intel.com>
2021-09-22 18:13:12 +0000

    Fix SWSB distance type to get more accurate dependence info.
    Fix SWSB distance type to get more accurate dependence info.

--- visa/LocalScheduler/SWSB_G4IR.h
+++ visa/LocalScheduler/SWSB_G4IR.h
@@ -787,7 +787,7 @@ namespace vISA
                 if (!builder.getOptions()->getOption(vISA_disableRegDistDep) &&
                     !builder.hasRegDistDepIssue())
                 {
-                instVec.front()->setOperandTypeIndicated(sameOperandType);
+                    instVec.front()->setOperandTypeIndicated(sameOperandType);
                 }
 
                 //Multiple dependences
@@ -799,15 +799,8 @@ namespace vISA
                         {
                             assert(!GetInstruction()->isSend()); //Send operand has no type, sameOperandType is always false
 
-                            if (GetInstruction()->isDpas()) //For DPAS, we also put the distance dependence into a sync instruction.
-                            {
-                                setAccurateDistType(depPipe);
-                                instVec.front()->setOperandTypeIndicated(false); //DPAS distance will be in sync inst
-                            }
-                            else //For math and other ALU instructions
-                            {
-                                instVec.front()->setDistanceTypeXe(G4_INST::DistanceType::DISTALL);
-                            }
+                            setAccurateDistType(depPipe);
+                            instVec.front()->setOperandTypeIndicated(false);
                         }
                         else // For send, we will set it to DISTALL if there are multiple dependences. For non-send, DIST
                         {
