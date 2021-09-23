3b10634b71b3da5d1f11577405b54d6fddf4fe95
Grzywacz, Maciej <maciej.grzywacz@intel.com>
2021-09-23 13:25:25 +0000

    Stateless scratch space for OGL
    Implement logic to detect if stateless scratch space pointer is needed
    (per thread scratch space size > 256kB), and set according information
    in compiler output and payload.

--- IGC/Compiler/CISACodeGen/CShader.cpp
+++ IGC/Compiler/CISACodeGen/CShader.cpp
@@ -3458,7 +3458,9 @@ void CShader::PackAndCopyVariable(
 
 bool CShader::CompileSIMDSizeInCommon(SIMDMode simdMode)
 {
-    bool ret = (m_ScratchSpaceSize <= m_ctx->platform.maxPerThreadScratchSpace());
+    bool ret = ((m_ScratchSpaceSize <= m_ctx->platform.maxPerThreadScratchSpace()) ||
+        m_ctx->m_DriverInfo.supportsStatelessSpacePrivateMemory());
+
     m_simdProgram.setScratchSpaceUsedByShader(m_ScratchSpaceSize);
     if (m_ctx->platform.hasScratchSurface() && m_ctx->m_DriverInfo.supportsSeparatingSpillAndPrivateScratchMemorySpace()) {
         ret = (m_simdProgram.getScratchSpaceUsageInSlot0() <= m_ctx->platform.maxPerThreadScratchSpace());
--- IGC/Compiler/Optimizer/OpenCLPasses/PrivateMemory/PrivateMemoryToSLM.cpp
+++ IGC/Compiler/Optimizer/OpenCLPasses/PrivateMemory/PrivateMemoryToSLM.cpp
@@ -12,7 +12,7 @@ SPDX-License-Identifier: MIT
 #include "Compiler/IGCPassSupport.h"
 #include "Compiler/CodeGenPublic.h"
 #include "Compiler/CISACodeGen/GenCodeGenModule.h"
-#include "Compiler/Optimizer/OpenCLPasses/PrivateMemory/PrivateMemoryResolution.hpp"
+#include "Compiler/ModuleAllocaAnalysis.hpp"
 
 #include "common/debug/Debug.hpp"
 #include "llvmWrapper/IR/DataLayout.h"
@@ -31,6 +31,7 @@ using namespace IGC::Debug;
 #define PASS_CFG_ONLY true
 #define PASS_ANALYSIS false
 IGC_INITIALIZE_PASS_BEGIN(PrivateMemoryToSLM, PASS_FLAG, PASS_DESCRIPTION, PASS_CFG_ONLY, PASS_ANALYSIS)
+IGC_INITIALIZE_PASS_DEPENDENCY(ModuleAllocaAnalysis)
 IGC_INITIALIZE_PASS_END(PrivateMemoryToSLM, PASS_FLAG, PASS_DESCRIPTION, PASS_CFG_ONLY, PASS_ANALYSIS)
 
 namespace IGC
@@ -135,10 +136,7 @@ namespace IGC
         auto* CodeGenCtx = getAnalysis<CodeGenContextWrapper>().getCodeGenContext();
         auto* MD = getAnalysis<MetaDataUtilsWrapper>().getMetaDataUtils();
         auto* ModuleMD = getAnalysis<MetaDataUtilsWrapper>().getModuleMetaData();
-        auto* FGA = getAnalysisIfAvailable<GenXFunctionGroupAnalysis>();
         auto DL = M.getDataLayout();
-
-        ModuleAllocaInfo allocaInfo(&M, &DL, FGA);
 
         bool modified = false;
         for (Module::iterator I = M.begin(); I != M.end(); ++I)
@@ -150,7 +148,7 @@ namespace IGC
                 continue;
             }
 
-            SmallVector<AllocaInst*, 8>& allocaInsts = allocaInfo.getAllocaInsts(F);
+            SmallVector<AllocaInst*, 8>& allocaInsts = getAnalysis<ModuleAllocaAnalysis>().getAllocaInsts(F);
 
             if (allocaInsts.empty())
             {
