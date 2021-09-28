d0266eca9f45b66446502a304a39526923e28d1e
Aleksander Us <aleksander.us@intel.com>
2021-09-28 12:38:48 +0000

    Improve LLVM patcher to handle minor versions
    Rewrite LLVM patching using python. Implement patching system that
    allows multiple minor versions. Current scheme will apply patches
    starting from most suitable minor/patch version to lowest available
    with same major component. Patches with duplicate names will be taken
    only once to allow overriding for newer versions. For example, opt
    CMakeLists.txt requires different patches for 10.0.0 and 10.0.1
    versions. New scheme allows to handle such cases.

---
+++ external/llvm/apply_patches.py
@@ -0,0 +1,127 @@
+#=========================== begin_copyright_notice ============================
+#
+# Copyright (C) 2021 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+#
+#============================ end_copyright_notice =============================
+
+import argparse
+import os
+from functools import total_ordering
+from subprocess import check_call
+
+parser = argparse.ArgumentParser(description='Apply LLVM patches for IGC')
+parser.add_argument('--llvm-version', required=True,
+                    help='LLVM version for patching')
+parser.add_argument('--llvm-project-dir', required=True,
+                    help='LLVM project copied sources')
+parser.add_argument('--patches-dir', required=True,
+                    help='Directory with patches')
+parser.add_argument('--patch-executable', required=True,
+                    help='Path to patch program')
+parser.add_argument('--dry-run', action='store_true',
+                    help='Only print list of patches that will be applied')
+
+args = parser.parse_args()
+
+def get_dir_patches(ver_dir):
+    """Return list of patches from given version directory.
+
+    Collect patches from 'patches' subdirectory and return them
+    as a list of DirEntry objects.
+    """
+    patches_dir = os.path.join(args.patches_dir, ver_dir)
+    patches = []
+    ext_patches = os.path.join(patches_dir, 'patches_external')
+    if os.path.exists(ext_patches):
+        patches.extend(os.scandir(ext_patches))
+    return patches
+
+def apply_patch(patch_path):
+    """Apply patch to llvm project."""
+    rel_path = os.path.relpath(patch_path.path, args.patches_dir)
+    print('Applying {} file'.format(rel_path))
+    if args.dry_run:
+        return
+    check_call([args.patch_executable,
+                '-t', '-s', '-N',
+                '-d', args.llvm_project_dir,
+                '-p1', '-i', patch_path])
+
+def split_ver_str(ver_str):
+    """Split version string into numeric components.
+
+    Return list of components as numbers additionally checking
+    that all components are correct (i.e. can be converted to numbers).
+    """
+    ver_list = []
+    for c in ver_str.split('.')[0:3]:
+        if not c.isdecimal():
+            raise RuntimeError("Malformed version string '{}'. "
+                               "Component '{}' is not an integer."
+                               .format(ver_str, c))
+        ver_list.append(int(c))
+
+    return ver_list
+
+def get_ver_component(ver_list, idx):
+    """Get version component from components list.
+
+    Return 0 for components out of range as default.
+    """
+    if idx < len(ver_list):
+        return ver_list[idx]
+    return 0
+
+@total_ordering
+class Version:
+    """Simple wrapper around three-component version.
+
+    This class provides convenient accessors to version components
+    represented by decimal numbers. Suitable for LLVM version in IGC.
+    """
+
+    def __init__(self, ver_str):
+        ver_list = split_ver_str(ver_str)
+        ver_tuple = tuple(get_ver_component(ver_list, idx) for idx in (0, 1, 2))
+        self.major = ver_tuple[0]
+        self.minor = ver_tuple[1]
+        self.patch = ver_tuple[2]
+
+    def as_tuple(self):
+        return (self.major, self.minor, self.patch)
+
+    def __repr__(self):
+        return '{}.{}.{}'.format(self.major, self.minor, self.patch)
+
+    def __eq__(self, other):
+        return self.as_tuple() == other.as_tuple()
+
+    def __lt__(self, other):
+        return self.as_tuple() < other.as_tuple()
+
+required_ver = Version(args.llvm_version)
+
+# Create mapping of version to directory with suitable
+# version range sorted in descending order.
+# All directories with same major version as required are suitable.
+patches_dir = os.listdir(args.patches_dir)
+ver_to_dir = [(Version(v), v) for v in patches_dir]
+ver_to_dir = filter(lambda tpl: tpl[0].major == required_ver.major, ver_to_dir)
+ver_to_dir = filter(lambda tpl: tpl[0] <= required_ver, ver_to_dir)
+ver_to_dir = sorted(ver_to_dir, key=lambda tpl: tpl[0], reverse=True)
+
+# Merge patches from suitable directories taking only patches from
+# newest versions if same patch is present in several directries.
+patches = {}
+for _, d in ver_to_dir:
+    dir_patches = get_dir_patches(d)
+    dir_patches = {d.name : d for d in dir_patches}
+    dir_patches.update(patches)
+    patches = dir_patches
+patches = list(patches.values())
+patches.sort(key=lambda p: p.name)
+
+for patch in patches:
+    apply_patch(patch)
--- external/llvm/llvm_patch_sources.cmake
+++ external/llvm/llvm_patch_sources.cmake
@@ -18,49 +18,17 @@ if(EXISTS "${IGC_LLVM_SOURCE_DIR}")
   return()
 endif()
 
-file(GLOB dirsPatch RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/releases ${CMAKE_CURRENT_SOURCE_DIR}/releases/*)
-list(SORT dirsPatch)
-list(REVERSE dirsPatch)
-
-string(REGEX MATCH "[0-9]+" LLVM_VERSION_MAJOR ${IGC_OPTION__LLVM_PREFERRED_VERSION})
-
-# Customization patches will be applied if present.
-foreach(dirPatch ${dirsPatch})
-  string(REGEX MATCH "[0-9]+" LLVM_VER_MAJOR_FOLDER ${dirPatch})
-  if(${LLVM_VERSION_MAJOR} MATCHES ${LLVM_VER_MAJOR_FOLDER})
-    set(DIR_WITH_PATCHES ${dirPatch})
-    break()
-  endif()
-endforeach()
-
-if(NOT DIR_WITH_PATCHES)
-  message(STATUS "[LLVM_PATCHER] : No patches found, using stock llvm")
-  set(IGC_LLVM_SOURCE_DIR ${IGC_OPTION__LLVM_SOURCES_DIR}/llvm)
-  return()
-endif()
-
 # Copy stock LLVM sources to IGC_LLVM_SOURCE_DIR to apply patches.
-message(STATUS "[LLVM_PATCHER] : Copying stock LLVM sources ${IGC_OPTION__LLVM_SOURCES_DIR} to ${IGC_LLVM_SOURCE_DIR}")
+message(STATUS "[LLVM] : Copying stock LLVM sources ${IGC_OPTION__LLVM_SOURCES_DIR} to ${IGC_LLVM_SOURCE_DIR}")
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${IGC_OPTION__LLVM_SOURCES_DIR}/.git ${IGC_LLVM_WORKSPACE_SRC}/.git)
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${IGC_OPTION__LLVM_SOURCES_DIR}/llvm ${IGC_LLVM_SOURCE_DIR})
 
-message(STATUS "[LLVM_PATCHER] : Applying patches for LLVM from version ${DIR_WITH_PATCHES}")
+message(STATUS "[LLVM] : Applying patches for LLVM from version ${DIR_WITH_PATCHES}")
 
-file(GLOB LLVM_PATCH_FILES
-  "${CMAKE_CURRENT_SOURCE_DIR}/releases/${DIR_WITH_PATCHES}/patches_external/*.patch"
+execute_process(COMMAND
+  ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/apply_patches.py
+  --llvm-version ${IGC_OPTION__LLVM_PREFERRED_VERSION}
+  --llvm-project-dir ${IGC_LLVM_WORKSPACE_SRC}
+  --patches-dir ${CMAKE_CURRENT_SOURCE_DIR}/releases
+  --patch-executable ${Patch_EXECUTABLE}
   )
-list(SORT LLVM_PATCH_FILES)
-# Apply customization patches if any.
-foreach(patch_file ${LLVM_PATCH_FILES})
-  message(STATUS "[LLVM_PATCHER] : Apply ${patch_file} file")
-  execute_process(COMMAND ${Patch_EXECUTABLE} -d ${IGC_LLVM_WORKSPACE_SRC} -p1 -i ${patch_file}
-  RESULT_VARIABLE rv
-  OUTPUT_VARIABLE ov
-  ERROR_VARIABLE ev)
-  if(NOT rv EQUAL 0)
-    message(FATAL_ERROR "[LLVM_PATCHER] : error: applying patch '${patch_file}' failed.\nstdout:\n${ov}\nstderr:${ev}"
-      "Probably you set incorrent version of LLVM (use IGC_OPTION__LLVM_PREFERRED_VERSION option),\n"
-      "or passed incorrect sources with different version (use IGC_OPTION__LLVM_SOURCES_DIR option)."
-      )
-  endif()
-endforeach()
