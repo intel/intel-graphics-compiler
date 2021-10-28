8605f70dc71036ecc1e39de15625b2a23544e767
Viacheslav Rybalov <viacheslav.g.rybalov@intel.com>
2021-10-28 14:16:14 +0000

    Fixing implicit args Metadata
    Implicit args are not added in non-entry functions
    if EnableImplicitArgAsIntrinsic is set, Metadata must
    be consistent.

--- IGC/Compiler/Optimizer/OpenCLPasses/WIFuncs/WIFuncsAnalysis.cpp
+++ IGC/Compiler/Optimizer/OpenCLPasses/WIFuncs/WIFuncsAnalysis.cpp
@@ -61,6 +61,7 @@ bool WIFuncsAnalysis::runOnModule(Module& M)
     {
         Function* pFunc = &(*I);
         if (pFunc->isDeclaration()) continue;
+        if (!isEntryFunc(m_pMDUtils, pFunc) && IGC_IS_FLAG_ENABLED(EnableImplicitArgAsIntrinsic)) continue;
         runOnFunction(*pFunc);
     }
 
