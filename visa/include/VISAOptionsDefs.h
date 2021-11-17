061520509576f0adf4f61d0c701e8dbdc62591a6
bcheng0127 <bu.qi.cheng@intel.com>
2021-11-17 22:51:30 +0000

    Changes in code.

--- visa/include/VISAOptionsDefs.h
+++ visa/include/VISAOptionsDefs.h
@@ -86,7 +86,6 @@ DEF_VISA_OPTION(vISA_doAccSubAfterSchedule, ET_BOOL, "-accSubPostSchedule",    U
 DEF_VISA_OPTION(vISA_localizationForAccSub, ET_BOOL, "-localizeForACC",    UNUSED, false)
 DEF_VISA_OPTION(vISA_mathAccSub, ET_BOOL, "-mathAccSub",    UNUSED, false)
 DEF_VISA_OPTION(vISA_src2AccSub, ET_BOOL, "-src2AccSub",    UNUSED, false)
-DEF_VISA_OPTION(vISA_relaxAccSubstitution,       ET_BOOL, "-relaxAccSub",          UNUSED, false)
 DEF_VISA_OPTION(vISA_hasDoubleAcc, ET_BOOL, "-hasDoubleAcc",    UNUSED, false)
 DEF_VISA_OPTION(vISA_ifCvt,                 ET_BOOL, "-noifcvt",     UNUSED, true)
 DEF_VISA_OPTION(vISA_RegSharingHeuristics,  ET_BOOL, "-regSharingHeuristics", UNUSED, false)
