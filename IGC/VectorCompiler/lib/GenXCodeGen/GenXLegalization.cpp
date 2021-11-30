b93e9db99cdd04ac463165cf1b65408aca9fb736
Sergei Kachkov <sergei.kachkov@intel.com>
2021-11-30 14:32:49 +0000

    Fold bitcasts to rdregion/wrregion by changing region parameters
    This transformation reorders rdregion->bitcast to bitcast->rdregion and
    bitcast->wrregion to wrregion->bitcast, so they can be baled later.

--- IGC/VectorCompiler/lib/GenXCodeGen/GenXLegalization.cpp
+++ IGC/VectorCompiler/lib/GenXCodeGen/GenXLegalization.cpp
@@ -329,7 +329,7 @@ private:
     Fixed4 = nullptr;
     TwiceWidth = nullptr;
   }
-  unsigned getExecSizeAllowedBits(Instruction *Inst);
+  unsigned adjustTwiceWidthOrFixed4(const Bale &B);
   bool checkIfLongLongSupportNeeded(Instruction *Inst) const;
   bool processInst(Instruction *Inst);
   bool processBale(Instruction *InsertBefore);
@@ -497,84 +497,30 @@ bool GenXLegalization::runOnFunction(Function &F) {
   return true;
 }
 
-/***********************************************************************
- * getExecSizeAllowedBits : get bitmap of allowed execution sizes
- *
- * Enter:   Inst = main instruction of bale
- *
- * Return:  bit N set if execution size 1<<N is allowed
- *
- * Most instructions have a minimum width of 1. But some instructions,
- * such as dp4 and lrp, have a minimum width of 4, and legalization cannot
- * allow such an instruction to be split to a smaller width.
- *
- * This also sets up fields in GenXLegalization: Fixed4 is set to a use
- * that is a FIXED4 operand, and TwiceWidth is set to a use that is a
- * TWICEWIDTH operand.
- */
-unsigned GenXLegalization::getExecSizeAllowedBits(Instruction *Inst) {
-
-  switch (Inst->getOpcode()) {
-  default:
-    break;
-  case BinaryOperator::SDiv:
-  case BinaryOperator::UDiv:
-  case BinaryOperator::SRem:
-  case BinaryOperator::URem:
-    // If integer division IS supported.
-    //   Set maximum SIMD width to 16:
-    //      Recent HW does not support SIMD16/SIMD32 division, however,
-    //      finalizer splits such SIMD16 operations and we piggy-back
-    //      on this behavior.
-    // If integer division IS NOT supported.
-    //   The expectation is for GenXEmulate pass to replace such operations
-    //   with emulation routines (which has no restriction on SIMD width)
-    return ST->hasIntDivRem32() ? 0x1f : 0x3f;
-  }
-
-  unsigned ID = GenXIntrinsic::getAnyIntrinsicID(Inst);
-  switch (ID) {
-    case GenXIntrinsic::genx_ssmad:
-    case GenXIntrinsic::genx_sumad:
-    case GenXIntrinsic::genx_usmad:
-    case GenXIntrinsic::genx_uumad:
-    case GenXIntrinsic::genx_ssmad_sat:
-    case GenXIntrinsic::genx_sumad_sat:
-    case GenXIntrinsic::genx_usmad_sat:
-    case GenXIntrinsic::genx_uumad_sat:
-    case Intrinsic::fma:
-      // Do not emit simd32 mad for pre-ICLLP.
-      return ST->isICLLPplus() ? 0x3f : 0x1f;
-    default:
-      break;
-  }
-
-  if (CallInst *CI = dyn_cast<CallInst>(Inst)) {
-    // We have a call instruction, so we can assume it is an intrinsic since
-    // otherwise processInst would not have got as far as calling us as
-    // a non-intrinsic call forces isSplittable() to be false.
-    auto CalledF = CI->getCalledFunction();
-    IGC_ASSERT(CalledF);
-    GenXIntrinsicInfo II(GenXIntrinsic::getAnyIntrinsicID(CalledF));
-    // While we have the intrinsic info, we also spot whether we have a FIXED4
-    // operand and/or a TWICEWIDTH operand.
+unsigned GenXLegalization::adjustTwiceWidthOrFixed4(const Bale &B) {
+  auto Main = B.getMainInst();
+  if (!Main)
+    return 0x3f;
+  // Spot whether we have a FIXED operand and/or a TWICEWIDTH operand.
+  if (GenXIntrinsic::isGenXIntrinsic(Main->Inst)) {
+    GenXIntrinsicInfo II(GenXIntrinsic::getAnyIntrinsicID(Main->Inst));
     for (auto i = II.begin(), e = II.end(); i != e; ++i) {
       auto ArgInfo = *i;
-      if (ArgInfo.isArgOrRet()) {
-        switch (ArgInfo.getRestriction()) {
-        case GenXIntrinsicInfo::FIXED4:
-          Fixed4 = &CI->getOperandUse(ArgInfo.getArgIdx());
-          break;
-        case GenXIntrinsicInfo::TWICEWIDTH:
-          TwiceWidth = &CI->getOperandUse(ArgInfo.getArgIdx());
-          break;
-        }
+      if (!ArgInfo.isArgOrRet())
+       continue;
+      switch (ArgInfo.getRestriction()) {
+      case GenXIntrinsicInfo::FIXED4:
+        Fixed4 = &Main->Inst->getOperandUse(ArgInfo.getArgIdx());
+        break;
+      case GenXIntrinsicInfo::TWICEWIDTH:
+        TwiceWidth = &Main->Inst->getOperandUse(ArgInfo.getArgIdx());
+        break;
       }
     }
-    return II.getExecSizeAllowedBits();
   }
-  return 0x3f;
+  return genx::getExecSizeAllowedBits(Main->Inst, ST);
 }
+
 /***********************************************************************
  * checkIfLongLongSupportNeeded: checks if an instruction requires
  *  target to support 64-bit integer operations
@@ -1157,9 +1103,7 @@ unsigned GenXLegalization::determineWidth(unsigned WholeWidth,
                                           unsigned StartIdx) {
   // Prepare to keep track of whether an instruction with a minimum width
   // (e.g. dp4) would be split too small, and whether we need to unbale.
-  unsigned ExecSizeAllowedBits = 0x3f;
-  if (auto Main = B.getMainInst())
-    ExecSizeAllowedBits = getExecSizeAllowedBits(Main->Inst);
+  unsigned ExecSizeAllowedBits = adjustTwiceWidthOrFixed4(B);
   unsigned MainInstMinWidth =
       1 << countTrailingZeros(ExecSizeAllowedBits, ZB_Undefined);
   // Determine the vector width that we need to split into.
@@ -2198,20 +2142,6 @@ static Value *createBitCastIfNeeded(Value *V, Type *NewTy,
                           V->getName() + ".cast", InsertBefore);
 }
 
-// Get type that represents OldTy as vector of NewScalarType.
-static Type *getNewVectorType(Type *OldTy, IntegerType *NewScalarType) {
-  IGC_ASSERT(OldTy->isIntOrIntVectorTy());
-  unsigned OldElemSize = OldTy->getScalarSizeInBits();
-  IGC_ASSERT(OldElemSize > 0);
-  auto *VTy = dyn_cast<IGCLLVM::FixedVectorType>(OldTy);
-  unsigned OldNumElems = VTy ? VTy->getNumElements() : 1;
-  unsigned NewElemSize = NewScalarType->getBitWidth();
-  if (OldElemSize * OldNumElems % NewElemSize)
-    return nullptr;
-  return IGCLLVM::FixedVectorType::get(NewScalarType,
-                         OldElemSize * OldNumElems / NewElemSize);
-}
-
 /***********************************************************************
  * transformMoveType : transform move bale to new integer type.
  *
@@ -2265,16 +2195,13 @@ Instruction *GenXLegalization::transformMoveType(Bale *B, IntegerType *FromTy,
     return nullptr;
   Region DstRgn = Wr ? makeRegionFromBaleInfo(Wr, BaleInfo()) : Region(Rd);
 
-  // Check that dst and src regions can be changed on new type.
-  if (SrcRgn.Indirect || DstRgn.Indirect || DstRgn.Mask)
-    return nullptr;
   // If destination region is not contiguous, changing element type can be achived
   // by conversion to 2D region, but we try to avoid it because such dst operands
   // are not supported in HW and require additional code for emulation.
   if (DstRgn.Stride != 1)
     return nullptr;
-  Type *NewSrcTy = getNewVectorType(Src->getType(), ToTy),
-       *NewDstTy = getNewVectorType(Dst->getType(), ToTy);
+  Type *NewSrcTy = genx::changeVectorType(Src->getType(), ToTy),
+       *NewDstTy = genx::changeVectorType(Dst->getType(), ToTy);
   if (!NewSrcTy || !NewDstTy)
     return nullptr;
 
