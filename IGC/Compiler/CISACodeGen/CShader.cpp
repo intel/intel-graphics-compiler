3f41f661b835255d9f45ce2a1fcee482298db625
Marcin Naczk <marcin.naczk@intel.com>
2021-12-02 12:15:20 -0800

    [Autobackout][FuncReg]Revert of change: 60d6c2333e5d7516445183653c733f05069de8da  llvm.stacksave support fix ver2
    For @llvm.stacksave returned var must be created based on SP instead of LLVM value.
    But it only applies when SP exists.

--- IGC/Compiler/CISACodeGen/CShader.cpp
+++ IGC/Compiler/CISACodeGen/CShader.cpp
@@ -2967,17 +2967,7 @@ CVariable* CShader::GetSymbol(llvm::Value* value, bool fromConstantPool)
     // need to create a new mapping
     if (!var)
     {
-        // for @llvm.stacksave returned var must be created based on SP instead of LLVM value
-        llvm::IntrinsicInst* IntrinsicInstruction = dyn_cast<llvm::IntrinsicInst>(value);
-        if (IntrinsicInstruction && IntrinsicInstruction->getIntrinsicID() == Intrinsic::stacksave && hasSP())
-        {
-            auto pSP = GetSP();
-            var = GetNewVariable(pSP->GetNumberElement(), pSP->GetType(), pSP->GetAlign(), value->getName());
-        }
-        else
-        {
-            var = GetNewVector(value, preferredAlign);
-        }
+        var = GetNewVector(value, preferredAlign);
     }
 
     symbolMapping.insert(std::pair<llvm::Value*, CVariable*>(value, var));
