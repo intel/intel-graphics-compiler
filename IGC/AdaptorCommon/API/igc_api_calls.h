6f171d49310ef086dcfdb197aa159d52d6e307b4
Benjamin Sluis <benjamin.sluis@intel.com>
2021-12-07 11:10:31 +0000

    refactor igc_api_calls.h
    minor refactoring of igc_api_calls.h
    add compilation guard and EmUtils.h

--- IGC/AdaptorCommon/API/igc_api_calls.h
+++ IGC/AdaptorCommon/API/igc_api_calls.h
@@ -6,7 +6,10 @@ SPDX-License-Identifier: MIT
 
 ============================= end_copyright_notice ===========================*/
 
-#pragma once
+#ifndef IGC_API_CALLS_H
+#define IGC_API_CALLS_H
+
+#include "../../common/EmUtils.h"
 
 #if defined(_WIN32)
     #define IGC_API_CALL_TYPE __stdcall
@@ -16,6 +19,8 @@ SPDX-License-Identifier: MIT
         #define IGC_API_CALL __declspec(dllimport) IGC_API_CALL_TYPE
     #endif
 #else
-    #define IGC_API_CALL_TYPE
+        #define IGC_API_CALL_TYPE
     #define IGC_API_CALL __attribute__ ((visibility("default"))) IGC_API_CALL_TYPE
 #endif
+
+#endif // IGC_API_CALLS_H
