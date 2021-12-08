335f1559c54e52a839cbadc36e4d53fb0c2dae45
lgotszal <lukasz.gotszald@intel.com>
2021-12-08 08:51:34 +0000

    update CopyrightHeaders in IGC
    update CopyrightHeaders in IGC

--- inc/common/platforms.h
+++ inc/common/platforms.h
@@ -1,3 +1,10 @@
+/*========================== begin_copyright_notice ============================
+
+Copyright (C) 2021 Intel Corporation
+
+SPDX-License-Identifier: MIT
+
+============================= end_copyright_notice ===========================*/
 
 typedef enum {
     UNKNOWN_ISA = 0,
