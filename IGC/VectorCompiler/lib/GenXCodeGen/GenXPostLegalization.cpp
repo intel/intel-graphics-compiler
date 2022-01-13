e3ee442865602afa3906ed2c084527cd73cb2d69
Sergei Kachkov <sergei.kachkov@intel.com>
2022-01-13 07:52:22 -0800

    [Autobackout][FuncReg]Revert of change: 81c979471b89893e5957fd0deb696e136a14b464  Fold bitcasts to rdregion/wrregion by changing region parameters
    This transformation reorders rdregion->bitcast to bitcast->rdregion and
    bitcast->wrregion to wrregion->bitcast, so they can be baled later.

--- IGC/VectorCompiler/lib/GenXCodeGen/GenXPostLegalization.cpp
+++ IGC/VectorCompiler/lib/GenXCodeGen/GenXPostLegalization.cpp
@@ -138,7 +138,7 @@ bool GenXPostLegalization::runOnFunction(Function &F)
   // Run the vector decomposer for this function.
   Modified |= VD.run(DT);
   // Cleanup region reads and writes.
-  Modified |= simplifyRegionInsts(&F, DL, ST);
+  Modified |= simplifyRegionInsts(&F, DL);
   // Cleanup redundant global loads.
   Modified |= cleanupLoads(&F);
   // Cleanup constant loads.
