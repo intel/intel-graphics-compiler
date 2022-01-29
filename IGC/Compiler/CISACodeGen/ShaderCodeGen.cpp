6df323b1bb6462a8209a9c9634c3d2da41d815b8
fftzeng <frances.tzeng@intel.com>
2022-01-29 04:31:27 -0800

    [Autobackout][FuncReg]Revert of change: be9466bb776c4a279e5330ea42489373782ae810  add ThreeWayLoadSpiltOpt
    For a pattern with 3 sample/load and only use one of them based on some
    condition, change it to use branches to only do the one sample/load that
    matters.

--- IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
+++ IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
@@ -1838,9 +1838,6 @@ void OptimizeIR(CodeGenContext* const pContext)
                 mpm.add(new SampleMultiversioning(pContext));
         }
 
-        // need to be before code sinking
-        mpm.add(createInsertBranchOptPass());
-
         bool disableGOPT = ( (IsStage1FastestCompile(pContext->m_CgFlag, pContext->m_StagingCtx) ||
                                IGC_GET_FLAG_VALUE(ForceFastestSIMD)) &&
                              (IGC_GET_FLAG_VALUE(FastestS1Experiments) & FCEXP_DISABLE_GOPT));
@@ -2064,6 +2061,11 @@ void OptimizeIR(CodeGenContext* const pContext)
                 mpm.add(new CustomUnsafeOptPass());
             }
             mpm.add(createGenOptLegalizer());
+        }
+
+        if (IGC_IS_FLAG_ENABLED(EnableAtomicBranch) || pContext->getModuleMetaData()->csInfo.atomicBranch)
+        {
+            mpm.add(createInsertBranchOptPass());
         }
 
         if (pContext->m_enableSubroutine &&
