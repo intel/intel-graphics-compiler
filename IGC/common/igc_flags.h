88311c5fe93c007031ce2bb50fefb6d18f4cd0c5
Arkady Shlykov <arkady.shlykov@intel.com>
2022-02-01 17:42:39 +0000

    Handle vector bitcasts in loads simplification
    Handle bitcasts presented between loads and extractelement instructions
    (e.g. in case of ByteAddressBuffer usage) that prevent memory operations
    simplification in VectorPreProcess pass.

--- IGC/common/igc_flags.h
+++ IGC/common/igc_flags.h
@@ -241,6 +241,8 @@ DECLARE_IGC_REGKEY(bool, EnableUnmaskedFunctions,    true,    "Enable unmaksed f
 DECLARE_IGC_REGKEY(bool, EnableStatefulAtomic,       false,   "Enable promoting stateless atomic to stateful atomic.", false)
 DECLARE_IGC_REGKEY(bool, EnableHoistDp3,             false,    "Enable dp3 Hoisting.", false)
 DECLARE_IGC_REGKEY(bool, ForceHoistDp3,              false,   "force dp3 Hoisting.", false)
+DECLARE_IGC_REGKEY(bool, EnableBitcastedLoadNarrowing,       true,  "Enable narrowing of vector loads in bitcasts patterns.", false)
+DECLARE_IGC_REGKEY(bool, EnableBitcastedLoadNarrowingToScalar, false, "Enable narrowing of vector loads to scalar ones in bitcasts patterns.", false)
 
 DECLARE_IGC_GROUP("Shader debugging")
 DECLARE_IGC_REGKEY(bool, ForceDisableShaderDebugHashCodeInKernel,   false,  "Disable hash code addition to the binary after EOT", false)
