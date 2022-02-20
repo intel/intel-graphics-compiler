6120070e4cebb64b8b3b7ee264c4de556df1ca0f
Gu, Junjie <junjie.gu@intel.com>
2022-02-19 19:44:46 -0800

    [Autobackout][FuncReg]Revert of change: c997e5c949a421b5374ad4cf6c0b8b57578b273a  Use wa insts provided by RA
    Instead of scanning kernel for finding insts that need NoMask WA, this
    change enables using insts provided by RA. This approach is complete
    and efficient compared with scanning kernel.
    
    This change affects new NoMask WA, which is on for VC now.

--- visa/Optimizer.h
+++ visa/Optimizer.h
@@ -271,7 +271,7 @@ private:
     void doNoMaskWA_postRA();
     void newDoNoMaskWA_postRA();
     void verifyRANoMaskWAList();
-    bool NoMaskWAUseRAList() const { return true; }
+    bool NoMaskWAUseRAList() const { return false; }
     void insertFenceAtEntry();
     void expandMulPostSchedule();
     void expandMadwPostSchedule();
