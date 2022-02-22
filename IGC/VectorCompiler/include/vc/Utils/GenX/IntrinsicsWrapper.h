e065ab148a06e979148649794d72cf6a2866cc85
Ivan Tetyushkin <ivan.tetyushkin@intel.com>
2022-02-22 12:36:28 +0000

    Adding some interface functions for IntrinsicsWrapper
    Adding:
     isOverloadedRet
     isOverloadedArg
     getAnyDeclaration

--- IGC/VectorCompiler/include/vc/Utils/GenX/IntrinsicsWrapper.h
+++ IGC/VectorCompiler/include/vc/Utils/GenX/IntrinsicsWrapper.h
@@ -13,8 +13,29 @@ SPDX-License-Identifier: MIT
 #include "llvm/GenXIntrinsics/GenXIntrinsics.h"
 
 namespace vc {
+// getAnyIntrinsicID: Return LLVM, GenX or Internal intrinsic ID
 unsigned getAnyIntrinsicID(const llvm::Function *F);
 unsigned getAnyIntrinsicID(const llvm::Value *V);
+
+// isOverloadedRet(ID) - Return true if return type
+//  in intrinsic is overloaded
+// Note: input of LLVM intrinsic not expected
+bool isOverloadedRet(unsigned ID);
+// isOverloadedArg(ID, ArgNum) - Return true if ArgumentNum
+//  in intrinsic overloaded
+// Note: input of LLVM intrinsic not expected
+bool isOverloadedArg(unsigned ID, unsigned ArgumentNum);
+
+// getAnyDeclaration - create a LLVM Function declaration
+// for an intrinsic, and return it.
+//
+// The Tys parameter is for intrinsics with overloaded types
+//  (e.g., those using iAny, fAny, vAny, or iPTRAny).
+// For a declaration of an overloaded intrinsic,
+// Tys must provide exactly one type for each overloaded type in the intrinsic.
+llvm::Function *
+getAnyDeclaration(llvm::Module *M, unsigned ID,
+                  llvm::ArrayRef<llvm::Type *> Tys = llvm::None);
 } // namespace vc
 
 #endif /* end of include guard: VC_UTILS_INTRINSICS_WRAPPER_H */
--- IGC/VectorCompiler/lib/Utils/GenX/IntrinsicsWrapper.cpp
+++ IGC/VectorCompiler/lib/Utils/GenX/IntrinsicsWrapper.cpp
@@ -16,7 +16,7 @@ unsigned vc::getAnyIntrinsicID(const Function *F) {
   if (InternalIntrinsic::isInternalNonTrivialIntrinsic(F))
     return InternalIntrinsic::getInternalIntrinsicID(F);
 
-  unsigned IID = F->getIntrinsicID();
+  Intrinsic::ID IID = F->getIntrinsicID();
   // TODO: return the biggest constant from intrinsics ID
   // currently saved the expected behaviour
   // to make sure assert
@@ -42,4 +42,32 @@ unsigned vc::getAnyIntrinsicID(const llvm::Value *V) {
     return GenXIntrinsic::not_any_intrinsic;
 
   return getAnyIntrinsicID(Callee);
+}
+
+bool vc::isOverloadedRet(unsigned ID) {
+  if (GenXIntrinsic::isGenXNonTrivialIntrinsic(ID))
+    return GenXIntrinsic::isOverloadedRet(ID);
+  if (vc::InternalIntrinsic::isInternalNonTrivialIntrinsic(ID))
+    return vc::InternalIntrinsic::isOverloadedRet(ID);
+  IGC_ASSERT_MESSAGE(false, "Not expected intrinsic ID");
+  return true;
+}
+
+bool vc::isOverloadedArg(unsigned ID, unsigned ArgumentNum) {
+  if (GenXIntrinsic::isGenXNonTrivialIntrinsic(ID))
+    return GenXIntrinsic::isOverloadedArg(ID, ArgumentNum);
+  if (vc::InternalIntrinsic::isInternalNonTrivialIntrinsic(ID))
+    return vc::InternalIntrinsic::isOverloadedArg(ID, ArgumentNum);
+  IGC_ASSERT_MESSAGE(false, "Not expected intrinsic ID");
+  return true;
+}
+
+Function *vc::getAnyDeclaration(Module *M, unsigned ID, ArrayRef<Type *> Tys) {
+  if (GenXIntrinsic::isGenXNonTrivialIntrinsic(ID))
+    return GenXIntrinsic::getGenXDeclaration(
+        M, static_cast<llvm::GenXIntrinsic::ID>(ID), Tys);
+  if (InternalIntrinsic::isInternalNonTrivialIntrinsic(ID))
+    return InternalIntrinsic::getInternalDeclaration(
+        M, static_cast<vc::InternalIntrinsic::ID>(ID), Tys);
+  return Intrinsic::getDeclaration(M, static_cast<Intrinsic::ID>(ID), Tys);
 }
