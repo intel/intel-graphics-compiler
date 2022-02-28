1b7cc9e355709d91f919441a7b761cbc8ce25cce
Maciej Kalinski <maciej.kalinski@intel.com>
2022-02-28 22:08:27 +0000

    Add ByteUnit to the Shader Units facilities
    Helpful for calculations involving bytes.

--- IGC/Compiler/CISACodeGen/ShaderUnits.hpp
+++ IGC/Compiler/CISACodeGen/ShaderUnits.hpp
@@ -11,52 +11,59 @@ SPDX-License-Identifier: MIT
 #include "common/Units.hpp"
 namespace IGC
 {
+/// Represents the size of one bit.
+struct BitUnitDescriptor
+{
+    static const int ratio = 1;
+    inline static const char* name(bool plural) { return plural ? "bits" : "bit"; }
+};
 
-    struct BitUnit
-    {
-        static const int ratio = 1;
-        inline static const char* name(bool plural) { return plural ? "bits" : "bit"; }
-    };
+/// Represents the size of one byte.
+struct ByteUnitDescriptor
+{
+    static const int ratio = 8 * BitUnitDescriptor::ratio; // 8 bits, aka BYTE
+    inline static const char* name(bool plural) { return plural ? "bytes" : "byte"; }
+};
 
-    /// Represents one element of the GS payload / URB write that keeps e.g. single color component
-    struct Element
-    {
-        static const int ratio = 32 * BitUnit::ratio; // 32 bits, aka DWORD
-        inline static const char* name(bool plural) { return plural ? "elements" : "element"; }
-    };
+/// Represents one element of the GS payload / URB write that keeps e.g. single color component
+struct Element
+{
+    static const int ratio = 32 * BitUnitDescriptor::ratio; // 32 bits, aka DWORD
+    inline static const char* name(bool plural) { return plural ? "elements" : "element"; }
+};
 
-    /// Represents the size of one attribute (that consists of four elements).
-    struct Attribute
-    {
-        static const int ratio = 4 * Element::ratio; // 128 bits
-        inline static const char* name(bool plural) { return plural ? "attributes" : "attribute"; }
-    };
+/// Represents the size of one attribute (that consists of four elements).
+struct Attribute
+{
+    static const int ratio = 4 * Element::ratio; // 128 bits
+    inline static const char* name(bool plural) { return plural ? "attributes" : "attribute"; }
+};
 
-    /// Represents the size of four elements.
-    struct QuadElement
-    {
-        static const int ratio = 4 * Element::ratio; // 128 bits
-        inline static const char* name(bool plural) { return plural ? "quad attributes" : "quad attribute"; }
-    };
+/// Represents the size of four elements.
+struct QuadElement
+{
+    static const int ratio = 4 * Element::ratio; // 128 bits
+    inline static const char* name(bool plural) { return plural ? "quad attributes" : "quad attribute"; }
+};
 
-    /// Represents the size of eight elements.
-    struct OctElement
-    {
-        static const int ratio = 8 * Element::ratio; // 256 bits
-        inline static const char* name(bool plural) { return plural ? "oct attributes" : "oct attribute"; }
-    };
+/// Represents the size of eight elements.
+struct OctElement
+{
+    static const int ratio = 8 * Element::ratio; // 256 bits
+    inline static const char* name(bool plural) { return plural ? "oct attributes" : "oct attribute"; }
+};
 
-    // URB Allocation size is counted in 512 bit units (cache line size)
-    struct URBAllocation
-    {
-        static const int ratio = 16 * Element::ratio;
-        inline static const char* name(bool plural) { return plural ? "URBAllocations" : "URBAllocation"; }
-    };
+// URB Allocation size is counted in 512 bit units (cache line size)
+struct URBAllocation
+{
+    static const int ratio = 16 * Element::ratio;
+    inline static const char* name(bool plural) { return plural ? "URBAllocations" : "URBAllocation"; }
+};
 
-    /// Shorthands for type names
-    typedef Unit<Element>       EltUnit;
-    typedef Unit<QuadElement>   QuadEltUnit;
-    typedef Unit<OctElement>    OctEltUnit;
-    typedef Unit<URBAllocation> URBAllocationUnit;
-
+/// Shorthands for type names
+typedef Unit<ByteUnitDescriptor> ByteUnit;
+typedef Unit<Element>       EltUnit;
+typedef Unit<QuadElement>   QuadEltUnit;
+typedef Unit<OctElement>    OctEltUnit;
+typedef Unit<URBAllocation> URBAllocationUnit;
 }
