990d4ff0f5f3cee37f2c822f6ea51ee5d682a3b4
Anatoly Parshintsev <anatoly.parshintsev@intel.com>
2022-03-03 13:19:53 +0000

    list of supported platforms moved to separate file
    minor cosmetic refactoring

---
+++ IGC/VectorCompiler/cmake/supported_platforms_list.cmake
@@ -0,0 +1,20 @@
+
+set(SUPPORTED_VC_PLATFORMS
+    "BDW"
+    "SKL"
+    "BXT"
+    "KBL"
+    "GLK"
+    "ICLLP"
+    "TGLLP"
+    "RKL"
+    "DG1"
+    "XEHP"
+    "ADLP"
+    "ADLS"
+    "DG2"
+    "PVC"
+    "PVCXT"
+    )
+
+
