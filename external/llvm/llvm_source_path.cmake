5f7ba435520ebdd3e15b484b0416017a29b895ef
Naczk, Marcin <marcin.naczk@intel.com>
2022-03-03 11:57:50 +0000

    Fixing finding LLVM directory when compiling LLVM prebuild
    Fixing finding LLVM directory when compiling LLVM prebuild

--- external/llvm/llvm_source_path.cmake
+++ external/llvm/llvm_source_path.cmake
@@ -7,12 +7,14 @@
 #============================ end_copyright_notice =============================
 
 # Use LLVM sources stored at fixed location
+message(STATUS "[LLVM] Current value of CMAKE_CURRENT_SOURCE_DIR : ${CMAKE_CURRENT_SOURCE_DIR}")
 list(APPEND IGC_LLVM_PATHS
   ${CMAKE_CURRENT_SOURCE_DIR}/../../llvm-project
   /opt/src/llvm-project_${IGC_OPTION__LLVM_PREFERRED_VERSION}
   ${CMAKE_CURRENT_SOURCE_DIR}/../../llvm-project_${IGC_OPTION__LLVM_PREFERRED_VERSION}
   ${CMAKE_CURRENT_SOURCE_DIR}/../../../llvm-project_${IGC_OPTION__LLVM_PREFERRED_VERSION}
   ${CMAKE_CURRENT_SOURCE_DIR}/../../../../llvm-project_${IGC_OPTION__LLVM_PREFERRED_VERSION}
+  ${CMAKE_CURRENT_SOURCE_DIR}/../../../../../llvm-project_${IGC_OPTION__LLVM_PREFERRED_VERSION}
   )
 
 find_path(DEFAULT_IGC_LLVM_SOURCES_DIR
