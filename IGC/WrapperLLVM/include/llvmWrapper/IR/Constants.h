63bb1f5d4ae34c0f8af0c233c223434c7b350cc8
Artem Gindinson <artem.gindinson@intel.com>
2022-03-15 13:00:01 +0000

    Introduce a wrapper for ConstantExpr::getShuffleVector
    Introduce wrappers for `ConstantExpr::getShuffleVector`, as the LLVM
    10 version only allows `Constant *` masks, while LLVM 11 solely
    provides a signature with `ArrayRef<int>` for the mask argument.

--- IGC/WrapperLLVM/include/llvmWrapper/IR/Constants.h
+++ IGC/WrapperLLVM/include/llvmWrapper/IR/Constants.h
@@ -32,6 +32,25 @@ namespace IGCLLVM
 #endif
   }
 
+    namespace ConstantExpr {
+#if LLVM_VERSION_MAJOR < 11
+        inline llvm::Constant *getShuffleVector(llvm::Constant *V1,
+            llvm::Constant *V2, llvm::ArrayRef<uint64_t> Mask,
+            llvm::Type *OnlyIfReducedTy = nullptr) {
+            return llvm::ConstantExpr::getShuffleVector(V1, V2,
+                       llvm::ConstantDataVector::get(V1->getContext(), Mask),
+                       OnlyIfReducedTy);
+        }
+#else
+        inline llvm::Constant *getShuffleVector(llvm::Constant *V1,
+            llvm::Constant *V2, llvm::ArrayRef<int> Mask,
+            llvm::Type *OnlyIfReducedTy = nullptr) {
+            return llvm::ConstantExpr::getShuffleVector(V1, V2, Mask,
+                                                        OnlyIfReducedTy);
+        }
+#endif
+    }
+
     namespace ConstantFixedVector
     {
         inline llvm::Constant *getSplat(unsigned NumElements, llvm::Constant *V)
