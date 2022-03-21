9e87804e5bc021ef48ae0a23bbcb43d4511b68ca
David Woo <david.j.woo@intel.com>
2022-03-21 21:18:20 +0000

    Add helper lane mode to wave intrinsics
    Added helper lane mode argument to wave intrinsics. Much like with
    GenISA_WaveShuffleIndex, this argument denotes that helper lanes
    should be active for this instruction when its value is 1.

--- IGC/Compiler/CISACodeGen/EmitVISAPass.cpp
+++ IGC/Compiler/CISACodeGen/EmitVISAPass.cpp
@@ -20657,6 +20657,7 @@ void EmitPass::emitWavePrefix(WavePrefixIntrinsic* I)
         if (CI->isAllOnesValue())
             Mask = nullptr;
     }
+    m_encoder->SetSubSpanDestination(false);
     emitScan(
         I->getSrc(), I->getOpKind(), I->isInclusiveScan(), Mask, false);
 }
@@ -20707,6 +20708,7 @@ void EmitPass::emitWaveAll(llvm::GenIntrinsicInst* inst)
     uint64_t identity = 0;
     GetReductionOp(op, inst->getOperand(0)->getType(), identity, opCode, type);
     CVariable* dst = m_destination;
+    m_encoder->SetSubSpanDestination(false);
     emitReductionAll(opCode, identity, type, false, src, dst);
 }
 
@@ -20720,6 +20722,7 @@ void EmitPass::emitWaveClustered(llvm::GenIntrinsicInst* inst)
     uint64_t identity = 0;
     GetReductionOp(op, inst->getOperand(0)->getType(), identity, opCode, type);
     CVariable *dst = m_destination;
+    m_encoder->SetSubSpanDestination(false);
     emitReductionClustered(opCode, identity, type, false, clusterSize, src, dst);
 }
 
--- IGC/Compiler/CISACodeGen/PromoteInt8Type.cpp
+++ IGC/Compiler/CISACodeGen/PromoteInt8Type.cpp
@@ -1141,28 +1141,32 @@ void PromoteInt8Type::promoteIntrinsic()
                     iArgs.push_back(GII->getArgOperand(2));
                     break;
                 }
-                case GenISAIntrinsic::GenISA_WaveAll:
+                case GenISAIntrinsic::GenISA_WaveClustered:
                 {
-                    // prototype Ty <waveall> (Ty, char)
-                    iArgs.push_back(GII->getArgOperand(1));
-                    break;
-                }
-                case GenISAIntrinsic::GenISA_WavePrefix:
-                {
-                    // prototype:  Ty <waveprefix> (Ty, char, bool, bool)
+                    // prototype:
+                    //     Ty <clustered> (Ty, char, int, int)
                     iArgs.push_back(GII->getArgOperand(1));
                     iArgs.push_back(GII->getArgOperand(2));
                     iArgs.push_back(GII->getArgOperand(3));
                     break;
                 }
+                case GenISAIntrinsic::GenISA_WavePrefix:
+                {
+                    // prototype:  Ty <waveprefix> (Ty, char, bool, bool, int)
+                    iArgs.push_back(GII->getArgOperand(1));
+                    iArgs.push_back(GII->getArgOperand(2));
+                    iArgs.push_back(GII->getArgOperand(3));
+                    iArgs.push_back(GII->getArgOperand(4));
+                    break;
+                }
                 case GenISAIntrinsic::GenISA_QuadPrefix:
                 case GenISAIntrinsic::GenISA_WaveShuffleIndex:
-                case GenISAIntrinsic::GenISA_WaveClustered:
+                case GenISAIntrinsic::GenISA_WaveAll:
                 {
                     // prototype:
                     //     Ty <quadprefix> (Ty, char, bool)
                     //     Ty <shuffleIndex> (Ty, int, int)
-                    //     Ty <clustered> (Ty, char, int)
+                    //     Ty <waveall> (Ty, char, int)
                     iArgs.push_back(GII->getArgOperand(1));
                     iArgs.push_back(GII->getArgOperand(2));
                     break;
