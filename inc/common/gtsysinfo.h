156b770b8203c2c18bc3485b42e6cc8c651c1d35
Jurek, Pawel <pawel.jurek@intel.com>
2022-03-25 13:46:31 +0000

    Increase GT_MAX_DUALSUBSLICE_PER_SLICE value
    Future IP can have more DSS per slice, so increase the maximum.

--- inc/common/gtsysinfo.h
+++ inc/common/gtsysinfo.h
@@ -22,8 +22,8 @@ SPDX-License-Identifier: MIT
 #define GT_MAX_SUBSLICE_PER_SLICE      (8)
 #define GT_MAX_VDBOX                   (2)
 #define GT_MAX_VEBOX                   (2)
-#define GT_MAX_SUBSLICE_PER_DSS        (2) // Currently max value based on Gen12
-#define GT_MAX_DUALSUBSLICE_PER_SLICE  (6) // Currently max value based on Gen12LP
+#define GT_MAX_SUBSLICE_PER_DSS        (2)
+#define GT_MAX_DUALSUBSLICE_PER_SLICE  (8)
 
 typedef struct GT_SUBSLICE_INFO
 {
