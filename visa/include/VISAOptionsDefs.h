f63d7d546ed7620635a939a07ced8506098e4639
bcheng0127 <bu.qi.cheng@intel.com>
2022-05-01 19:10:29 +0000

    Disable preRA scheduling for ACC
    Disable preRA scheduling for ACC

--- visa/include/VISAOptionsDefs.h
+++ visa/include/VISAOptionsDefs.h
@@ -82,7 +82,7 @@ DEF_VISA_OPTION(vISA_EnableSplitVariables,  ET_BOOL, "-noSplitVariables", UNUSED
 DEF_VISA_OPTION(vISA_ChangeMoveType,        ET_BOOL, "-ALTMode",       UNUSED, true)
 DEF_VISA_OPTION(vISA_accSubstitution,       ET_BOOL, "-noAccSub",          UNUSED, true)
 DEF_VISA_OPTION(vISA_accSubBeforeRA,        ET_BOOL, "-noAccSubBRA",          UNUSED, true)
-DEF_VISA_OPTION(vISA_PreSchedForAcc,   ET_BOOL,  "-preSchedForAcc", UNUSED, true)
+DEF_VISA_OPTION(vISA_PreSchedForAcc,   ET_BOOL,  "-preSchedForAcc", UNUSED, false)
 DEF_VISA_OPTION(vISA_EnableGatherWithImm,      ET_BOOL, "-gatherWithImm",        UNUSED, 0)
 DEF_VISA_OPTION(vISA_doAccSubAfterSchedule, ET_BOOL, "-accSubPostSchedule",    UNUSED, true)
 DEF_VISA_OPTION(vISA_localizationForAccSub, ET_BOOL, "-localizeForACC",    UNUSED, false)
