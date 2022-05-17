d854c3153a3b5ce2723238900066a55d7066dea0
dlei6g <xiao.lei@intel.com>
2022-05-17 19:12:41 +0000

    Stackcall performance fixes
    1. Disables the EnableWriteOldFPToStack flag by default. This saves us from creating an extra store when it's not necessary.
    2. Check push size is greater than 0, before generating add instructions to increment SP.

--- IGC/common/igc_flags.h
+++ IGC/common/igc_flags.h
@@ -358,7 +358,7 @@ DECLARE_IGC_REGKEY(bool, InitializeAddressRegistersBeforeUse,     false, "Settin
 DECLARE_IGC_REGKEY(bool, EnableGTLocationDebugging,     false, "Setting this to 1 (true) enables GT location expression emmitions for GPU debugger", true)
 DECLARE_IGC_REGKEY(bool, UseOffsetInLocation,           true,  "Setting this to 1 (true) preserves private base and per thread offset and removes preservation of any other debug variables", true)
 DECLARE_IGC_REGKEY(bool, EnableRelocations,             false, "Setting this to 1 (true) makes IGC emit relocatable ELF with debug info", true)
-DECLARE_IGC_REGKEY(bool, EnableWriteOldFPToStack,       true,  "Setting this to 1 (true) writes the caller frame's frame-pointer to the start of callee's frame on stack, to support stack walk", false)
+DECLARE_IGC_REGKEY(bool, EnableWriteOldFPToStack,       false, "Setting this to 1 (true) writes the caller frame's frame-pointer to the start of callee's frame on stack, to support stack walk", true)
 DECLARE_IGC_REGKEY(bool, ZeBinCompatibleDebugging,      true,  "Setting this to 1 (true) enables embed debug info in zeBinary", true)
 DECLARE_IGC_REGKEY(bool, DebugInfoEnforceAmd64EM,       false, "Enforces elf file with the debug infomation to have eMachine set to AMD64", false)
 DECLARE_IGC_REGKEY(bool, DebugInfoValidation,           false, "Enable optional (strict) checks to detect debug information inconsistencies", false)
