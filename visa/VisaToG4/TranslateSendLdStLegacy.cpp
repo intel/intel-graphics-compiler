b432ed58cd0fc7c4b029b4d01b3a0e4568a33286
Gu, Junjie <junjie.gu@intel.com>
2022-05-25 05:42:32 -0700

    [Autobackout][FuncReg]Revert of change: 8f983836e2a451202dda2dd254d20d58fb75356a  Incorrect payload preparation for some send
    If send's execsize is small, like 1|2/4, etc so that its payload size is
    less than 1 GRF, preparePayload() does not generate correct code.
    For example,
    
    .decl dst_0 v_type=G type=uq num_elts=1 align=wordx32
    .decl V0032 v_type=G type=d num_elts=1 align=wordx32
    
    svm_atomic.add (M1_NM, 1) dst_0.0 %null.0 V0032.0 %null.0
    
    incorrect code generated:
    
    (W) mov (8)              M3(0,0)<1>:uq  dst_0(0,0)<1;1,0>:uq
    (W) mov (8)              M3(2,0)<1>:d  V0032(0,0)<1;1,0>:d
    (W) send (1)             null<1>:ud M3(0,0) 0x60487ff:ud
    
    preparePayload() created those mov insts whose sources are out of bound.
    
    The proposed fix is:
    (W) mov (1)              M3(0,0)<1>:uq  dst_0(0,0)<0;1,0>:uq
    (W) mov (1)              M3(0,0)<1>:d  V0032(0,0)<0;1,0>:d
    (W) send (1)             null<1>:ud M3(0,0) 0x60487ff:ud
    
    This fix does not initialize all elements of M3, only initialize elements that
    are necessary.
    
    This change fixes the issue in igc-5400. There are potential ones in other places that
    invokes preparePayload(), which will be addressed once this fix is stable.

--- visa/VisaToG4/TranslateSendLdStLegacy.cpp
+++ visa/VisaToG4/TranslateSendLdStLegacy.cpp
@@ -2997,11 +2997,7 @@ int IR_Builder::translateVISASVMAtomicInst(
 
     G4_SrcRegRegion *msgs[2] = {0, 0};
     unsigned sizes[2] = {0, 0};
-    // For send that has smaller execsize than exSize, like
-    //     "send (4)  ..."
-    // Make sure to use send's execsize (4) as batchsize, not 8/16/32.
-    // Thus, batchsize is min(exSize, instExSize).
-    preparePayload(msgs, sizes, std::min(exSize, instExSize), useSplitSend, sources, len);
+    preparePayload(msgs, sizes, exSize, useSplitSend, sources, len);
     unsigned dstLength = dst->isNullReg() ? 0 : ((bitwidth == 16 || bitwidth == 32) ? 1 : 2);
     unsigned msgDesc = 0;
     msgDesc |= getA64BTI();
