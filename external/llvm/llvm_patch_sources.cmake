5f9fcde71de6f8282ac291905de0699404948d25
Szymichowski, Pawel <pawel.szymichowski@intel.com>
2022-06-03 13:34:05 +0000

    Fix LLVM 14 CMake common utils problem
    In LLVM 14 a new directory in the project root was created: `/cmake`:
    https://github.com/llvm/llvm-project/tree/release/14.x/cmake
    which all `clang`, `lld` & `llvm` use. We now copy it while patching LLVM.

--- external/llvm/llvm_patch_sources.cmake
+++ external/llvm/llvm_patch_sources.cmake
@@ -24,6 +24,15 @@ message(STATUS "[LLVM] : Copying stock LLVM sources ${IGC_OPTION__LLVM_SOURCES_D
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${IGC_OPTION__LLVM_SOURCES_DIR}/.git ${IGC_LLVM_WORKSPACE_SRC}/.git)
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${IGC_OPTION__LLVM_SOURCES_DIR}/llvm ${IGC_LLVM_SOURCE_DIR})
 
+# In LLVM 14 a /cmake common utils dir was created in the project's root directory
+# that's used by all llvm, clang & lld as relative import.
+# It may be better to put this is another *.cmake file, however it's convenient here
+# as it should always happen when we copy LLVM to out of it's original directory.
+if(EXISTS ${IGC_OPTION__LLVM_SOURCES_DIR}/cmake)
+  message(STATUS "[LLVM] : Copying LLVM common CMake utils ${IGC_OPTION__LLVM_SOURCES_DIR}/cmake to ${IGC_LLVM_WORKSPACE_SRC}/cmake")
+  execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${IGC_OPTION__LLVM_SOURCES_DIR}/cmake ${IGC_LLVM_WORKSPACE_SRC}/cmake)
+endif()
+
 message(STATUS "[LLVM] : Applying patches for LLVM from version ${DIR_WITH_PATCHES}")
 
 execute_process(COMMAND
