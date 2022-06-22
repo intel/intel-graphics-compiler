e010db8c246691e6385e14bcb61657cb9479633c
ktrifunovic <konrad.trifunovic@intel.com>
2022-06-22 13:16:40 +0000

    Gate the execution of stateless to bindless promotion with 4gb buffer size flag
    If buffers with size >4GB are required (in the advanced bindless mode) - stateless to bindless promotion cannot be safely

--- IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
+++ IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
@@ -776,7 +776,11 @@ static void AddLegalizationPasses(CodeGenContext& ctx, IGCPassManager& mpm, PSSi
 
     if (ctx.type == ShaderType::OPENCL_SHADER &&
         static_cast<OpenCLProgramContext&>(ctx).
-            m_InternalOptions.PromoteStatelessToBindless)
+            m_InternalOptions.PromoteStatelessToBindless &&
+        (static_cast<OpenCLProgramContext&>(ctx).
+            m_InternalOptions.UseBindlessLegacyMode ||
+            !ctx.getModuleMetaData()->compOpt.GreaterThan4GBBufferRequired)
+        )
     {
         mpm.add(new PromoteStatelessToBindless());
     }
