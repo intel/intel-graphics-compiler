b9ab7aca98498e7d90472c3f0b450bbf88ce4f34
Irene Wu <irene.w.wu@intel.com>
2022-07-28 00:41:13 +0000

    Allow promotion of alloca i64
    Remove the check that prevents i64 promotion

--- IGC/Compiler/CISACodeGen/LowerGEPForPrivMem.cpp
+++ IGC/Compiler/CISACodeGen/LowerGEPForPrivMem.cpp
@@ -242,8 +242,7 @@ static void GetAllocaLiverange(Instruction* I, unsigned int& liverangeStart, uns
 
 bool LowerGEPForPrivMem::IsNativeType(Type* type)
 {
-    if ((type->isDoubleTy() && m_ctx->platform.hasNoFP64Inst()) ||
-        (type->isIntegerTy(64) && m_ctx->platform.hasNoFullI64Support()))
+    if (type->isDoubleTy() && m_ctx->platform.hasNoFP64Inst())
     {
         return false;
     }
