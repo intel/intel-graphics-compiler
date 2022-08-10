6187fc9f66d0e97d101f772c18526bab3f52e3d5
dlei6g <xiao.lei@intel.com>
2022-08-10 19:17:21 +0000

    Add emit error instead of segfault for exported functions using (1) implicit args and (2) localSLM
    Add emit error instead of segfault for exported functions using (1) implicit args and (2) localSLM
    Allow exported functions to use implicit args if IA buffer is enabled (EnableGlobalStateBuffer=1)

--- IGC/AdaptorCommon/AddImplicitArgs.cpp
+++ IGC/AdaptorCommon/AddImplicitArgs.cpp
@@ -565,7 +565,11 @@ bool BuiltinCallGraphAnalysis::pruneCallGraphForStackCalls(CallGraph& CG)
         // since these attributes are always coupled together.
         if (pF->hasFnAttribute("referenced-indirectly"))
         {
-            IGC_ASSERT_MESSAGE(0, "Cannot force inline indirect calls! Requires ForceInlineStackCallWithImplArg=0 and IA buffer support.");
+            if (IGC_IS_FLAG_DISABLED(EnableGlobalStateBuffer))
+            {
+                IGC_ASSERT_MESSAGE(0, "Cannot force inline indirect calls! Requires IA Buffer support, i.e. EnableGlobalStateBuffer = 1");
+                getAnalysis<CodeGenContextWrapper>().getCodeGenContext()->EmitError("Exported functions does not support implicit arguments", pF);
+            }
             continue;
         }
 
