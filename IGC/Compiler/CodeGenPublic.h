9f417e4a4225c1e8cef9f647398b95dd340ded4d
Pawel Jurek <pawel.jurek@intel.com>
2022-09-21 13:58:31 +0000

    -cl-intel-enable-auto-large-GRF-mode flag
    The flag will enable the "reg sharing heuristics" in vISA, so that user has large GRF available and the compiler will choose the size.

--- IGC/Compiler/CodeGenPublic.h
+++ IGC/Compiler/CodeGenPublic.h
@@ -1874,6 +1874,10 @@ namespace IGC
                     // atoi(..) ignores leading white spaces and characters after the actual number
                     requiredEUThreadCount = atoi(op + strlen("-intel-reqd-eu-thread-count="));
                 }
+                if (strstr(options, "-intel-enable-auto-large-GRF-mode"))
+                {
+                    IntelEnableAutoLargeGRF = true;
+                }
             }
 
             bool CorrectlyRoundedSqrt;
@@ -1883,6 +1887,8 @@ namespace IGC
             bool IsLibraryCompilation = false;
             bool IntelRequiredEUThreadCount = false;
             uint32_t requiredEUThreadCount = 0;
+            // Enable compiler heuristics ("regSharingHeuristics" in VISA) for large GRF selection.
+            bool IntelEnableAutoLargeGRF = false;
         };
 
         // output: shader information
