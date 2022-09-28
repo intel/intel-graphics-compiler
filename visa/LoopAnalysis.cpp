b3492a0c9f333f90cfb0555915172f4d368a82f6
Pratik J Ashar <pratik.j.ashar@intel.com>
2022-09-28 08:25:13 -0700

    [Autobackout][FuncReg]Revert of change: 1bdf2c34ce1dfc3e2fe3f22dc28069c3b901edbf  Fix labels when adding new loop pre-header
    When adding new loop pre-header, check whether any uniform CF edge
    directly jumps to loop header. If it does, then modify label operand so
    it points to new pre-header label.

--- visa/LoopAnalysis.cpp
+++ visa/LoopAnalysis.cpp
@@ -10,7 +10,6 @@ SPDX-License-Identifier: MIT
 #include "G4_Kernel.hpp"
 #include "G4_BB.hpp"
 #include "BitSet.h"
-#include "BuildIR.h"
 
 using namespace vISA;
 
@@ -636,19 +635,6 @@ G4_BB* LoopDetection::getPreheader(Loop* loop)
     {
         if (loop->contains(pred))
             continue;
-
-        if (pred->back()->isCFInst())
-        {
-            auto cfInst = pred->back()->asCFInst();
-            MUST_BE_TRUE(cfInst->isUniform() && cfInst->opcode() == G4_jmpi,
-                "non-uniform branch");
-            auto lbl = cfInst->getSrc(0);
-            if (lbl->isLabel() &&
-                header->getLabel() == lbl->asLabel())
-            {
-                pred->back()->setSrc(preHeader->getLabel(), 0);
-            }
-        }
 
         kernel.fg.removePredSuccEdges(pred, header);
         kernel.fg.addPredSuccEdges(pred, preHeader);
