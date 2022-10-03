2fc28c434cb2be8186deaeb52fa83047814393e0
Michael LIAO <michael.liao@intel.com>
2022-10-03 16:46:49 -0700

    [Autobackout][FuncReg]Revert of change: eb6792c63dc524597223fbd83d1b7b5622ff2243  Enable SCEV-based GEP simplification.
    Enable SCEV-based GEP simplification.

--- IGC/common/igc_flags.h
+++ IGC/common/igc_flags.h
@@ -491,7 +491,7 @@ DECLARE_IGC_REGKEY(bool, EnableSamplerSupport, false, "Enables sampler messages 
 DECLARE_IGC_REGKEY(bool, EnableLSC, false, "Enables the new dataport encoding for LSC messages.", true)
 DECLARE_IGC_REGKEY(bool, ForceNoLSC, false, "Disables the new dataport encoding for LSC messages.", true)
 DECLARE_IGC_REGKEY(bool, EnableMadLoopSlice, true, "Enables the slicing of mad loops.", true)
-DECLARE_IGC_REGKEY(bool, EnableGEPSimplification, true, "Enable GEP simplification", true)
+DECLARE_IGC_REGKEY(bool, EnableGEPSimplification, false, "Enable GEP simplification", true)
 
 DECLARE_IGC_REGKEY(bool, EnableDG2LSCSIMD8WA, true, "Enables WA for DG2 LSC simd8 d32-v8/d64-v3/d64-v4. [temp, should be replaced with WA id", false)
 DECLARE_IGC_REGKEY(bool, EnableScratchMessageD64WA, false, "Enables WA to legalize D64 scratch messages to D32", false)
