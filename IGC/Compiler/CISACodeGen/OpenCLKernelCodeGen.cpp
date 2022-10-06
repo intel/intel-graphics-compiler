f7a9a5ce25328f63e1ea17323f1040671553b8ca
Mateusz Belicki <mateusz.belicki@intel.com>
2022-10-06 09:13:06 +0000

    Fixed regression introduced by external globals support.
    This patch fixes a regression in OCL atomic fetch tests introduced by
    external globals support.

--- IGC/Compiler/CISACodeGen/OpenCLKernelCodeGen.cpp
+++ IGC/Compiler/CISACodeGen/OpenCLKernelCodeGen.cpp
@@ -2206,13 +2206,7 @@ namespace IGC
 
     CVariable* COpenCLKernel::GetGlobalMapping(llvm::Value* c)
     {
-        GlobalVariable* globalVar = cast<GlobalVariable>(c);
         VISA_Type type = GetType(c->getType());
-
-        if (!globalVar->hasInitializer()) {
-            return GetUndef(type);
-        }
-
         unsigned int val = GetGlobalMappingValue(c);
         return ImmToVariable(val, type);
     }
