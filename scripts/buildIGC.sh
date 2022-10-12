5805c380ed1d01f5e1ea6bc8cab6eb0ea96ecff3
Marcin Naczk <marcin.naczk@intel.com>
2022-10-12 10:24:26 +0000

    Remove build on Ubuntu18 from GithubAction
    Remove build on Ubuntu18 from GithubAction.
    Also remove broken send email notification

--- scripts/buildIGC.sh
+++ scripts/buildIGC.sh
@@ -9,8 +9,8 @@
 #============================ end_copyright_notice =============================
 
 set -e
-# UBUNTU_VERSION   supported value [ 18, 20 ]                                                default 20
-# LLVM_VERSION     supported value [ 10, 11, 12 ]                                            default 11
+# UBUNTU_VERSION   supported value [ 20, 22 ]                                                default 20
+# LLVM_VERSION     supported value [ 10, 11, 12, 13, 14 ]                                    default 11
 # COMPILER         supported value [ gcc, clang ]                                            default gcc
 # OWN_CMAKE_FLAGS  not suported but can be use as WA (each flag should be with -D prefix)    default empty
 # example run:     UBUNTU_VERSION=ubuntu2004 LLVM_VERSION=11 COMPILER=gcc sh /home/buildIGC.sh
@@ -52,25 +52,10 @@ apt-get update
 apt-get install -y flex bison libz-dev cmake curl wget build-essential git software-properties-common unzip file
 echo "[Build Status] flex bison libz-dev cmake curl wget build-essential git software-properties-common unzip file INSTALLED"
 
-if [ "$UBUNTU_VERSION" = "18.04" ]; then
-    wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | apt-key add -
-    apt-add-repository "deb https://apt.kitware.com/ubuntu/ bionic main"
-    apt update
-    apt-get install -y cmake
-    echo "[Build Status] new cmake on Ubuntu18.04 INSTALLED"
-    wget --no-check-certificate -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
-    add-apt-repository "deb http://apt.llvm.org/bionic/   llvm-toolchain-bionic-$LLVM_VERSION  main"
-    apt update
-    echo '[Build Status] LLVM "$LLVM_VERSION" on Ubuntu18.04 PREPARED'
-fi
 apt-get install -y llvm-"$LLVM_VERSION" llvm-"$LLVM_VERSION"-dev clang-"$LLVM_VERSION" liblld-"$LLVM_VERSION" liblld-"$LLVM_VERSION"-dev
 echo "[Build Status] LLVM INSTALLED"
 
-if [ "$UBUNTU_VERSION" = "18.04" ] && [ "$LLVM_VERSION" = "11" ]; then
-    LLVM_VERSION_PREFERRED="$LLVM_VERSION".1.0
-else
-    LLVM_VERSION_PREFERRED="$LLVM_VERSION".0.0
-fi
+LLVM_VERSION_PREFERRED="$LLVM_VERSION".0.0
 echo "[Build Status] LLVM_VERSION_PREFERRED = $LLVM_VERSION_PREFERRED"
 
 echo "[Build Status] Prepare install OpenCL Clang"
--- scripts/buildSLT.sh
+++ scripts/buildSLT.sh
@@ -9,8 +9,8 @@
 #============================ end_copyright_notice =============================
 
 set -e
-# UBUNTU_VERSION   supported value [ 18, 20 ]                                                default 20
-# LLVM_VERSION     supported value [ 10, 11, 12 ]                                            default 11
+# UBUNTU_VERSION   supported value [ 20, 22 ]                                                default 20
+# LLVM_VERSION     supported value [ 10, 11, 12, 13, 14 ]                                    default 11
 # OWN_CMAKE_FLAGS  not suported but can be use as WA (each flag should be with -D prefix)    default empty
 # example run:     UBUNTU_VERSION=ubuntu2004 LLVM_VERSION=11 sh /home/buildSLT.sh
 
@@ -33,20 +33,10 @@ apt-get update
 apt-get install -y flex bison libz-dev cmake curl wget build-essential git software-properties-common unzip
 echo "[Build Status] flex bison libz-dev cmake curl wget build-essential git software-properties-common INSTALLED"
 
-if [ "$UBUNTU_VERSION" = "18.04" ]; then
-    wget --no-check-certificate -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
-    add-apt-repository "deb http://apt.llvm.org/bionic/   llvm-toolchain-bionic-$LLVM_VERSION  main"
-    apt update
-    echo '[Build Status] LLVM "$LLVM_VERSION" on Ubuntu18.04 PREPARED'
-fi
 apt-get install -y llvm-"$LLVM_VERSION" llvm-"$LLVM_VERSION"-dev clang-"$LLVM_VERSION" liblld-"$LLVM_VERSION" liblld-"$LLVM_VERSION"-dev
 echo "[Build Status] LLVM INSTALLED"
 
-if [ "$UBUNTU_VERSION" = "18.04" ] && [ "$LLVM_VERSION" = "11" ]; then
-    LLVM_VERSION_PREFERRED="$LLVM_VERSION".1.0
-else
-    LLVM_VERSION_PREFERRED="$LLVM_VERSION".0.0
-fi
+LLVM_VERSION_PREFERRED="$LLVM_VERSION".0.0
 echo "[Build Status] LLVM_VERSION_PREFERRED = $LLVM_VERSION_PREFERRED"
 
 echo "[Build Status] build and install SPIRV-LLVM-Translator"
