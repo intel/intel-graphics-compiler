641ddaa1a5b5851ba1a6fe142ea95479839184a3
bcheng0127 <bu.qi.cheng@intel.com>
2022-10-13 05:21:48 +0000

    Enable WA
    Enable WA

--- inc/common/sku_wa_defs.h
+++ inc/common/sku_wa_defs.h
@@ -572,3 +572,9 @@ SPDX-License-Identifier: MIT
         "Workaround",
         WA_BUG_TYPE_UNKNOWN,
         WA_BUG_PERF_IMPACT_UNKNOWN, WA_COMPONENT_UNKNOWN)
+
+        WA_DECLARE(
+        Wa_16018038193,
+        "Workaround",
+        WA_BUG_TYPE_UNKNOWN,
+        WA_BUG_PERF_IMPACT_UNKNOWN, WA_COMPONENT_UNKNOWN)
