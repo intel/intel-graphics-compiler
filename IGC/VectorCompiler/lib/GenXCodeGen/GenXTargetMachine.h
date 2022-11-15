172a84a990354a82406c70d3b42dc543b90739bd
Gorban, Igor <igor.gorban@intel.com>
2022-11-15 18:59:45 +0000

    Support llvm_14 build, add intel copyright notice
    .

--- IGC/VectorCompiler/lib/GenXCodeGen/GenXTargetMachine.h
+++ IGC/VectorCompiler/lib/GenXCodeGen/GenXTargetMachine.h
@@ -255,6 +255,7 @@ void initializeGenXStackUsagePass(PassRegistry &);
 void initializeCMLowerVLoadVStorePass(PassRegistry &);
 void initializeGenXStructSplitterPass(PassRegistry &);
 void initializeGenXPredRegionLoweringPass(PassRegistry &);
+void initializeGenXPredToSimdCFPass(PassRegistry &);
 void initializeGenXInlineAsmLoweringPass(PassRegistry &);
 void initializeGenXDebugLegalizationPass(PassRegistry &);
 void initializeGenXFixInvalidFuncNamePass(PassRegistry &);
