176ba7047f13b0ddd96bf826d00c1d0a3c0c9e7b
Pete Chou <chinyen.chou@intel.com>
2022-12-15 18:31:05 -0800

    [Autobackout][FuncReg]Revert of change: be321da92d096a62a64d903d8a010c152c85e321  Enable DPASFuseRSWA enabling from WA id again.
    This change updates the dummy DPAS to use null as src0.

--- visa/LocalScheduler/SWSB_G4IR.cpp
+++ visa/LocalScheduler/SWSB_G4IR.cpp
@@ -6819,9 +6819,7 @@ G4_INST *G4_BB_SB::createADummyDpasRSWAInst(LiveGRFBuckets *LB, SBNode *curNode,
 
   G4_DstRegRegion *newDst = builder.duplicateOperand(curInst->getDst());
   G4_Operand *newSrc[3];
-  // Use null as src0 of the dummy inst to prevent a potential bank/bundle
-  // conflict from happening when src0 and src1 are same.
-  newSrc[0] = builder.createNullSrc(curInst->getSrc(0)->getType());
+  newSrc[0] = builder.duplicateOperand(curInst->getSrc(0));
   newSrc[2] = builder.duplicateOperand(curInst->getSrc(2));
 
   unsigned dstLB = curInst->getDst()->getLinearizedStart();
