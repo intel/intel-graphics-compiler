a031151432b718861e4c87be862bcf41a09fe6c3
Andrzej Ratajewski <andrzej.ratajewski@intel.com>
2022-12-20 14:59:04 +0000

    Fix private pointers tagging
    This change fixes the following scenario:
    ```cpp
    __private void* __builtin_IB_to_private(void*);
    
    void func(generic int* p)
    {
        private int* pp = __builtin_IB_to_private(p); // OpGenericCastToPtrExplicit(private)
    
        // __builtin_IB_to_private is resolved by IGC to the following sequence:
        // tag = shr `p`, 61
        // is_private = icmp eq tag, privateTag
        // if(is_private):
        //    pp = p
        // else:
        //    pp = NULL
    
        pp[0] = 1;
    }
    
    void kernel test(int cond)
    {
        int x = 10;
        // Without this change, IGC doesn't set a tag, when passing generic
        // pointer to `x` as an argument to `func`.
        func(&x);
    }
    ```
    
    If there is any call to `__builtin_IB_to_private` or `__builtin_IB_to_global`
    in a kernel, then it is necessary to set a tag for private pointers casted
    to generic addrspace, since without a tag, it is not possible to differentiate
    global pointer from private pointer while handling the builtins.

--- IGC/Compiler/CodeGenContext.cpp
+++ IGC/Compiler/CodeGenContext.cpp
@@ -741,6 +741,11 @@ namespace IGC
         return false;
     }
 
+    bool CodeGenContext::forceTagForPrivatePointers() const
+    {
+        return false;
+    }
+
     bool CodeGenContext::noLocalToGenericOptionEnabled() const
     {
         return false;
