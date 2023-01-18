beb75447121c73c2570674de60ccb28938bb1ba4
Szymichowski, Pawel <pawel.szymichowski@intel.com>
2023-01-18 10:16:01 +0000

    Add RocketLake & Alderlake to ZEBin exceptions
    .

--- IGC/Compiler/CISACodeGen/OpenCLKernelCodeGen.hpp
+++ IGC/Compiler/CISACodeGen/OpenCLKernelCodeGen.hpp
@@ -289,6 +289,10 @@ namespace IGC
                 case IGFX_ICELAKE:
                 case IGFX_ICELAKE_LP:
                 case IGFX_TIGERLAKE_LP:
+                case IGFX_ROCKETLAKE:
+                case IGFX_ALDERLAKE_S:
+                case IGFX_ALDERLAKE_P:
+                case IGFX_ALDERLAKE_N:
                     return false;
                 }
             };
