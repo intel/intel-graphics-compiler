51ae08bb028dd390834e9f0301ac88aecb0c6a6a
Mateusz Borzyszkowski <mateusz.borzyszkowski@intel.com>
2023-01-23 13:24:42 +0000

    Ensure DP emulation check in PrivateMemoryUsageAnalysis pass
    Restore DP emulation check in PrivateMemoryUsageAnalysis pass.
    OCL API checks DP emulation in ErrorCheck, but other APIs can use
    PrivateMemoryUsageAnalysis to check DP emulation

--- IGC/Compiler/Optimizer/OpenCLPasses/PrivateMemory/PrivateMemoryUsageAnalysis.cpp
+++ IGC/Compiler/Optimizer/OpenCLPasses/PrivateMemory/PrivateMemoryUsageAnalysis.cpp
@@ -121,10 +121,19 @@ bool PrivateMemoryUsageAnalysis::runOnFunction(Function& F)
         }
     }
 
-    CodeGenContext* pCtx = getAnalysis<CodeGenContextWrapper>().getCodeGenContext();
+    // For double emulation, need to add private base (conservative).
     if (!m_hasPrivateMem)
     {
-        // For double emulation, need to add private base (conservative).
+        CodeGenContext* pCtx = getAnalysis<CodeGenContextWrapper>().getCodeGenContext();
+
+        // OCL API checks DP emulation in ErrorCheck, this condition is used e.g. by OGL API
+        if (IGC_IS_FLAG_ENABLED(ForceDPEmulation) ||
+            (pCtx->m_DriverInfo.NeedFP64(pCtx->platform.getPlatformInfo().eProductFamily) && pCtx->platform.hasNoFP64Inst()))
+        {
+            pCtx->m_hasDPEmu = true;
+        }
+
+        // This is the condition that double emulation is used.
         if (pCtx->m_hasDPEmu)
         {
             m_hasPrivateMem = true;
