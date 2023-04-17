fac990d7f680b0198997c14f66473de9484b5c9c
Victor Mustya <victor.mustya@intel.com>
2023-04-17 18:14:45 +0000

    Pass numeric platform ID into CM frontend
    .

--- IGC/common/VCPlatformSelector.hpp
+++ IGC/common/VCPlatformSelector.hpp
@@ -1,16 +1,14 @@
 /*========================== begin_copyright_notice ============================
 
-Copyright (C) 2021-2022 Intel Corporation
+Copyright (C) 2021-2023 Intel Corporation
 
 SPDX-License-Identifier: MIT
 
 ============================= end_copyright_notice ===========================*/
 
-// VC platform selection process need to be used in two places:
-// * igcdeps library to determine platform for SPIRV compilation
-// * fcl library to determine platform for sources compilation
-// this is header-only because we do not want link dependencies
-// from library with only two functions
+// VC platform selection process need to be used fcl library to determine
+// platform for sources compilation this is header-only because we do not want
+// link dependencies from library with only two functions
 
 #ifndef VC_PLATFORM_SELECTOR_H
 #define VC_PLATFORM_SELECTOR_H
@@ -21,57 +19,90 @@ SPDX-License-Identifier: MIT
 #include "StringMacros.hpp"
 #include "igfxfmid.h"
 
+#include <sstream>
+
 namespace cmc {
 
 constexpr int ComputeTileMaskPVC = 0x7;
 inline const char *getPlatformStr(PLATFORM Platform, unsigned &RevId) {
-  // after some consultations with Wndows KMD folks,
-  // only render core shall be used in all cases
+  auto GmdId = Platform.sRenderBlockID;
   auto Core = Platform.eRenderCoreFamily;
   auto Product = Platform.eProductFamily;
+  auto DeviceId = Platform.usDeviceID;
   IGC_ASSERT(RevId == Platform.usRevId);
 
-  switch (Core) {
-  case IGFX_GEN9_CORE:
-    return "SKL";
-  case IGFX_GEN11_CORE:
-    return "ICLLP";
-  case IGFX_GEN12_CORE:
-  case IGFX_GEN12LP_CORE:
-  case IGFX_XE_HP_CORE:
-  case IGFX_XE_HPG_CORE:
-  case IGFX_XE_HPC_CORE:
-    if (Product == IGFX_TIGERLAKE_LP)
-      return "TGLLP";
-    if (Product == IGFX_DG1)
-      return "DG1";
-    if (Product == IGFX_ROCKETLAKE)
-      return "RKL";
-    if (Product == IGFX_ALDERLAKE_S)
-      return "ADLS";
-    if (Product == IGFX_ALDERLAKE_P)
-      return "ADLP";
-    if (Product == IGFX_ALDERLAKE_N)
-      return "ADLN";
-    else if (Product == IGFX_XE_HP_SDV)
-      return "XEHP";
-    else if (Product == IGFX_DG2)
-        return "DG2";
-    else if (Product == IGFX_METEORLAKE)
-        return "MTL";
-    else if (Product == IGFX_PVC) {
-      // fixing revision id for PVC to compute tile
-      RevId &= cmc::ComputeTileMaskPVC;
-      if (RevId < REVISION_B)
-        return "PVC";
-      else if (RevId < REVISION_D)
-        return "PVCXT_A0"; // PVC XT A0 RevID==0x3==REVISION_B
-      else
-        return "PVCXT";
-    }
+  if (GmdId.Value != 0) {
+    static std::string PlatformId;
+    std::ostringstream Out;
+    Out << GmdId.GmdID.GMDArch << '.' << GmdId.GmdID.GMDRelease << '.'
+        << GmdId.GmdID.RevisionID;
+    PlatformId = Out.str();
+    return PlatformId.c_str();
+  }
+
+  // If GdmId is not defined, fallback to product id scheme
+  switch (Product) {
+  case IGFX_BROADWELL:
+    return "bdw";
+  case IGFX_SKYLAKE:
+    return "skl";
+  case IGFX_KABYLAKE:
+    return "kbl";
+  case IGFX_COFFEELAKE:
+    return "cfl";
+  case IGFX_BROXTON:
+    return "bxt";
+  case IGFX_GEMINILAKE:
+    return "glk";
+  case IGFX_ICELAKE:
+  case IGFX_ICELAKE_LP:
+    return "icllp";
+  case IGFX_JASPERLAKE:
+    return "jsl";
+  case IGFX_TIGERLAKE_LP:
+    return "tgllp";
+  case IGFX_ROCKETLAKE:
+    return "rkl";
+  case IGFX_ALDERLAKE_S:
+    return "adl-s";
+  case IGFX_ALDERLAKE_P:
+    return "adl-p";
+  case IGFX_ALDERLAKE_N:
+    return "adl-n";
+  case IGFX_DG1:
+    return "dg1";
+  case IGFX_XE_HP_SDV:
+    return "xehp-sdv";
+  case IGFX_DG2:
+    if (GFX_IS_DG2_G10_CONFIG(DeviceId))
+      return "acm-g10";
+    if (GFX_IS_DG2_G11_CONFIG(DeviceId))
+      return "acm-g11";
+    if (GFX_IS_DG2_G12_CONFIG(DeviceId))
+      return "acm-g12";
+    return "dg2";
+  case IGFX_PVC:
+    RevId &= cmc::ComputeTileMaskPVC;
+    if (RevId <= 1)
+      return "pvc-sdv";
+    if (RevId == 3)
+      return "12.60.3";
+    return "pvc";
+  case IGFX_METEORLAKE:
+    return "mtl";
   default:
     break;
   }
+
+  switch (Core) {
+  case IGFX_GEN9_CORE:
+    return "skl";
+  case IGFX_GEN11_CORE:
+    return "icllp";
+  default:
+    break;
+  }
+
   return nullptr;
 }
 
