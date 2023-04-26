6d09bf381fd149eaf30f7ad3880404d17dda460d
Pete Chou <chinyen.chou@intel.com>
2023-04-26 21:03:39 +0000

    Revert vISA scheduler latency refactor.
    Revert vISA scheduler latency refactor.

--- visa/LocalScheduler/SWSB_G4IR.h
+++ visa/LocalScheduler/SWSB_G4IR.h
@@ -921,7 +921,7 @@ class SWSB {
   const unsigned tokenAfterWriteSendSlmCycle;
   const unsigned tokenAfterWriteSendMemoryCycle;
   const unsigned tokenAfterWriteSendSamplerCycle;
-  int tokenAfterDPASCycle = 0;
+  int tokenAfterDPASCycle;
 
   // For profiling
   uint32_t syncInstCount = 0;
@@ -1113,10 +1113,8 @@ public:
     indexes.longIndex = 0;
     indexes.DPASIndex = 0;
     indexes.mathIndex = 0;
-    if (k.fg.builder->hasDPAS()) {
-      tokenAfterDPASCycle =
-          LatencyTable::createLatencyTable(*k.fg.builder)->getDPASLatency(8);
-    }
+    tokenAfterDPASCycle =
+        LatencyTable::createLatencyTable(*k.fg.builder)->getDPASLatency(8);
   }
   ~SWSB() {}
   void SWSBGenerator();
