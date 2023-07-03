a2cb7c202d354ea8c63568949a9dc71c3fcc9033
Jakacki, Jakub <jakub.jakacki@intel.com>
2023-07-03 20:32:08 +0000

    Extend the nomask exception to UAV loads
    Extend nomask the exception to UAV loads

--- IGC/Compiler/CISACodeGen/EmitVISAPass.cpp
+++ IGC/Compiler/CISACodeGen/EmitVISAPass.cpp
@@ -267,8 +267,7 @@ bool EmitPass::IsNoMaskAllowed(SDAG& sdag)
 {
     if (auto* I = dyn_cast<LoadInst>(sdag.m_root))
     {
-        return I->getPointerAddressSpace() != ADDRESS_SPACE_PRIVATE &&
-               I->getPointerAddressSpace() != ADDRESS_SPACE_CONSTANT;
+        return false;
     }
 
     return true;
