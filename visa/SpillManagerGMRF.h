fec3144fde61b98e5f8a31cdffaa23b998022428
Pratik J Ashar <pratik.j.ashar@intel.com>
2023-07-04 10:38:00 +0000

    Treat spilled relocatable operands specially
    When RA spills a variable that is known to always contain a compile time constant, we
    optimize the fill by reloading the constant using a mov rather than
    loading from memory.
    
    However, this optimization is illegal when constant being written in to
    spilled variable is a relocatable value. This change prevents spill/fill
    optimization for such relocatable operands.

--- visa/SpillManagerGMRF.h
+++ visa/SpillManagerGMRF.h
@@ -800,7 +800,8 @@ static bool immFillCandidate(G4_INST *inst) {
   return inst->opcode() == G4_mov && inst->getExecSize() == g4::SIMD1 &&
          inst->getSrc(0)->isImm() && inst->isWriteEnableInst() &&
          inst->getDst()->getType() == inst->getSrc(0)->getType() &&
-         !inst->getPredicate() && !inst->getCondMod() && !inst->getSaturate();
+         !inst->getPredicate() && !inst->getCondMod() && !inst->getSaturate() &&
+         !inst->getSrc(0)->isRelocImm();
 }
 
 G4_SrcRegRegion *getSpillFillHeader(IR_Builder &builder, G4_Declare *decl);
