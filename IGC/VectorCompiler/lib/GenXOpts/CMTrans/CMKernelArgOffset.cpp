5b19d8468e5ec1f2ab1acfad0de04d0cd0f35e77
Victor Mustya <victor.mustya@intel.com>
2023-07-12 17:37:48 +0000

    Fix for VC kernel vector arguments
    .

--- IGC/VectorCompiler/lib/GenXOpts/CMTrans/CMKernelArgOffset.cpp
+++ IGC/VectorCompiler/lib/GenXOpts/CMTrans/CMKernelArgOffset.cpp
@@ -1,6 +1,6 @@
 /*========================== begin_copyright_notice ============================
 
-Copyright (C) 2017-2021 Intel Corporation
+Copyright (C) 2017-2023 Intel Corporation
 
 SPDX-License-Identifier: MIT
 
@@ -592,8 +592,8 @@ void CMKernelArgOffset::processKernelOnOCLRT(Function *F) {
               DL.getPointerABIAlignment(Ty->getPointerAddressSpace()));
         }
       } else {
-        Bytes = Ty->getPrimitiveSizeInBits() / 8;
-        Alignment = IGCLLVM::getAlignmentValue(Ty->getScalarSizeInBits() / 8);
+        Bytes = DL.getTypeSizeInBits(Ty) / 8;
+        Alignment = IGCLLVM::getAlignmentValue(DL.getABITypeAlignment(Ty));
       }
       placeArg(&Arg, Bytes, Alignment);
     }
