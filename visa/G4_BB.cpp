579212d7caeaed3f65e8c4dd26554b88f306fccd
bcheng0127 <bu.qi.cheng@intel.com>
2023-07-24 07:09:11 -0700

    [Autobackout][FuncReg]Revert of change: 86d5f15a89b903ad7b851d67a9bad84453be121f  Enable the SBID WAR dependence localization by default
    Enable the SBID WAR dependence localization by default

--- visa/G4_BB.cpp
+++ visa/G4_BB.cpp
@@ -32,11 +32,7 @@ bool G4_BB::isLastInstEOT() const {
     return false;
   }
 
-  // Scan backward because some pass may insert nop after EOT.
-  auto iter =
-      std::find_if(instList.rbegin(), instList.rend(),
-                   [](G4_INST *inst) { return inst->opcode() != G4_nop; });
-  G4_INST *i = *iter;
+  G4_INST *i = instList.back();
 
   if (parent->builder->hasSendShootdown()) {
     // due to send shootdown, a predicated send may not actually be an EOT
