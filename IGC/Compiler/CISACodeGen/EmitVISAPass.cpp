98bc3ae53f5b68c487f8702ed5c4d8cb5b055241
David Woo <david.j.woo@intel.com>
2023-07-28 00:00:54 -0700

    [Autobackout][FuncReg]Revert of change: a899941906c8bf8f866947d9be77efdcc7b3ee92  Don't add NoMask related to "subspan use" to constant buffer loads
    While a corrupted address can't cause a page fault for a constant
    buffer in the binding table, it can still cause a hang if the address
    is unaligned. This change prevents NoMask from being added to loads
    from constant buffers in the binding table in order to prevent hangs.

--- IGC/Compiler/CISACodeGen/EmitVISAPass.cpp
+++ IGC/Compiler/CISACodeGen/EmitVISAPass.cpp
@@ -269,11 +269,9 @@ bool EmitPass::IsNoMaskAllowed(SDAG& sdag)
 {
     if (auto* I = dyn_cast<LoadInst>(sdag.m_root))
     {
-        BufferType bufType = DecodeBufferType(I->getPointerAddressSpace());
         return I->getPointerAddressSpace() != ADDRESS_SPACE_PRIVATE &&
                I->getPointerAddressSpace() != ADDRESS_SPACE_GLOBAL &&
-               I->getPointerAddressSpace() != ADDRESS_SPACE_CONSTANT &&
-               bufType != CONSTANT_BUFFER;
+               I->getPointerAddressSpace() != ADDRESS_SPACE_CONSTANT;
     }
 
     return true;
