2f0ad598c845817ad79f5f87846ff1dc62a19e8a
Mateusz Chudyk <mateusz.chudyk@intel.com>
2023-08-02 18:37:32 -0700

    [Autobackout][FuncReg]Revert of change: 47b4ae5f226432afb058e97b532e1de4fcdcd834  Handle new format of "Source Lang Literal" metadata
    Handle new format of "Source Lang Literal" metadata

--- IGC/DebugInfo/Utils.hpp
+++ IGC/DebugInfo/Utils.hpp
@@ -36,17 +36,8 @@ static void OrderedTraversal(const ContainerType &Data, BinaryFunction Visit,
 
 constexpr int32_t SOURCE_LANG_LITERAL_MD_IS_NOT_PRESENT = -1;
 
-//
-// Flag "Source Lang Literal" contains sourceLanguage.
-//
-// Example:
-//   !llvm.module.flags = !{..., !3, ...}
-//   ...
-//   !3 = !{i32 2, !"Source Lang Literal", 33}
-//   ...
-//
-inline int32_t getSourceLangLiteralMDValueLegacy(const llvm::Module *module) {
-  auto sourceLangLiteral = module->getModuleFlag("Source Lang Literal");
+inline int32_t getSourceLangLiteralMDValue(const llvm::Module &module) {
+  auto sourceLangLiteral = module.getModuleFlag("Source Lang Literal");
   if (!sourceLangLiteral) {
     return SOURCE_LANG_LITERAL_MD_IS_NOT_PRESENT;
   }
@@ -56,52 +47,9 @@ inline int32_t getSourceLangLiteralMDValueLegacy(const llvm::Module *module) {
       llvm::cast<llvm::ConstantInt>(constant->getValue())->getZExtValue());
 }
 
-//
-// Flag "Source Lang Literal" contains a list (MDTuple) of pairs (MDTuple):
-//   (compileUnit, sourceLanguage)
-//
-// Example:
-//   !llvm.module.flags = !{..., !3, ...}
-//   ...
-//   !3 = !{i32 2, !"Source Lang Literal", !4}
-//   !4 = !{!5, !1834, ...}
-//   !5 = !{!6, i32 33}
-//   !6 = !DICompileUnit(...
-//   ...
-//   !1834 = !{!1835, i32 33}
-//   !1835 = !DICompileUnit(...
-//   ...
-//
-inline int32_t
-getSourceLangLiteralMDValue(const llvm::DICompileUnit *compileUnit,
-                            const llvm::Module *module) {
-  const auto flagName = "Source Lang Literal";
-
-  if (!module->getModuleFlag(flagName)) {
-    return SOURCE_LANG_LITERAL_MD_IS_NOT_PRESENT;
-  }
-
-  auto node = llvm::dyn_cast<llvm::MDTuple>(module->getModuleFlag(flagName));
-  if (!node) {
-    return getSourceLangLiteralMDValueLegacy(module);
-  }
-
-  for (auto &op : node->operands()) {
-    auto entry = llvm::cast<llvm::MDTuple>(op);
-    if (llvm::cast<llvm::DICompileUnit>(entry->getOperand(0)) == compileUnit) {
-      auto constant =
-          llvm::cast<llvm::ConstantAsMetadata>(entry->getOperand(1));
-      return int32_t(
-          llvm::cast<llvm::ConstantInt>(constant->getValue())->getZExtValue());
-    }
-  }
-
-  return SOURCE_LANG_LITERAL_MD_IS_NOT_PRESENT;
-}
-
-inline uint16_t getSourceLanguage(const llvm::DICompileUnit *compileUnit,
+inline uint16_t getSourceLanguage(llvm::DICompileUnit *compileUnit,
                                   const llvm::Module *module) {
-  int32_t sourceLanguage = getSourceLangLiteralMDValue(compileUnit, module);
+  int32_t sourceLanguage = getSourceLangLiteralMDValue(*module);
   if (sourceLanguage == SOURCE_LANG_LITERAL_MD_IS_NOT_PRESENT) {
     sourceLanguage = compileUnit->getSourceLanguage();
   }
