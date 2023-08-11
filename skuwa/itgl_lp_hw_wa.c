2b7886818ea7a504176f3bdc5578ce1e616e3d56
Aleksander Mielczarek <aleksander.mielczarek@intel.com>
2023-08-11 13:18:40 +0000

    Refreshed Workarounds
    Refreshed Workarounds

--- skuwa/itgl_lp_hw_wa.c
+++ skuwa/itgl_lp_hw_wa.c
@@ -18,6 +18,7 @@ void InitTglLpHwWaTable(PWA_TABLE pWaTable, PSKU_FEATURE_TABLE pSkuTable, PWA_IN
 {
         int iStepId_TGL_LP = (int)pWaParam->usRevId;
 
+
     SI_WA_ENABLE(
         Wa_220856683,
         "No Link Provided",
@@ -67,7 +68,23 @@ void InitTglLpHwWaTable(PWA_TABLE pWaTable, PSKU_FEATURE_TABLE pSkuTable, PWA_IN
 
 
     SI_WA_ENABLE(
+        Wa_16013338947,
+        "No Link Provided",
+        "No HWSightingLink provided",
+        PLATFORM_ALL,
+        SI_WA_BETWEEN(iStepId_TGL_LP, TGL_LP_GT_REV_ID_A0, FUTURE_PROJECT));
+
+
+    SI_WA_ENABLE(
         Wa_14017131883,
+        "No Link Provided",
+        "No HWSightingLink provided",
+        PLATFORM_ALL,
+        SI_WA_BETWEEN(iStepId_TGL_LP, TGL_LP_GT_REV_ID_A0, FUTURE_PROJECT));
+
+
+    SI_WA_ENABLE(
+        Wa_14018126777,
         "No Link Provided",
         "No HWSightingLink provided",
         PLATFORM_ALL,
