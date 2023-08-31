49a11664b3ab63222fb76a58c3b6cf4ff0ccb16f
Diana Chen <po-yu.chen@intel.com>
2023-08-31 17:07:25 +0000

    IGA: GEDLibrary update
    GED update to gedVersion = "93b971c4"

--- visa/iga/GEDLibrary/GED_external/build/autogen-intel64/version.cpp
+++ visa/iga/GEDLibrary/GED_external/build/autogen-intel64/version.cpp
@@ -6,4 +6,4 @@ SPDX-License-Identifier: MIT
 
 ============================= end_copyright_notice ===========================*/
 
-const char* gedVersion = "fc796d5a";
+const char* gedVersion = "93b971c4";
