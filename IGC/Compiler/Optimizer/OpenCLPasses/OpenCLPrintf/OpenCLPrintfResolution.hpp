06eeecbc8a4f7695a934fc18c9698514972cd531
Milczek, Szymon <szymon.milczek@intel.com>
2023-09-20 10:48:28 +0000

    Replace empty printf calls with succesful printf ret val
    If a module contains a printf call that has an empty string literal as
    its first argument, replace it with the value that printf retuns on
    success, which is 0. This also fixes an issue with an assert.

--- IGC/Compiler/Optimizer/OpenCLPasses/OpenCLPrintf/OpenCLPrintfResolution.hpp
+++ IGC/Compiler/Optimizer/OpenCLPasses/OpenCLPrintf/OpenCLPrintfResolution.hpp
@@ -108,6 +108,8 @@ namespace IGC
 
         //        llvm::Value* fixupVectorPrintfArg(llvm::CallInst &printfCall, llvm::Value* arg, IGC::SHADER_PRINTF_TYPE argDataType);
 
+        bool isEmptyStringPrintfCall(llvm::CallInst& printfCall);
+
                 // Returns true if a printf argument is string.
         bool argIsString(llvm::Value* printfArg);
 
