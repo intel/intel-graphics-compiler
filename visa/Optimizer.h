5cfc8d5355441335cb1c0bd9277b65e77aba8fdf
Pete Chou <chinyen.chou@intel.com>
2023-10-24 18:12:55 -0700

    [Autobackout][FuncReg]Revert of change: 2d39298420ee376e661cc7ea1308423e8ef88d1b  Reland the WA that emits a NOP after branch instruction to support single stepping in debugger.
    The WA is enabled only when compiling with -O0 currently.

--- visa/Optimizer.h
+++ visa/Optimizer.h
@@ -261,7 +261,6 @@ private:
   void applyNamedBarrierWA(INST_LIST_ITER it, G4_BB *bb);
   void insertIEEEExceptionTrap();
   void expandIEEEExceptionTrap(INST_LIST_ITER it, G4_BB *bb);
-  void insertNopAfterCFInst(BB_LIST_ITER ib, INST_LIST_ITER it);
 
   typedef std::vector<vISA::G4_INST *> InstListType;
   // create instruction sequence to calculate call offset from ip
