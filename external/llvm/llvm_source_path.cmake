60ce2e52a991057952df5a667343202340395e73
Brad Hilton <brad.hilton@intel.com>
2023-11-29 21:36:56 +0000

    Add additional llvm git repo folder to search list
    ---------------------------

--- external/llvm/llvm_source_path.cmake
+++ external/llvm/llvm_source_path.cmake
@@ -9,6 +9,7 @@
 # Use LLVM sources stored at fixed location
 message(STATUS "[LLVM] Current value of CMAKE_CURRENT_SOURCE_DIR : ${CMAKE_CURRENT_SOURCE_DIR}")
 list(APPEND IGC_LLVM_PATHS
+  ${CMAKE_CURRENT_SOURCE_DIR}/../../llvm-project-full_${IGC_OPTION__LLVM_PREFERRED_VERSION}
   ${CMAKE_CURRENT_SOURCE_DIR}/../../llvm-project
   /opt/src/llvm-project_${IGC_OPTION__LLVM_PREFERRED_VERSION}
   ${CMAKE_CURRENT_SOURCE_DIR}/../../llvm-project_${IGC_OPTION__LLVM_PREFERRED_VERSION}
