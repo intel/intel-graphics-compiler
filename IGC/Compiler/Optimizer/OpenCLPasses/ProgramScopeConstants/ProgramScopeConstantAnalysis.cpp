intel/intel-graphics-compiler

commit 4f7756c73295e958bc8dd12b92eb81ec4a6c5022

Address patching for zeroed global variables and i64 offsets
Fix size overflows for .data and .bss zebin sections by using
64 bit integers to calculate them.
Use 64 bits to represent offsets into inlineProgramScopeOffsets,
globalBase and constBase to prevent overflows.

Add address patching support for zeroinitialized global variables.
Previously we cannot do the zero-initializer optimization if the variable is used by another global.
This is because the offsets for the zero-init variables have not been calculated at the time of patching.
With this change, we save the patching info, and patch later after those offsets have been calculated.
This enables the optimization to defer copying zero values to the global/const buffers.

IGC/Compiler/Optimizer/OpenCLPasses/ProgramScopeConstants/ProgramScopeConstantAnalysis.cpp
@@ -187,22 +187,8 @@ bool ProgramScopeConstantAnalysis::runOnModule(Module& M)
 
         if (initializer->isZeroValue())
         {
-            // For zero initialized values, we dont need to copy the data, just tell driver how much to allocate
-            // However, if it's used as a pointer value, we need to do patching and therefore cannot defer the offset calculation
-            bool hasPointerUser = false;
-            for (auto UI : globalVar->users())
-            {
-                if (isa<Constant>(UI) && UI->getType()->isPointerTy())
-                {
-                    hasPointerUser = true;
-                    break;
-                }
-            }
-            if (!hasPointerUser)
-            {
-                zeroInitializedGlobals.push_back(globalVar);
-                continue;
-            }
+            zeroInitializedGlobals.push_back(globalVar);
+            continue;
         }
 
         // Align the buffer.
@@ -243,6 +229,22 @@ bool ProgramScopeConstantAnalysis::runOnModule(Module& M)
 #endif
         inlineProgramScopeOffsets[globalVar] = offset;
         offset += (unsigned)(m_DL->getTypeAllocSize(IGCLLVM::getNonOpaquePtrEltTy(globalVar->getType())));
+    }
+
+    // Patch the offsets for usages of zero initialized globals after those offsets have been calculated in the previous step.
+    // TODO: Remove this logic after enabling ZeBinary, since we will switch the patching to use relocation table instead.
+    for (ZeroInitPatchInfo &patchData : m_PatchLaterDataVector)
+    {
+        char* whereToPatch = std::get<0>(patchData);
+        unsigned patchSize = std::get<1>(patchData);
+        GlobalVariable* globalVar = std::get<2>(patchData);
+        uint64_t offset = std::get<3>(patchData);
+
+        auto iter = inlineProgramScopeOffsets.find(globalVar);
+        IGC_ASSERT(iter != inlineProgramScopeOffsets.end());
+
+        const uint64_t patchOffset = iter->second + offset;
+        memcpy_s(whereToPatch, patchSize, (char*)&patchOffset, patchSize);
     }
 
     if (inlineProgramScopeOffsets.size())
@@ -346,7 +348,7 @@ bool ProgramScopeConstantAnalysis::runOnModule(Module& M)
     const bool changed = !inlineProgramScopeOffsets.empty();
     for (auto offset : inlineProgramScopeOffsets)
     {
-        m_pModuleMd->inlineProgramScopeOffsets[offset.first] = offset.second;
+        m_pModuleMd->inlineProgramScopeOffsets[offset.first] = static_cast<uint64_t>(offset.second);
     }
 
     // Update LLVM metadata based on IGC MetadataUtils
@@ -476,20 +478,26 @@ void ProgramScopeConstantAnalysis::addData(Constant* initializer,
                 }
                 else
                 {
+                    // Add the patching info for runtime
+                    pointerOffsetInfoList.push_back(PointerOffsetInfo(addressSpace, inlineProgramScopeBuffer.size(), pointedToAddrSpace));
+
                     auto iter = inlineProgramScopeOffsets.find(ptrBase);
-                    IGC_ASSERT(iter != inlineProgramScopeOffsets.end());
+                    if (iter != inlineProgramScopeOffsets.end())
+                    {
+                        const uint64_t pointeeOffset = iter->second + offset;
+                        // For old patching logic, write the offset relative to the entire global/constant buffer where the base global resides.
+                        // The base address of the buffer will be added to it at runtime.
+                        inlineProgramScopeBuffer.insert(inlineProgramScopeBuffer.end(), (char*)&pointeeOffset, ((char*)&pointeeOffset) + pointerSize);
+                    }
+                    else
+                    {
+                        // If we can't find the base global variable, it must be a zero initialized value whose data is not directly copied.
+                        // Save the info for now, and patch it later once we have the offsets for this zeroinit global vars.
+                        unsigned patchIdx = inlineProgramScopeBuffer.size();
+                        inlineProgramScopeBuffer.insert(inlineProgramScopeBuffer.end(), pointerSize, 0);
 
-                    const uint64_t pointeeOffset = iter->second + offset;
-
-                    pointerOffsetInfoList.push_back(
-                        PointerOffsetInfo(
-                            addressSpace,
-                            inlineProgramScopeBuffer.size(),
-                            pointedToAddrSpace));
-
-                    // For old patching logic, write the offset relative to the entire global/constant buffer where the base global resides.
-                    // The base address of the buffer will be added to it at runtime.
-                    inlineProgramScopeBuffer.insert(inlineProgramScopeBuffer.end(), (char*)&pointeeOffset, ((char*)&pointeeOffset) + pointerSize);
+                        m_PatchLaterDataVector.push_back(std::make_tuple((char*)(&inlineProgramScopeBuffer[patchIdx]), pointerSize, ptrBase, offset));
+                    }
                 }
             }
             else

IGC/Compiler/Optimizer/OpenCLPasses/ProgramScopeConstants/ProgramScopeConstantResolution.cpp
@@ -126,22 +126,14 @@ bool ProgramScopeConstantResolution::runOnModule(Module& M)
             continue;
         }
 
-        // Get the offset of this constant from the base.
-        int offset = -1;
-
         auto bufferOffset = modMD->inlineProgramScopeOffsets.find(pGlobalVar);
-        if (bufferOffset != modMD->inlineProgramScopeOffsets.end())
-        {
-            offset = bufferOffset->second;
-        }
+        if (bufferOffset == modMD->inlineProgramScopeOffsets.end())
+            continue; // This constant is not used, so it didn't get an offset.
 
-        // This constant is not used, so it didn't get an offset.
-        if (offset == -1)
-        {
-            continue;
-        }
+        // Get the offset of this constant from the base.
+        int64_t offset = bufferOffset->second;
+        ConstantInt* pOffset = ConstantInt::get(Type::getInt64Ty(C), offset);
 
-        ConstantInt* pOffset = ConstantInt::get(Type::getInt32Ty(C), offset);
         const ImplicitArg::ArgType argType =
             AS == ADDRESS_SPACE_GLOBAL ? ImplicitArg::GLOBAL_BASE : ImplicitArg::CONSTANT_BASE;
 

IGC/AdaptorOCL/OCL/sp/zebin_builder.cpp
@@ -193,9 +193,9 @@ void ZEBinaryBuilder::addGlobalConstants(const IGC::SOpenCLProgramInfo& annotati
     if (annotations.m_initConstantAnnotation && annotations.m_initConstantAnnotation->AllocSize) {
         auto& ca = annotations.m_initConstantAnnotation;
         // the normal .data.const size
-        uint32_t dataSize = ca->InlineData.size();
+        uint64_t dataSize = ca->InlineData.size();
         // the zero-initialize variables size, the .bss.const size
-        uint32_t bssSize = ca->AllocSize - dataSize;
+        uint64_t bssSize = ca->AllocSize - dataSize;
         uint32_t alignment = ca->Alignment;
 
         if (IGC_IS_FLAG_ENABLED(AllocateZeroInitializedVarsInBss)) {
@@ -220,8 +220,9 @@ void ZEBinaryBuilder::addGlobalConstants(const IGC::SOpenCLProgramInfo& annotati
         } else {
             // before runtime can support bss section, we create all 0s in .const.data section by adding
             // bssSize of padding
+            IGC_ASSERT_MESSAGE(bssSize == static_cast<uint32_t>(bssSize), ".const.data padding size overflows 32 bits");
             mGlobalConstSectID = mBuilder.addSectionData("const", (const uint8_t*)ca->InlineData.data(),
-                dataSize, bssSize, alignment, /*rodata*/true);
+                dataSize, static_cast<uint32_t>(bssSize), alignment, /*rodata*/true);
         }
     }
 
@@ -248,8 +249,8 @@ void ZEBinaryBuilder::addGlobals(const IGC::SOpenCLProgramInfo& annotations)
     if (!ca->AllocSize)
         return;
 
-    uint32_t dataSize = ca->InlineData.size();
-    uint32_t bssSize = ca->AllocSize - dataSize;
+    uint64_t dataSize = ca->InlineData.size();
+    uint64_t bssSize = ca->AllocSize - dataSize;
     uint32_t alignment = ca->Alignment;
 
     if (IGC_IS_FLAG_ENABLED(AllocateZeroInitializedVarsInBss)) {
@@ -271,8 +272,9 @@ void ZEBinaryBuilder::addGlobals(const IGC::SOpenCLProgramInfo& annotations)
     } else {
         // before runtime can support bss section, we create all 0s in .global.data section by adding
         // bssSize of padding
+        IGC_ASSERT_MESSAGE(bssSize == static_cast<uint32_t>(bssSize), ".global.data padding size overflows 32 bits");
         mGlobalSectID = mBuilder.addSectionData("global", (const uint8_t*)ca->InlineData.data(),
-            dataSize, bssSize, alignment, /*rodata*/false);
+            dataSize, static_cast<uint32_t>(bssSize), alignment, /*rodata*/false);
     }
 }
 

IGC/Compiler/Optimizer/OpenCLPasses/ProgramScopeConstants/ProgramScopeConstantAnalysis.hpp
@@ -54,7 +54,7 @@ namespace IGC
 
     protected:
         typedef std::vector<unsigned char> DataVector;
-        typedef llvm::MapVector<llvm::GlobalVariable*, int> BufferOffsetMap;
+        typedef llvm::MapVector<llvm::GlobalVariable*, int64_t> BufferOffsetMap;
 
         struct PointerOffsetInfo
         {
@@ -91,6 +91,10 @@ namespace IGC
 
         const llvm::DataLayout* m_DL;
         ModuleMetaData* m_pModuleMd;
+
+        // Used to patch offsets for zero initialized globals
+        typedef std::tuple<char*, unsigned, llvm::GlobalVariable*, uint64_t> ZeroInitPatchInfo;
+        std::vector<ZeroInitPatchInfo> m_PatchLaterDataVector;
     };
 
 } // namespace IGC

IGC/Compiler/tests/ProgramScopeConstantAnalysis/basic.ll
@@ -66,8 +66,8 @@ entry:
 ; CHECK-DAG: [[SYMBOL]] = !{!"Symbol", !"a"}
 ; CHECK-DAG: !{!"inlineProgramScopeOffsets", [[INLINE_PROGRAM_SCOPE_OFFSETS_MAP0:![0-9]*]], [[INLINE_PROGRAM_SCOPE_OFFSETS_VALUE0:![0-9]*]], [[INLINE_PROGRAM_SCOPE_OFFSETS_MAP1:![0-9]*]], [[INLINE_PROGRAM_SCOPE_OFFSETS_VALUE1:![0-9]*]], [[INLINE_PROGRAM_SCOPE_OFFSETS_MAP2:![0-9]*]], [[INLINE_PROGRAM_SCOPE_OFFSETS_VALUE2:![0-9]*]]}
 ; CHECK-DAG: [[INLINE_PROGRAM_SCOPE_OFFSETS_MAP0]] = !{!"inlineProgramScopeOffsetsMap[0]", [2 x i32] addrspace(2)* @a}
-; CHECK-DAG: [[INLINE_PROGRAM_SCOPE_OFFSETS_VALUE0]] = !{!"inlineProgramScopeOffsetsValue[0]", i32 0}
+; CHECK-DAG: [[INLINE_PROGRAM_SCOPE_OFFSETS_VALUE0]] = !{!"inlineProgramScopeOffsetsValue[0]", i64 0}
 ; CHECK-DAG: [[INLINE_PROGRAM_SCOPE_OFFSETS_MAP1]] = !{!"inlineProgramScopeOffsetsMap[1]", i32 addrspace(2)* addrspace(1)* @d}
-; CHECK-DAG: [[INLINE_PROGRAM_SCOPE_OFFSETS_VALUE1]] = !{!"inlineProgramScopeOffsetsValue[1]", i32 0}
+; CHECK-DAG: [[INLINE_PROGRAM_SCOPE_OFFSETS_VALUE1]] = !{!"inlineProgramScopeOffsetsValue[1]", i64 0}
 ; CHECK-DAG: [[INLINE_PROGRAM_SCOPE_OFFSETS_MAP2]] = !{!"inlineProgramScopeOffsetsMap[2]", i32 addrspace(1)* @c}
-; CHECK-DAG: [[INLINE_PROGRAM_SCOPE_OFFSETS_VALUE2]] = !{!"inlineProgramScopeOffsetsValue[2]", i32 8}
+; CHECK-DAG: [[INLINE_PROGRAM_SCOPE_OFFSETS_VALUE2]] = !{!"inlineProgramScopeOffsetsValue[2]", i64 8}

IGC/Compiler/tests/ProgramScopeConstantResolution/basic.ll
@@ -29,11 +29,11 @@
 define spir_kernel void @test_program(i32 addrspace(1)* %dst, <8 x i32> %r0, <8 x i32> %payloadHeader, i8 addrspace(2)* %constBase, i8 addrspace(1)* %globalBase, i8* %privateBase, i32 %bufferOffset) #0 {
 ; CHECK-LABEL: @test_program(
 ; CHECK:  entry:
-; CHECK:    [[OFFC:%.*]] = getelementptr i8, i8 addrspace(1)* %globalBase, i32 8
+; CHECK:    [[OFFC:%.*]] = getelementptr i8, i8 addrspace(1)* %globalBase, i64 8
 ; CHECK:    [[CASTC:%.*]] = bitcast i8 addrspace(1)* [[OFFC]] to i32 addrspace(1)*
-; CHECK:    [[OFFD:%.*]] = getelementptr i8, i8 addrspace(1)* %globalBase, i32 0
+; CHECK:    [[OFFD:%.*]] = getelementptr i8, i8 addrspace(1)* %globalBase, i64 0
 ; CHECK:    [[CASTD:%.*]] = bitcast i8 addrspace(1)* [[OFFD]] to i32 addrspace(2)* addrspace(1)*
-; CHECK:    [[OFFA:%.*]] = getelementptr i8, i8 addrspace(2)* %constBase, i32 0
+; CHECK:    [[OFFA:%.*]] = getelementptr i8, i8 addrspace(2)* %constBase, i64 0
 ; CHECK:    [[CASTA:%.*]] = bitcast i8 addrspace(2)* [[OFFA]] to [2 x i32] addrspace(2)*
 ; CHECK:    [[DST_ADDR:%.*]] = alloca i32 addrspace(1)*, align 8
 ; CHECK:    [[AA:%.*]] = alloca i32, align 4

IGC/common/MDFrameWork.h
@@ -692,7 +692,7 @@ namespace IGC
         std::vector<uint32_t> RasterizerOrderedByteAddressBuffer;
         std::set<uint32_t> RasterizerOrderedViews;
         unsigned int MinNOSPushConstantSize = 0;
-        llvm::MapVector<llvm::GlobalVariable*, int> inlineProgramScopeOffsets;
+        llvm::MapVector<llvm::GlobalVariable*, uint64_t> inlineProgramScopeOffsets;
         ShaderData shaderData;
         URBLayoutInfo URBInfo;
         bool UseBindlessImage = false;

commit 469ae7d876748bfbd972a31739ea71db03fb940a

Fix a bug in ProgramScopeConstantResolution
Revert a bug in ProgramScopeConstantResolution.cpp introduced by
erroneous refactor in commit 4f7756c73295e958bc8dd12b92eb81ec4a6c5022
We need to reach `continue` statement when
bufferOffset->second is equal to -1.

IGC/Compiler/Optimizer/OpenCLPasses/ProgramScopeConstants/ProgramScopeConstantAnalysis.cpp
@@ -101,9 +101,9 @@ bool ProgramScopeConstantAnalysis::runOnModule(Module& M)
             }
             // Include the variable in 'inlineProgramScopeOffsets', otherwise
             // code gen will not emit relocation. In case of S_UNDEF/extenal
-            // variable offset is not expected to be used. Using -1 as a
-            // placeholder so the unexpected usage will fail visibly when
-            // trying to emit variable with 0xffffffff offset
+            // variable offset is not expected to be used.
+            // Using -1 to communicate to ProgramScopeConstantResolution that
+            // this offset shouldn't be resolved.
             inlineProgramScopeOffsets[globalVar] = -1;
             continue;
         }

IGC/Compiler/Optimizer/OpenCLPasses/ProgramScopeConstants/ProgramScopeConstantResolution.cpp
@@ -1,6 +1,6 @@
 /*========================== begin_copyright_notice ============================
 
-Copyright (C) 2017-2021 Intel Corporation
+Copyright (C) 2017-2023 Intel Corporation
 
 SPDX-License-Identifier: MIT
 
@@ -132,8 +132,10 @@ bool ProgramScopeConstantResolution::runOnModule(Module& M)
 
         // Get the offset of this constant from the base.
         int64_t offset = bufferOffset->second;
-        ConstantInt* pOffset = ConstantInt::get(Type::getInt64Ty(C), offset);
+        if (offset == -1)
+            continue;
 
+        ConstantInt* pOffset = ConstantInt::get(Type::getInt64Ty(C), offset);
         const ImplicitArg::ArgType argType =
             AS == ADDRESS_SPACE_GLOBAL ? ImplicitArg::GLOBAL_BASE : ImplicitArg::CONSTANT_BASE;
 

IGC/Compiler/tests/ProgramScopeConstantResolution/offset_minus_one.ll
@@ -1,23 +1,28 @@
 ;=========================== begin_copyright_notice ============================
 ;
-; Copyright (C) 2022-2023 Intel Corporation
+; Copyright (C) 2023 Intel Corporation
 ;
 ; SPDX-License-Identifier: MIT
 ;
 ;============================ end_copyright_notice =============================
 ; REQUIRES: regkeys
 ;
-; RUN: igc_opt -regkey EnableZEBinary=0 -enable-debugify --igc-programscope-constant-resolve -S < %s 2>&1 | FileCheck %s --check-prefixes=CHECK,%LLVM_DEPENDENT_CHECK_PREFIX%
+; RUN: igc_opt -regkey EnableZEBinary=0 -enable-debugify --igc-programscope-constant-resolve -S < %s 2>&1 | FileCheck %s
 ; ------------------------------------------------
 ; ProgramScopeConstantResolution
 ; ------------------------------------------------
 
-; Debug-info related checks
+; CHECK: CheckModuleDebugify: PASS
+
+; This test is based on basic.ll but checks that we skip constant resolution when
+; inlineProgramScopeOffsetsValue[...] is set to -1.
+; There is at least one case in ProgramScopeConstantAnalysis pass where we need to include
+; a variable in 'inlineProgramScopeOffsets' for the purposes of codegen emitting a relocation
+; at a later point. We do not want to use the offset value itself so we put -1 there.
 ;
-; For llvm 14 check-debugify treats missing debug location on globalbase getter
-; at the begining of BB as a warning, while on earlier llvm versions its treated as an error.
-;
-; CHECK-LLVM-14-PLUS: CheckModuleDebugify: PASS
+; Here is an example taken from a shader dump:
+; !373 = !{!"inlineProgramScopeOffsetsMap[0]", %struct.kmp_program_data addrspace(1)* @__omp_spirv_program_data}
+; !374 = !{!"inlineProgramScopeOffsetsValue[0]", i64 -1}
 
 @a = internal addrspace(2) constant [2 x i32] [i32 0, i32 1], align 4
 @d = internal addrspace(1) global i32 addrspace(2)* getelementptr inbounds ([2 x i32], [2 x i32] addrspace(2)* @a, i32 0, i32 0), align 8
@@ -29,21 +34,15 @@
 define spir_kernel void @test_program(i32 addrspace(1)* %dst, <8 x i32> %r0, <8 x i32> %payloadHeader, i8 addrspace(2)* %constBase, i8 addrspace(1)* %globalBase, i8* %privateBase, i32 %bufferOffset) {
 ; CHECK-LABEL: @test_program(
 ; CHECK:  entry:
-; CHECK:    [[OFFC:%.*]] = getelementptr i8, i8 addrspace(1)* %globalBase, i64 8
-; CHECK:    [[CASTC:%.*]] = bitcast i8 addrspace(1)* [[OFFC]] to i32 addrspace(1)*
-; CHECK:    [[OFFD:%.*]] = getelementptr i8, i8 addrspace(1)* %globalBase, i64 0
-; CHECK:    [[CASTD:%.*]] = bitcast i8 addrspace(1)* [[OFFD]] to i32 addrspace(2)* addrspace(1)*
-; CHECK:    [[OFFA:%.*]] = getelementptr i8, i8 addrspace(2)* %constBase, i64 0
-; CHECK:    [[CASTA:%.*]] = bitcast i8 addrspace(2)* [[OFFA]] to [2 x i32] addrspace(2)*
 ; CHECK:    [[DST_ADDR:%.*]] = alloca i32 addrspace(1)*, align 8
 ; CHECK:    [[AA:%.*]] = alloca i32, align 4
 ; CHECK:    store i32 addrspace(1)* [[DST:%.*]], i32 addrspace(1)** [[DST_ADDR]], align 8
-; CHECK:    [[TMP0:%.*]] = getelementptr inbounds [2 x i32], [2 x i32] addrspace(2)* [[CASTA]], i64 0, i64 1
+; CHECK:    [[TMP0:%.*]] = getelementptr inbounds [2 x i32], [2 x i32] addrspace(2)* @a, i64 0, i64 1
 ; CHECK:    [[TMP1:%.*]] = load i32, i32 addrspace(2)* [[TMP0]], align 4
 ; CHECK:    store i32 [[TMP1]], i32* [[AA]], align 4
-; CHECK:    [[TMP2:%.*]] = load i32 addrspace(2)*, i32 addrspace(2)* addrspace(1)* [[CASTD]], align 8
+; CHECK:    [[TMP2:%.*]] = load i32 addrspace(2)*, i32 addrspace(2)* addrspace(1)* @d, align 8
 ; CHECK:    [[TMP3:%.*]] = load i32, i32 addrspace(2)* [[TMP2]], align 4
-; CHECK:    store i32 [[TMP3]], i32 addrspace(1)* [[CASTC]], align 4
+; CHECK:    store i32 [[TMP3]], i32 addrspace(1)* @c, align 4
 ; CHECK:    ret void
 ;
 entry:
@@ -65,11 +64,11 @@ entry:
 !0 = !{!"ModuleMD", !1}
 !1 = !{!"inlineProgramScopeOffsets", !2, !3, !4, !5, !6, !7}
 !2 = !{!"inlineProgramScopeOffsetsMap[0]", [2 x i32] addrspace(2)* @a}
-!3 = !{!"inlineProgramScopeOffsetsValue[0]", i64 0}
+!3 = !{!"inlineProgramScopeOffsetsValue[0]", i64 -1}
 !4 = !{!"inlineProgramScopeOffsetsMap[1]", i32 addrspace(1)* @c}
-!5 = !{!"inlineProgramScopeOffsetsValue[1]", i64 8}
+!5 = !{!"inlineProgramScopeOffsetsValue[1]", i64 -1}
 !6 = !{!"inlineProgramScopeOffsetsMap[2]", i32 addrspace(2)* addrspace(1)* @d}
-!7 = !{!"inlineProgramScopeOffsetsValue[2]", i64 0}
+!7 = !{!"inlineProgramScopeOffsetsValue[2]", i64 -1}
 
 !20 = !{void (i32 addrspace(1)*, <8 x i32>, <8 x i32>, i8 addrspace(2)*, i8 addrspace(1)*, i8*, i32)* @test_program, !21}
 !21 = !{!22, !23}