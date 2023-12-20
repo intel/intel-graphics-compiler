ccf0181bfe03292bdb41775c6a141853fef5d9d7
Andrzej Ratajewski <andrzej.ratajewski@intel.com>
2023-12-20 11:20:21 +0000

    Versioning mechanism for Indirect Access Detection
    An indirect access happens when GPU loads from an address that was not
    directly given as one of the kernel arguments. It's usually a pointer
    loaded from memory pointed by a kernel argument. IGC has an analysis
    that is able to detect whether any indirect access is used in a kernel.
    If there are no indirect accesses, then IGC passes this information to
    Neo through ZEBin. Based on that information Neo can disable indirect
    accesses which results in performance improvements.
    
    Since the Indirect Access Detection feature was implemented, there were
    several bugs detected in IGC indirect accesses analysis implementation.
    IGC was not able to detect some cases of indirect accesses, therefore
    falsely reporting to Neo that they are not used, resulting in functional
    issues.
    
    All the reported issues have been fixed in IGC, but kernel binaries that
    were AOT-compiled with faulty IGC version, still have a misleading info
    in ZEBinary that indirect accesses aren't used, even though they are.
    On the other hang, Neo doesn't know whether a particular kernel binary
    was compiled with a faulty IGC or not, therefore it cannot decide if
    recompilation is necessary.
    
    This change introduces a new note to `.note.intelgt.compat` ZEBin's section:
    `NT_INTELGT_INDIRECT_ACCESS_DETECTION_VERSION`
    
    `NT_INTELGT_INDIRECT_ACCESS_DETECTION_VERSION` indicates with what version
    of Indirect Access Detection mechanism, a kernel was compiled.
    If Neo knows that the newest version of the detection mechanism is `2`,
    then it can trigger recompilation of all AOT-compiled kernels with version
    lower than `2`.
    
    Example output from `readelf`:
    ```
    Displaying notes found in: .note.intelgt.compat
      Owner                Data size        Description
      IntelGT              0x00000004       NT_VERSION (version)
       description data: f6 04 00 00
      IntelGT              0x00000004       NT_ARCH (architecture)
       description data: 07 0c 00 00
      IntelGT              0x00000004       NT_INTELGT_INDIRECT_ACCESS_DETECTION_VERSION
       description data: 01 00 00 00
    ```
    
    The open question remains:
    When should `INDIRECT_ACCESS_DETECTION_VERSION` be updated?
    
    Indirect Access Detection mechanism is spread in IGC across several
    different places:
    1. `ScalarArgAsPointer` pass.
    2. `CountStatelessIndirectAccess` function in `EmitVISAPass`.
    3. `StatelessToStateful` pass - potentially deprecated as it's under
       `DumpHasNonKernelArgLdSt` flag which is disabled by default.
    
    There is no easy way to notify an IGC developers that the mechanism
    version should be updated, in case they implement fixes in any of these
    places. We may consider unifying the mechanism implementation into one
    place and implement a git hook that prints a warning in case someone makes
    changes there.
    
    (cherry picked from commit bf1d7c192aa421a5d09ba7e984a74e96f38c4468)

---
+++ IGC/AdaptorOCL/ocl_igc_shared/indirect_access_detection/version.h
@@ -0,0 +1,16 @@
+/*========================== begin_copyright_notice ============================
+
+Copyright (C) 2023 Intel Corporation
+
+SPDX-License-Identifier: MIT
+
+============================= end_copyright_notice ===========================*/
+
+// This file stores version of indirect access detection mechanism that is supported
+// by IGC. The version is used to generate .note.intelgt.compat in ZEBinary. It is also
+// shared with Neo, so that a decision whether a kernel binary requires recompilation can
+// be made.
+
+#pragma once
+
+const uint32_t INDIRECT_ACCESS_DETECTION_VERSION = 1;
