f8fdc34e8029b699595a5a387b0e99517eff0a1e
Victor Mustya <victor.mustya@intel.com>
2023-12-20 23:37:54 -0800

    [Autobackout][FuncReg]Revert of change: 93a6827934c8fd6cb2362519ae2ec2d1f0e9c65b  Replace bfloat16 convert VC intrinsics with llvm instructions
    LLVM supports `bfloat` type to be used with `fpext` and `fptrunc`
    instructions.

--- IGC/VectorCompiler/lib/GenXOpts/CMTrans/GenXTranslateIntrinsics.cpp
+++ IGC/VectorCompiler/lib/GenXOpts/CMTrans/GenXTranslateIntrinsics.cpp
@@ -180,23 +180,6 @@ Value *GenXTranslateIntrinsics::translateBFloat16Convert(CallInst &I) const {
   auto *ArgETy = ArgTy;
   auto *RetTy = I.getType();
 
-#if LLVM_VERSION_MAJOR >= 11
-  auto *BFloatTy = Builder.getBFloatTy();
-  if (auto *ArgVTy = dyn_cast<IGCLLVM::FixedVectorType>(ArgTy)) {
-    ArgETy = ArgVTy->getElementType();
-    BFloatTy =
-        IGCLLVM::FixedVectorType::get(BFloatTy, ArgVTy->getNumElements());
-  }
-
-  Value *NewI = nullptr;
-
-  if (ArgETy->isHalfTy()) {
-    auto *Cast = Builder.CreateBitCast(Arg, BFloatTy);
-    NewI = Builder.CreateFPExt(Cast, RetTy);
-  } else {
-    NewI = Builder.CreateFPTrunc(Arg, BFloatTy);
-  }
-#else  // LLVM_VERSION_MAJOR >= 11
   Type *I16Ty = Builder.getInt16Ty();
 
   if (auto *ArgVTy = dyn_cast<IGCLLVM::FixedVectorType>(ArgTy)) {
@@ -215,9 +198,8 @@ Value *GenXTranslateIntrinsics::translateBFloat16Convert(CallInst &I) const {
   }
 
   auto *NewI = Builder.CreateCall(Func, {Arg});
-#endif // LLVM_VERSION_MAJOR >= 11
-
   LLVM_DEBUG(dbgs() << "Created: " << *NewI << "\n");
+
   return Builder.CreateBitCast(NewI, RetTy);
 }
 
