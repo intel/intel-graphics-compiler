866a241a2cac419033693de7bb07ed894575137e
Kotynia, Piotr <piotr.kotynia@intel.com>
2024-01-03 16:45:17 +0000

    Move debug loc from empty BB to succesor.
    This fix covers the following scenario:
    
    BB1:
      br label %BB1, !dbg !123
    BB2:
      ret void
    
    !123 = !DILocation(line: 1, column: 1, scope: !2)
    
    The above example should be transformed to:
    
    BB1:
      br label %BB1
    BB2:
      ret void, !dbg !123
    
    !123 = !DILocation(line: 1, column: 1, scope: !2)
    
    Because BB with only 1 'br' instruction is treated as empty and it wil not be emitted to VISA.
    We could lose the debug location in this way.

--- IGC/Compiler/CISACodeGen/BlockCoalescing.cpp
+++ IGC/Compiler/CISACodeGen/BlockCoalescing.cpp
@@ -169,7 +169,17 @@ namespace IGC
         while (IsEmptyBlock(block))
         {
             IGC_ASSERT(block->getTerminator()->getNumSuccessors() == 1);
+            Instruction &last = *block->getTerminator();
             block = block->getNextNode();
+            Instruction &first = *block->begin();
+
+            // If the debug loc is set on an empty BB, move it to the first instruction of the successor.
+            // This will not work if the first instruction of BB already has a loc.
+            if (last.getDebugLoc().get() && !first.getDebugLoc().get())
+            {
+                first.setDebugLoc(last.getDebugLoc());
+                last.setDebugLoc(DebugLoc());
+            }
         }
         return block;
     }
