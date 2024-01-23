b973d4e065521ae700578f2940a21b9706f4b950
Andrzej Ratajewski <andrzej.ratajewski@intel.com>
2024-01-23 09:14:33 +0000

    Partially revert 603b9dd
    Revert the change merged in 603b9dd due to regressions

--- IGC/Compiler/CISACodeGen/EmitVISAPass.cpp
+++ IGC/Compiler/CISACodeGen/EmitVISAPass.cpp
@@ -18994,7 +18994,6 @@ void EmitPass::emitPushFrameToStack(unsigned& pushSize)
             }
 
             pFP = ReAlignUniformVariable(pFP, EALIGN_GRF);
-            pOldFP = ReAlignUniformVariable(pOldFP, EALIGN_GRF);
             bool useA64 = (pFP->GetSize() == 8);
             if (shouldGenerateLSC())
             {
