c674d8a9d71e9f4e8febae83e73a608f092d4220
Merecki, Mariusz <mariusz.merecki@intel.com>
2024-01-24 17:17:09 +0000

    Add optimization to shrink array-of-vector allocas
    Add optimization to remove unused vector elements in array-of-vector allocas

---
+++ IGC/Compiler/ShrinkArrayAlloca.h
@@ -0,0 +1,59 @@
+/*========================== begin_copyright_notice ============================
+
+Copyright (C) 2024 Intel Corporation
+
+SPDX-License-Identifier: MIT
+
+============================= end_copyright_notice ===========================*/
+
+#pragma once
+
+#include "common/LLVMWarningsPush.hpp"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Pass.h"
+#include "common/LLVMWarningsPop.hpp"
+
+namespace IGC
+{
+////////////////////////////////////////////////////////////////////////////////
+/// This pass analyzes all uses of array-of-vector allocas to find unused
+/// vector elements. The type of alloca is modified to remove the unused vector
+/// elements, e.g., the following piece of IR:
+///
+///   %alloca = alloca [64 x <4 x float>], align 16
+///   %addr = getelementptr [64 x <4 x float>], [64 x <4 x float>]* %alloca, i32 0, i32 %index
+///   %vec = load <4 x float>, <4 x float>* %addr1
+///   %a = extractelement <4 x float> %vec1, i64 2
+///   ret float %a
+///
+/// is transformed to:
+///
+///   %alloca = alloca [64 x float], align 16
+///   %addr = getelementptr [64 x float], [64 x float]* %alloca, i32 0, i32 %index
+///   %a = load float, float* %addr1
+///   ret float %a
+///////////////////////////////////////////////////////////////////////////////
+class ShrinkArrayAllocaPass : public llvm::FunctionPass
+{
+public:
+    static char ID;
+
+    ShrinkArrayAllocaPass();
+    ~ShrinkArrayAllocaPass() {};
+
+    virtual bool runOnFunction(llvm::Function& function) override;
+
+    virtual llvm::StringRef getPassName() const override;
+
+private:
+    void GatherAllocas(llvm::Function& F);
+    bool Resolve();
+
+private:
+    using UsageInfo = llvm::SmallVector<bool, 4>;
+    llvm::SmallVector<std::pair<llvm::AllocaInst*, UsageInfo>, 4> m_AllocaInfo;
+};
+
+void initializeShrinkArrayAllocaPassPass(llvm::PassRegistry&);
+} // namespace IGC
+
---
+++ IGC/Compiler/tests/ShrinkArrayAlloca/vec4-to-scalar-bitcast.ll
@@ -0,0 +1,30 @@
+;=========================== begin_copyright_notice ============================
+;
+; Copyright (C) 2024 Intel Corporation
+;
+; SPDX-License-Identifier: MIT
+;
+;============================ end_copyright_notice =============================
+
+; RUN: igc_opt %s -S --shrink-array-alloca | FileCheck %s
+;
+
+define float @f0(i32 %index) #1 {
+Label-0:
+  %alloca = alloca [64 x <4 x float>], align 16
+; CHECK:        [[ALLOCA:%[A-z0-9]*]] = alloca [64 x float], align 4
+  %addr = getelementptr [64 x <4 x float>], [64 x <4 x float>]* %alloca, i32 0, i32 %index
+; CHECK:        [[ADDR:%[A-z0-9]*]] = getelementptr [64 x float], [64 x float]* [[ALLOCA]], i32 0, i32 %index
+  %addr1 = bitcast <4 x float>* %addr to <4 x i32>*
+; CHECK:        [[ADDR1:%[A-z0-9]*]] = bitcast float* [[ADDR]] to i32*
+  %vec = load <4 x i32>, <4 x i32>* %addr1
+; CHECK:        [[A:%[A-z0-9]*]] = load i32, i32* [[ADDR1]], align 4
+  %vec1 = bitcast <4 x i32> %vec to <4 x float>
+; CHECK:        [[A1:%[A-z0-9]*]] = bitcast i32 [[A]] to float
+  %a = extractelement <4 x float> %vec1, i64 2
+  ret float %a
+; CHECK:        ret float [[A1]]
+}
+
+attributes #1 = { "null-pointer-is-valid"="true" }
+
