981506e6431f2e1c99a44d93534d11463098e0b6
Paige, Alexander <alexander.paige@intel.com>
2024-03-18 21:42:33 +0000

    Adding new enums and structs in CodeGenPublic
    This change adds needed new enums and structs in CodeGenPublic

--- IGC/Compiler/CodeGenPublic.h
+++ IGC/Compiler/CodeGenPublic.h
@@ -414,11 +414,19 @@ namespace IGC
         int  DerivedConstantsOffset = -1;
     };
 
-
     struct SResInfoFoldingOutput
     {
         uint32_t textureID;
         bool value[4];
+    };
+
+    struct CustomResourceOutput
+    {
+        unsigned regIndex;
+        unsigned regSpace;
+        int rangeSize;
+        DXResourceClass resType;
+        DXResourceKind resShape;
     };
 
     enum SIMDInfoBit
@@ -471,6 +479,7 @@ namespace IGC
 
         bool         hasEvalSampler = false;
         std::vector<SResInfoFoldingOutput> m_ResInfoFoldingOutput;
+
         // GenUpdateCB outputs
         void*       m_ConstantBufferReplaceShaderPatterns = nullptr;
         uint        m_ConstantBufferReplaceShaderPatternsSize = 0;
