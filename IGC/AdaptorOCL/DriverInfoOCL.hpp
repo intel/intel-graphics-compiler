3d5ac432382a8303ca3dc75ecb32a4e542390317
Pete Chou <chinyen.chou@intel.com>
2024-05-29 18:17:57 +0000

    n/a
    n/a

--- IGC/AdaptorOCL/DriverInfoOCL.hpp
+++ IGC/AdaptorOCL/DriverInfoOCL.hpp
@@ -67,6 +67,8 @@ namespace TC
         bool RespectPerInstructionContractFlag() const override { return true; }
 
         bool EnableLSCForLdRawAndStoreRawOnDG2() const override { return true; }
+
+        bool supportsLogicalSSIDInHWTID() const override { return true; }
     };
 
     // In case some cpas are specific to NEO
