8e50c90bb24cbc86c1f3bd464ea039f583ed8339
Wesierski, Lukasz <lukasz.wesierski@intel.com>
2024-12-16 12:58:15 +0000

    Enable vector8 load/store by default.
    Enable vector8 load/store by default to all platforms supporting this feature on OCL path.

--- IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
+++ IGC/Compiler/CISACodeGen/ShaderCodeGen.cpp
@@ -768,7 +768,7 @@ void AddLegalizationPasses(CodeGenContext& ctx, IGCPassManager& mpm, PSSignature
         bool AllowVector8LoadStore =
             IGC_IS_FLAG_ENABLED(EnableVector8LoadStore) ||
             ((ctx.type == ShaderType::RAYTRACING_SHADER || ctx.hasSyncRTCalls()) && ctx.platform.supports8DWLSCMessage()) ||
-            ctx.platform.getPlatformInfo().eProductFamily == IGFX_PVC;
+            (ctx.platform.isCoreChildOf(IGFX_XE_HPC_CORE) && ctx.platform.supports8DWLSCMessage());
 
         mpm.add(createMemOptPass(AllowNegativeSymPtrsForLoad, AllowVector8LoadStore));
 
