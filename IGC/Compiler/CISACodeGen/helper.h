0ea9c160093ddcc04423ab6cc017ba344fff6424
Merecki, Mariusz <mariusz.merecki@intel.com>
2024-12-17 15:41:48 +0000

    Add `BINDLESS_READONLY` and `BINLDESS_WRITEONLY` resource types
    New resource types allow to specify a read-only and write-only images.

--- IGC/Compiler/CISACodeGen/helper.h
+++ IGC/Compiler/CISACodeGen/helper.h
@@ -235,6 +235,8 @@ namespace IGC
     {
         return t == BINDLESS ||
             t == BINDLESS_CONSTANT_BUFFER ||
+            t == BINDLESS_READONLY ||
+            t == BINDLESS_WRITEONLY ||
             t == BINDLESS_TEXTURE;
     }
     inline bool IsSSHbindless(BufferType t)
@@ -261,6 +263,8 @@ namespace IGC
             t == CONSTANT_BUFFER ||
             t == BINDLESS ||
             t == BINDLESS_CONSTANT_BUFFER ||
+            t == BINDLESS_READONLY ||
+            t == BINDLESS_WRITEONLY ||
             t == SSH_BINDLESS ||
             t == SSH_BINDLESS_CONSTANT_BUFFER;
     }
