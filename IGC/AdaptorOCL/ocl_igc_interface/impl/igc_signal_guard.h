8f348362fe1faddb5317053e683325f85ce88d24
Stefan Ilic <stefan.ilic@intel.com>
2025-02-06 13:39:10 +0000

    Don't partially set signal handlers
    If some of the signal handlers are already set we can get into situation
    where we can not clear our handlers. This can lead to infinite loops.

--- IGC/AdaptorOCL/ocl_igc_interface/impl/igc_signal_guard.h
+++ IGC/AdaptorOCL/ocl_igc_interface/impl/igc_signal_guard.h
@@ -9,28 +9,23 @@ SPDX-License-Identifier: MIT
 #ifndef IGC_SIGNAL_GUARD_H
 #define IGC_SIGNAL_GUARD_H
 
-#include <setjmp.h>
-#include <signal.h>
+#include <csignal>
 
-namespace IGC {
-namespace detail {
+namespace IGC::detail {
 class SignalGuard {
 public:
   SignalGuard(int Signal, void (*Handler)(int, siginfo_t *, void *))
       : Signal(Signal) {
     sigaction(Signal, nullptr, &SAOld);
-    if (SAOld.sa_handler == SIG_DFL) {
-      struct sigaction SA;
-      sigemptyset(&SA.sa_mask);
-      SA.sa_sigaction = Handler;
-      SA.sa_flags = 0;
-      sigaction(Signal, &SA, nullptr);
-    }
+    struct sigaction SA{};
+    sigemptyset(&SA.sa_mask);
+    SA.sa_sigaction = Handler;
+    SA.sa_flags = 0;
+    sigaction(Signal, &SA, nullptr);
   }
 
   ~SignalGuard() {
-    if (SAOld.sa_handler == SIG_DFL)
-      sigaction(Signal, &SAOld, nullptr);
+    sigaction(Signal, &SAOld, nullptr);
   }
 
   SignalGuard(const SignalGuard &) = delete;
@@ -40,8 +35,7 @@ private:
   const int Signal;
   struct sigaction SAOld;
 }; // class SignalGuard
-} // namespace detail
-} // namespace IGC
+} // namespace IGC::detail
 
 #define SET_SIG_HANDLER(SIG)                                                   \
   ::IGC::detail::SignalGuard SG##SIG(SIG, signalHandler);
