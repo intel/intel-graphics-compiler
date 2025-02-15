5415e6384c94b92d59cefc52f298f15e2179ecb7
sys_igc <IGC@intel.com>
2025-02-15 09:00:49 +0100

    [Autobackout][FunctionalRegression]Revert of change: 97698ad611bf9b6c362309b87e6186e651e51f4f: Fixing ConstantCoalescing optimiztion
    Fixing ConstantCoalescing optimiztion.
        The fix removes an unreasonable limitation on base address calculations.

--- IGC/Compiler/CISACodeGen/ConstantCoalescing.cpp
+++ IGC/Compiler/CISACodeGen/ConstantCoalescing.cpp
@@ -1596,7 +1596,7 @@ bool ConstantCoalescing::DecomposePtrExp(
         int64_t Offset = 0;
         auto *Ptr = getPointerBaseWithConstantOffset(ptr_val, Offset, *dataLayout);
 
-        if (Offset < 0)
+        if (Ptr == ptr_val || Offset < 0)
             return false;
 
         buf_idxv = Ptr;
