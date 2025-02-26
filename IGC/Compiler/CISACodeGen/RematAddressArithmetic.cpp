e3b22dba8ceb872841e9cce190e4cfa6f8ed4d3e
Stefan Ilic <stefan.ilic@intel.com>
2025-02-26 16:56:03 +0000

    Make remat output deterministic
    Makes CloneAddressArithmetic pass produce deterministic output for
    easier comparisons.

--- IGC/Compiler/CISACodeGen/RematAddressArithmetic.cpp
+++ IGC/Compiler/CISACodeGen/RematAddressArithmetic.cpp
@@ -68,11 +68,10 @@ public:
         AU.addRequired<WIAnalysis>();
     }
 
-    typedef llvm::SmallVector<llvm::Instruction *, 16> RematChain;
-
-    typedef std::unordered_set<llvm::Instruction *> RematSet;
-    typedef std::pair<RematSet, RematSet> RematPair;
-    typedef llvm::SmallVector<RematPair, 16> SliceToRematTargetVector;
+    using RematChain = llvm::SmallVector<llvm::Instruction *, 16>;
+    using RematSet = llvm::SmallSetVector<llvm::Instruction *, 16>;
+    using RematPair = std::pair<RematSet, RematSet>;
+    using SliceToRematTargetVector = llvm::SmallVector<RematPair, 16>;
 
     bool runOnFunction(Function&) override;
 
@@ -322,8 +321,11 @@ void CloneAddressArithmetic::addToSystem(RematSet &Set, llvm::Instruction *I) {
         }
     }
 
-    if(!Same)
-        Vector.push_back(RematPair(Set, {I}));
+    if (!Same) {
+        llvm::SmallSetVector<llvm::Instruction *, 16> NewSet;
+        NewSet.insert(I);
+        Vector.push_back(RematPair(Set, NewSet));
+    }
 
     PRINT_LOG_NL("");
 }
@@ -462,12 +464,12 @@ void CloneAddressArithmetic::speculateWholeChain(RematSet &ToProcess, unsigned i
 
         addToSetRemat(I, Set);
         for(auto& el: Chain) {
-            Set.erase(el);
+            Set.remove(el);
             PRINT_LOG("[" << FlowMap[el] << "] "); PRINT_INST_NL(el);
             addToSetRemat(el, Set);
          }
 
-        for(auto el : Set) {
+        for(auto* el : Set) {
             PRINT_LOG("origin: "); PRINT_INST(el); PRINT_LOG_NL("");
         }
 
