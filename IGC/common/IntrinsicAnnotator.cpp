ac5fed1e37fbdb8fabcfe01a2d1c850cbceee891
krystian-andrzejewski <krystian.andrzejewski@intel.com>
2025-04-18 15:49:34 +0000

    Fix multiline comments above intrinsics
    Fix multiline comments above intrinsics

--- IGC/common/IntrinsicAnnotator.cpp
+++ IGC/common/IntrinsicAnnotator.cpp
@@ -12,6 +12,8 @@ SPDX-License-Identifier: MIT
 #include "common/LLVMWarningsPop.hpp"
 #include "GenISAIntrinsics/GenIntrinsics.h"
 
+#include <sstream>
+
 using namespace llvm;
 using namespace IGC;
 using namespace GenISAIntrinsic;
@@ -20,16 +22,30 @@ void IntrinsicAnnotator::emitFunctionAnnot(const Function* func, formatted_raw_o
 {
     if (isIntrinsic(func))
     {
+        auto AddCommentPrefixes = [](const char* in)
+            {
+                std::stringstream ss(in);
+                std::string content;
+                if (std::getline(ss, content, '\n'))
+                {
+                    std::string line;
+                    while (std::getline(ss, line, '\n')) {
+                        content += "\n; " + line;
+                    }
+                }
+                return content;
+            };
+
         auto ID = getIntrinsicID(func);
         IntrinsicComments comments = getIntrinsicComments(ID);
-        OS << "; Function Desc: " << comments.funcDescription << "\n";
+        OS << "; Function Desc: " << AddCommentPrefixes(comments.funcDescription) << "\n";
         for (auto out : comments.outputs)
         {
-            OS << "; Output: " << out << "\n";
+            OS << "; Output: " << AddCommentPrefixes(out) << "\n";
         }
         for (std::vector<int>::size_type i = 0; i != comments.inputs.size(); i++)
         {
-            OS << "; Arg " << i << ": " << comments.inputs[i] << "\n";
+            OS << "; Arg " << i << ": " << AddCommentPrefixes(comments.inputs[i]) << "\n";
         }
     }
 }
