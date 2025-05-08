be7690434fb59c9fe0b9fdc3ec523e27e6b5e7cf
Mahammad ashfaq Sasalatti <ashfaqsasalatti@gmail.com>
2025-05-08 10:09:10 +0000

    Changes in code.

--- inc/common/sku_wa.h
+++ inc/common/sku_wa.h
@@ -229,7 +229,6 @@ typedef struct _SKU_FEATURE_TABLE
         unsigned int   FtrGttCacheInvalidation          : 1;
         unsigned int   FtrMemorySeg                     : 1;
         unsigned int   FtrCacheCoherentMemSeg           : 1;
-        unsigned int   FtrDynamicDisplayAliasing        : 1;
         unsigned int   FtrLinearCCS                     : 1;
 
         unsigned int   FtrKMDTestSupportFromRegKey : 1;
