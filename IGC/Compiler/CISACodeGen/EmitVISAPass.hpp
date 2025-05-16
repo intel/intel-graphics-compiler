4af024f891c5dfb112a500a113997a181d87c2d8
Stefan Ilic <stefan.ilic@intel.com>
2025-05-16 09:10:16 +0000

    Add heuristic for dropping to SIMD16 on XE3
    Add heuristic on XE3 to force SIMD16 mode before reaching
    larger GRF sizes.

--- IGC/Compiler/CISACodeGen/EmitVISAPass.hpp
+++ IGC/Compiler/CISACodeGen/EmitVISAPass.hpp
@@ -1184,6 +1184,8 @@ private:
 
     bool shouldForceEarlyRecompile(IGCMD::MetaDataUtils* pMdUtils, llvm::Function* F);
 
+    bool shouldDropToSIMD16(IGCMD::MetaDataUtils* pMdUtils, llvm::Function* F);
+
     bool isHalfGRFReturn(CVariable* dst, SIMDMode simdMode);
 
     void emitFeedbackEnable();
