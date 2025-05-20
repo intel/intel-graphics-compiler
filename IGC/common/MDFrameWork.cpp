intel/intel-graphics-compiler

commit c5a9f769706bc7facc56852b7a47abf5a2d5e26f

Print a warning message about a currently dumped vector's size only if ShaderDumpEnable flag is enabled.
Print a warning message about a currently dumped vector's size only if ShaderDumpEnable flag is enabled.

IGC/common/MDFrameWork.cpp
@@ -186,7 +186,7 @@ MDNode* CreateNode(const std::vector<val> &vec, Module* module, StringRef name)
     for (auto it = vec.begin(); it != vec.end(); ++it)
     {
         nodes.push_back(CreateNode(*(it), module, name.str() + "Vec[" + std::to_string(i++) + "]"));
-        if (IGC_IS_FLAG_DISABLED(ShowFullVectorsInShaderDumps) && i > MAX_VECTOR_SIZE_TO_PRINT_IN_SHADER_DUMPS)
+        if (IGC_IS_FLAG_ENABLED(ShaderDumpEnable) && IGC_IS_FLAG_DISABLED(ShowFullVectorsInShaderDumps) && i > MAX_VECTOR_SIZE_TO_PRINT_IN_SHADER_DUMPS)
         {
             std::string flagName = "ShowFullVectorsInShaderDumps";
             #ifndef _WIN32

commit 3a4d4435b30007bf82dd6e1ae94a8efef1ab12fe

Fixing regression of "break" logic of MDFramework for Vec.Buff/Vec[]
Adjusting change introduced in: https://github.com/intel/intel-graphics-compiler/commit/c5a9f769706bc7facc56852b7a47abf5a2d5e26f

The problem with original change was that it not only affected warning printing, but the "break" logic also.
So if ShaderDumpEnable was disabled, then it never "broke" and in cases where Vec.Buff is huge e.g 100k entries then
compiling such kernel took hours

IGC/common/MDFrameWork.cpp
@@ -184,8 +184,10 @@ MDNode* CreateNode(const std::vector<val> &vec, Module* module, StringRef name)
     for (auto it = vec.begin(); it != vec.end(); ++it)
     {
         nodes.push_back(CreateNode(*(it), module, name.str() + "Vec[" + std::to_string(i++) + "]"));
-        if (IGC_IS_FLAG_ENABLED(ShaderDumpEnable) && IGC_IS_FLAG_DISABLED(ShowFullVectorsInShaderDumps) && i > MAX_VECTOR_SIZE_TO_PRINT_IN_SHADER_DUMPS)
+        if (IGC_IS_FLAG_DISABLED(ShowFullVectorsInShaderDumps) && i > MAX_VECTOR_SIZE_TO_PRINT_IN_SHADER_DUMPS)
         {
+          if (IGC_IS_FLAG_ENABLED(ShaderDumpEnable))
+          {
             std::string flagName = "ShowFullVectorsInShaderDumps";
             #ifndef _WIN32
                 flagName = "IGC_" + flagName;
@@ -203,7 +205,8 @@ MDNode* CreateNode(const std::vector<val> &vec, Module* module, StringRef name)
                 printWarningFirstTime = false;
             }
             nodes.push_back(CreateNode(false, module, warningMessage + " " + flagName + " currently equals"));
-            break;
+          }
+          break;
         }
     }
     MDNode* node = MDNode::get(module->getContext(), nodes);