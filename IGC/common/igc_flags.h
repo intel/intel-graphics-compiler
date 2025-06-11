7be70daae3402bd39a5529be9ffde8f55957c2ac
Cheng, Bu Qi <bu.qi.cheng@intel.com>
2025-06-11 21:38:59 +0000

    Disable LTO
    The LTO doesn't clean the %sp %fp instruction clearly. Which will cause
    VRT issue, because the %sp %fp are hardwired

--- IGC/common/igc_flags.h
+++ IGC/common/igc_flags.h
@@ -14,7 +14,7 @@ SPDX-License-Identifier: MIT
 #include "common/EmUtils.h"
 
 DECLARE_IGC_GROUP("VISA optimization")
-DECLARE_IGC_REGKEY(DWORD, VISALTO,                       0x3F, "vISA LTO optimization flags. check LINKER_TYPE for more details", false)
+DECLARE_IGC_REGKEY(DWORD, VISALTO,                       0, "vISA LTO optimization flags. check LINKER_TYPE for more details", false)
 DECLARE_IGC_REGKEY(bool, DisableSendS,                  false, "Setting this to 1/true adds a compiler switch to not generate sends commands, default is to enable sends ", false)
 DECLARE_IGC_REGKEY(bool, ForcePreserveR0,               false, "Setting this to true makes VISA preserve r0 in r0", true)
 DECLARE_IGC_REGKEY(bool, EnablePreemption,              true,  "Enable generating preeemptable code (SKL+)", false)
