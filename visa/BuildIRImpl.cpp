012585e8d154f137376d581dc61e74469a32e27a
Borzyszkowski, Mateusz <mateusz.borzyszkowski@intel.com>
2025-07-02 18:49:12 +0000

    Set insertion point for global Imm after SSO instruction
    The change ensures that the insertion point for the global Imm
    is set after the SSO (ScratchSurfaceOffset) instruction.
    Without this change, we could generate code where the `r126.7`
    register is used before it is initialized.
    
    (cherry picked from commit 8ab686cd1519012f8aca93518f8776c397c02759)

--- visa/BuildIRImpl.cpp
+++ visa/BuildIRImpl.cpp
@@ -944,6 +944,7 @@ void IR_Builder::initScratchSurfaceOffset() {
           std::find_if(instList.begin(), instList.end(),
                         [](G4_INST *inst) { return !inst->isLabel(); });
       instList.insert(iter, andInst);
+      setSSOInst(andInst);
 
       // scratchSurfaceOffset (r0.5+0x400)>>4 is reserved for spillfill,
       // pvtmem should use r0.5>>4 shift-right by 4 is done before send-msg
@@ -963,6 +964,7 @@ void IR_Builder::initScratchSurfaceOffset() {
           std::find_if(instList.begin(), instList.end(),
                         [](G4_INST *inst) { return !inst->isLabel(); });
       instList.insert(iter, andInst);
+      setSSOInst(andInst);
     }
   }
 
