5eee6f46866bd14e4f95f38c4840c637f02b77db
Cheng, Bu Qi <bu.qi.cheng@intel.com>
2025-07-09 22:36:43 +0000

    Reduce the SWSB compilation time when there is subroutine
    For subroutine, there is no need add live out dependence of call BB

--- visa/LocalScheduler/SWSB_G4IR.h
+++ visa/LocalScheduler/SWSB_G4IR.h
@@ -1122,7 +1122,6 @@ class SWSB {
   void SWSBDepDistanceGenerator(PointsToAnalysis &p, LiveGRFBuckets &LB,
                                 LiveGRFBuckets &globalSendsLB,
                                 LiveGRFBuckets &GRFAlignedGlobalSendsLB);
-  void handleSubRoutineCall();
   void SWSBGlobalTokenGenerator(PointsToAnalysis &p, LiveGRFBuckets &LB,
                                 LiveGRFBuckets &globalSendsLB,
                                 LiveGRFBuckets &GRFAlignedGlobalSendsLB);
