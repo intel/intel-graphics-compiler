a0f4cb0f63d54d96231c311ab975eac962aa20a6
sys_igc <IGC@intel.com>
2025-07-10 21:35:11 +0200

    [Autobackout][FunctionalRegression]Revert of change: 089d12eb4b51f591df722fbc4e6b5f837b78e8fd: Fix operands alignment issues for SIMD2 instructions with 64b or float datatype
    Fix operands alignment issues for SIMD2 instructions with 64b or float datatype

--- visa/HWConformity.cpp
+++ visa/HWConformity.cpp
@@ -8358,12 +8358,8 @@ void HWConformity::fixUnalignedRegions(INST_LIST_ITER it, G4_BB *bb) {
     // split currently can't handle packed imm
     // Also don't split src byte type since scalar byte to float conversion is
     // not allowed
-    auto canSplit = [](G4_INST *inst, G4_BB *bb) {
-      // Can not split any instruction except for NoMask ones because there is
-      // no legal emask for the split instructions.
-      bool isNoMaskInst = !inst->getPredicate() &&
-                          (inst->isWriteEnableInst() || bb->isAllLaneActive());
-      if (inst->getPredicate() || inst->getCondMod() || !isNoMaskInst) {
+    auto canSplit = [](G4_INST *inst) {
+      if (inst->getPredicate() || inst->getCondMod()) {
         return false;
       }
       for (int i = 0, numSrc = inst->getNumSrc(); i < numSrc; ++i) {
@@ -8374,7 +8370,7 @@ void HWConformity::fixUnalignedRegions(INST_LIST_ITER it, G4_BB *bb) {
       }
       return true;
     };
-    if (canSplit(inst, bb)) {
+    if (canSplit(inst)) {
       auto prevIt = it == bb->begin() ? it : std::prev(it);
       if (evenlySplitInst(it, bb)) {
         // split introduces new moves which may need fixing
@@ -8386,8 +8382,8 @@ void HWConformity::fixUnalignedRegions(INST_LIST_ITER it, G4_BB *bb) {
           fixUnalignedRegions(insertIt, bb);
           insertIt++;
         }
-        return;
       }
+      return;
     }
   }
 
