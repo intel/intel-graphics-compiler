af86a2b64c71293dcead86935bd5ce8f9d957604
sys_igc <IGC@intel.com>
2025-07-11 10:07:28 +0200

    [Autobackout][FunctionalRegression]Revert of change: 16e7042597ebd4d0c0b8f7ee8299b6d011098b7c: Reduce the SWSB compilation time when there is subroutine
    For subroutine, there is no need add live out dependence of call BB
    
    (cherry picked from commit 599485a6104a1e83cc4ed1481dcb17ea11a7c8c2)

--- visa/LocalScheduler/SWSB_G4IR.h
+++ visa/LocalScheduler/SWSB_G4IR.h
@@ -1121,6 +1121,7 @@ class SWSB {
   void SWSBDepDistanceGenerator(PointsToAnalysis &p, LiveGRFBuckets &LB,
                                 LiveGRFBuckets &globalSendsLB,
                                 LiveGRFBuckets &GRFAlignedGlobalSendsLB);
+  void handleSubRoutineCall();
   void SWSBGlobalTokenGenerator(PointsToAnalysis &p, LiveGRFBuckets &LB,
                                 LiveGRFBuckets &globalSendsLB,
                                 LiveGRFBuckets &GRFAlignedGlobalSendsLB);
