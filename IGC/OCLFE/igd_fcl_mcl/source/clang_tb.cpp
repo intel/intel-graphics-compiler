554cabda6681cf01d6101312838ea3ac65922ef4
sys_igc <IGC@intel.com>
2025-07-26 05:33:19 +0200

    [Autobackout][FunctionalRegression]Revert of change: b224b29b4d3011e78c6f374e14a754ff0dca759b: Remove workaround -Wno-error=implicit-int
    Previously, some workloads used implicit int types in function
        definitions. With LLVM 15, implicit ints are treated as errors.
        The workaround to disable this error has now been removed, so IGC
        enforces the same behavior as LLVM 15 and treats implicit ints
        as errors.

--- IGC/OCLFE/igd_fcl_mcl/source/clang_tb.cpp
+++ IGC/OCLFE/igd_fcl_mcl/source/clang_tb.cpp
@@ -1353,6 +1353,9 @@ bool CClangTranslationBlock::TranslateClang(const TranslateClangArgs *pInputArgs
     optionsEx += " -U__IMAGE_SUPPORT__";
   }
 
+  // TODO: Workaround - remove after some time to be consistent with LLVM15+ behavior
+  optionsEx += " -Wno-error=implicit-int";
+
   IOCLFEBinaryResult *pResultPtr = NULL;
   int res = 0;
   {
