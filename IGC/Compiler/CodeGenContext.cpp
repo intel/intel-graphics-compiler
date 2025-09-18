a1b485d887740613b2a964979bf25a7335ef0697
bokrzesi <bartlomiej.okrzesik@intel.com>
2025-09-18 10:56:45 +0000

    [LLVM16] Removal of Reapply_hasSetOpaquePointersValue.patch
    In order to reduce the amount of patches this PR moves
    check whether pointers mode was already set from LLVM to IGC
    
    More details in comments
    
    (cherry picked from commit ff5722679155953ec47adc672c49dbded52d24b0)

--- IGC/Compiler/CodeGenContext.cpp
+++ IGC/Compiler/CodeGenContext.cpp
@@ -312,11 +312,22 @@ LLVMContextWrapper::LLVMContextWrapper(bool createResourceDimTypes) : m_UserAddr
   // perform automatic conversion of builtin types which should be represented using TargetExtTy.
   // TODO: For transition purposes, consider introducing an IGC internal option to tweak typed/opaque pointers
   // with a precedence over the environment flag.
-  if (IGC::canOverwriteLLVMCtxPtrMode(basePtr)) {
-    bool enableOpaquePointers = AreOpaquePointersEnabled();
 
-    IGCLLVM::setOpaquePointers(basePtr, enableOpaquePointers);
+  // TODO: Remove/Re-evaluate once fully moved to the LLVM 16 opaque ptrs.
+  // This WA_OpaquePointersCL flag is related to the same flag in LLVM itself
+  // We're using it here to have consistent behaviour.
+  // https://github.com/llvm/llvm-project/blob/release/16.x/llvm/lib/IR/LLVMContextImpl.cpp#L50
+  auto WA_OpaquePointersCL = cl::getRegisteredOptions()["opaque-pointers"];
+  if (WA_OpaquePointersCL && WA_OpaquePointersCL->getNumOccurrences() > 0) {
+    IGC_IsPointerModeAlreadySet = true;
   }
+  
+  if (IGC::canOverwriteLLVMCtxPtrMode(basePtr, IGC_IsPointerModeAlreadySet)) {
+    bool enableOpaquePointers = AreOpaquePointersEnabled();
+    IGCLLVM::setOpaquePointers(basePtr, enableOpaquePointers);
+    IGC_IsPointerModeAlreadySet = true;
+  }
+  // TODO: end
 }
 
 void LLVMContextWrapper::AddRef() { refCount++; }
