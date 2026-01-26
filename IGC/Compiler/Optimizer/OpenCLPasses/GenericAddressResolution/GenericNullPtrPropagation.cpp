ee60f4673c2599ea0fccc4bfe185938173b5de69
Stefan Ilic <stefan.ilic@intel.com>
2026-01-26 16:41:43 +0000

    Relax requirement for generic nullptr opt
    Also consider addrspacecasts with multiple users for optimization.

--- IGC/Compiler/Optimizer/OpenCLPasses/GenericAddressResolution/GenericNullPtrPropagation.cpp
+++ IGC/Compiler/Optimizer/OpenCLPasses/GenericAddressResolution/GenericNullPtrPropagation.cpp
@@ -31,16 +31,91 @@ public:
   void getAnalysisUsage(AnalysisUsage &AU) const override {
     AU.addRequired<CodeGenContextWrapper>();
     AU.addRequired<llvm::DominatorTreeWrapperPass>();
+    AU.setPreservesCFG();
   }
   bool runOnFunction(Function &F) override;
   void visitAddrSpaceCastInst(AddrSpaceCastInst &I);
 
+  void releaseMemory() override { toVisit.clear(); }
+
 private:
-  bool m_modified = false;
   DominatorTree *dt = nullptr;
   CodeGenContext *m_ctx = nullptr;
+  SmallPtrSet<AddrSpaceCastInst *, 8> toVisit;
 };
 } // namespace
+
+#if LLVM_VERSION_MAJOR < 16
+static Instruction *findNearestCommonInstDominator(Instruction *I1, Instruction *I2, DominatorTree *DT) {
+  BasicBlock *BB1 = I1->getParent();
+  BasicBlock *BB2 = I2->getParent();
+  if (BB1 == BB2)
+    return I1->comesBefore(I2) ? I1 : I2;
+  if (!DT->isReachableFromEntry(BB2))
+    return I1;
+  if (!DT->isReachableFromEntry(BB1))
+    return I2;
+  BasicBlock *DomBB = DT->findNearestCommonDominator(BB1, BB2);
+  if (BB1 == DomBB)
+    return I1;
+  if (BB2 == DomBB)
+    return I2;
+  return DomBB->getTerminator();
+}
+#endif
+
+// Update the location of the addrspacecast to be closer to its uses if we can.
+// This enables PrivateMemoryResolution pass to sink allocas better and reduce RP.
+static void updateAddrSpaceCastLocation(AddrSpaceCastInst &I, DominatorTree *DT) {
+  if (I.use_empty()) {
+    return;
+  }
+
+  SmallVector<Instruction *, 4> UInsts;
+
+  for (auto *U : I.users()) {
+    auto *UI = dyn_cast<Instruction>(U);
+    if (!UI || isa<PHINode>(UI)) {
+      return;
+    }
+    UInsts.push_back(UI);
+  }
+
+  auto *DomInst = UInsts[0];
+  for (unsigned i = 1; i < UInsts.size(); ++i) {
+#if LLVM_VERSION_MAJOR >= 16
+    DomInst = DT->findNearestCommonDominator(DomInst, UInsts[i]);
+#else
+    DomInst = findNearestCommonInstDominator(DomInst, UInsts[i], DT);
+#endif
+    if (!DomInst) {
+      return;
+    }
+  }
+
+  auto *DomBB = DomInst->getParent();
+  auto FirstInsertPt = DomBB->getFirstInsertionPt();
+  if (FirstInsertPt == DomBB->end() || DomInst->comesBefore(&*FirstInsertPt)) {
+    return;
+  }
+  I.moveBefore(&*DomInst);
+}
+
+static void addChecks(AddrSpaceCastInst &I, DominatorTree *DT) {
+  updateAddrSpaceCastLocation(I, DT);
+
+  Value *src = I.getPointerOperand();
+  Value *srcNull = ConstantPointerNull::get(cast<PointerType>(src->getType()));
+  Value *dstNull = ConstantPointerNull::get(cast<PointerType>(I.getType()));
+
+  IGCLLVM::IRBuilder<> builder(&I);
+  auto *AddrSpaceCastCpy = builder.CreateAddrSpaceCast(src, I.getType(), I.getName());
+  auto *isNotNull = builder.CreateICmpNE(src, srcNull);
+  auto *select = builder.CreateSelect(isNotNull, AddrSpaceCastCpy, dstNull);
+
+  I.replaceAllUsesWith(select);
+  I.eraseFromParent();
+}
 
 #define PASS_FLAG "igc-generic-null-ptr-propagation"
 #define PASS_DESCRIPTION "Propagates null pointers through addrespace casts."
@@ -48,6 +123,7 @@ private:
 #define PASS_ANALYSIS false
 IGC_INITIALIZE_PASS_BEGIN(GenericNullPtrPropagation, PASS_FLAG, PASS_DESCRIPTION, PASS_CFG_ONLY, PASS_ANALYSIS)
 IGC_INITIALIZE_PASS_DEPENDENCY(CodeGenContextWrapper)
+IGC_INITIALIZE_PASS_DEPENDENCY(DominatorTreeWrapperPass)
 IGC_INITIALIZE_PASS_END(GenericNullPtrPropagation, PASS_FLAG, PASS_DESCRIPTION, PASS_CFG_ONLY, PASS_ANALYSIS)
 
 char GenericNullPtrPropagation::ID = 0;
@@ -56,34 +132,8 @@ bool GenericNullPtrPropagation::runOnFunction(Function &F) {
   m_ctx = getAnalysis<CodeGenContextWrapper>().getCodeGenContext();
   dt = &getAnalysis<llvm::DominatorTreeWrapperPass>().getDomTree();
   visit(F);
-  return m_modified;
-}
-
-static void updateAddrSpaceCastLocation(AddrSpaceCastInst &I, DominatorTree *dt) {
-  if (!I.hasOneUser()) {
-    return;
-  }
-  // If we can move addrspace cast closer to use we do it, to enable PrivateMemoryResolution to
-  // optimize alloca location better.
-  auto *user = llvm::dyn_cast<llvm::Instruction>(*I.user_begin());
-  if (!user) {
-    return;
-  }
-
-  if (llvm::isa<llvm::PHINode>(user)) {
-    return;
-  }
-
-  auto *addrSpaceCastParent = I.getParent();
-  auto *userParent = user->getParent();
-  if (addrSpaceCastParent != userParent && dt->dominates(addrSpaceCastParent, userParent)) {
-    auto firstInsertPt = userParent->getFirstInsertionPt();
-
-    if (firstInsertPt == userParent->end() || user->comesBefore(&*firstInsertPt)) {
-      return;
-    }
-    I.moveBefore(user);
-  }
+  llvm::for_each(toVisit, [this](AddrSpaceCastInst *I) { addChecks(*I, dt); });
+  return !toVisit.empty();
 }
 
 void GenericNullPtrPropagation::visitAddrSpaceCastInst(AddrSpaceCastInst &I) {
@@ -91,20 +141,8 @@ void GenericNullPtrPropagation::visitAddrSpaceCastInst(AddrSpaceCastInst &I) {
        (I.getSrcAddressSpace() == ADDRESS_SPACE_PRIVATE && m_ctx->mustDistinguishBetweenPrivateAndGlobalPtr())) &&
       I.getDestAddressSpace() == ADDRESS_SPACE_GENERIC) {
 
-    updateAddrSpaceCastLocation(I, dt);
-
-    Value *src = I.getPointerOperand();
-    Value *srcNull = ConstantPointerNull::get(cast<PointerType>(src->getType()));
-    Value *dstNull = ConstantPointerNull::get(cast<PointerType>(I.getType()));
-
-    IGCLLVM::IRBuilder<> builder(&I);
-    auto *AddrSpaceCastCpy = builder.CreateAddrSpaceCast(src, I.getType(), I.getName());
-    auto *isNotNull = builder.CreateICmpNE(src, srcNull);
-    auto *select = builder.CreateSelect(isNotNull, AddrSpaceCastCpy, dstNull);
-
-    I.replaceAllUsesWith(select);
-    I.eraseFromParent();
-    m_modified = true;
+    toVisit.insert(&I);
+    return;
   }
 }
 
--- IGC/Compiler/tests/GenericAddressDynamicResolution/generic-null-ptr-propagation4.ll
+++ IGC/Compiler/tests/GenericAddressDynamicResolution/generic-null-ptr-propagation4.ll
@@ -1,12 +1,12 @@
 ;=========================== begin_copyright_notice ============================
 ;
-; Copyright (C) 2025 Intel Corporation
+; Copyright (C) 2026 Intel Corporation
 ;
 ; SPDX-License-Identifier: MIT
 ;
 ;============================ end_copyright_notice =============================
 
-; Check that null check is placed closer to use of alloca.
+; Check that null check is placed closer to use of alloca even if we have multiple users.
 ; RUN: igc_opt --typed-pointers %s -S -igc-generic-null-ptr-propagation | FileCheck %s
 
 ; CHECK-LABEL: do_work:
@@ -16,14 +16,21 @@
 
 %"class.sycl::_V1::nd_item" = type { i8 }
 
-define spir_func void @example_local_to_generic_triggers_pass(i1 %cond) {
+define spir_func void @example_local_to_generic_triggers_pass(i1 %cond1, i1 %cond2) {
 entry:
   %local.ptr = addrspacecast i8* null to i8 addrspace(3)*
   %g = addrspacecast i8 addrspace(3)* %local.ptr to i8 addrspace(4)*
-  br i1 %cond, label %do_work, label %exit
+  br i1 %cond1, label %do_work, label %exit
 
 do_work:
-  %isnull = icmp eq i8 addrspace(4)* %g, null
+  br i1 %cond2, label %case1, label %case2
+
+case1:
+  %isnull1 = icmp eq i8 addrspace(4)* %g, null
+  br label %exit
+
+case2:
+  %isnull2 = icmp eq i8 addrspace(4)* %g, null
   br label %exit
 
 exit:
