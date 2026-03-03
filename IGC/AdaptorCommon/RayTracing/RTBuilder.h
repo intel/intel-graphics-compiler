c81db347e77df1abaa23397bd4f88b6791f8ed42
sys_igc <IGC@intel.com>
2026-03-03 09:44:13 +0100

    [Autobackout][FunctionalRegression]Revert of change: de2035732b7b6763d5fc5a160fafd420f22ceacb: Add getEntryFirstInsertionPointInBlock
    Add a version of getEntryFirstInsertionPt which operates on
        a specified BasicBlock, not the entire function.

--- IGC/AdaptorCommon/RayTracing/RTBuilder.h
+++ IGC/AdaptorCommon/RayTracing/RTBuilder.h
@@ -350,9 +350,6 @@ public:
   static Instruction *getEntryFirstInsertionPt(Function &F,
                                                const std::vector<Value *> *pAdditionalInstructionsToSkip = nullptr);
 
-  static Instruction *getEntryFirstInsertionPointInBlock(BasicBlock &block,
-                                                         const std::vector<Value *> *additionalInstructionsToSkip);
-
   Value *getTraceRayPayload(Value *bvhLevel, Value *traceRayCtrl, bool isRayQuery, const Twine &PayloadName = "");
 
 
