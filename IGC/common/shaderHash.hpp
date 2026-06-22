b0468658eb50cb54aa89d256d8b1047b61526129
Wang, Elvin <elvin.wang@intel.com>
2026-06-22 19:04:33 +0000

    Seperate ShaderHash class
    Separate this monolithic class to make the uses more flexible.

--- IGC/common/shaderHash.hpp
+++ IGC/common/shaderHash.hpp
@@ -8,25 +8,39 @@ SPDX-License-Identifier: MIT
 
 #pragma once
 
-class ShaderHash {
+class ShaderHashBase {
 public:
-  ShaderHash()
-      : asmHash(0), nosHash(0), psoHash(0), perShaderPsoHash(0), rtlHash(0), dcHash(0), ltoHash(0), stateHash(0) {}
+  ShaderHashBase() : asmHash(0), perShaderPsoHash(0), stateHash(0), rtlHash(0) {}
   QWORD getAsmHash() const { return asmHash; }
+  QWORD getPerShaderPsoHash() const { return perShaderPsoHash; }
+
+  QWORD asmHash;
+  QWORD perShaderPsoHash;
+  QWORD stateHash;
+  QWORD rtlHash;
+};
+
+class ShaderHash3D : public ShaderHashBase {
+public:
+  ShaderHash3D() : ShaderHashBase(), dcHash(0), ltoHash(0) {}
+  ShaderHash3D(const ShaderHashBase &base) : ShaderHashBase(base), dcHash(0), ltoHash(0) {}
+
+  QWORD dcHash;
+  QWORD ltoHash;
+};
+
+class ShaderHash : public ShaderHash3D {
+public:
+  ShaderHash() : ShaderHash3D(), nosHash(0), psoHash(0) {}
+  ShaderHash(const ShaderHash3D &spec) : ShaderHash3D(spec), nosHash(0), psoHash(0) {}
+  ShaderHash(const ShaderHashBase &base) : ShaderHash3D(base), nosHash(0), psoHash(0) {}
   QWORD getNosHash() const { return nosHash; }
   QWORD getPsoHash() const { return psoHash; }
-  QWORD getPerShaderPsoHash() const { return perShaderPsoHash; }
 
   bool is_set() const {
     return ((asmHash | nosHash | psoHash | perShaderPsoHash | rtlHash | dcHash | ltoHash | stateHash) != 0);
   }
 
-  QWORD asmHash;
   QWORD nosHash;
   QWORD psoHash;
-  QWORD perShaderPsoHash;
-  QWORD rtlHash;
-  QWORD dcHash;
-  QWORD ltoHash;
-  QWORD stateHash;
 };
