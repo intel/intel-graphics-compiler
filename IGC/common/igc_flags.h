579d992dc652143a40e29657da6f093addb44a16
sys-igc <IGC@intel.com>
2026-07-29 11:36:23 +0200

    Revert of 'Enable phi and select lowering for SOA promotion'
    Revert of 'Enables features by default'

--- IGC/common/igc_flags.h
+++ IGC/common/igc_flags.h
@@ -1878,15 +1878,15 @@ DECLARE_IGC_REGKEY(DWORD, EnablePrivMemNewSOATranspose, 1,
                    "2 : 1 plus new algo for array of dw[xn]/qw[xn],etc "
                    "3 : 2 plus new algo for array of complicated struct.",
                    true)
-DECLARE_IGC_REGKEY(bool, EnableSOAFallbackToOldAlgorithm, true,
+DECLARE_IGC_REGKEY(bool, EnableSOAFallbackToOldAlgorithm, false,
                    "Enable fallback to old SOA algorithm when new algorithm is not applicable", true)
-DECLARE_IGC_REGKEY(bool, EnableLowerGEPPtrHoisting, true, "Enable hoisting loads past pointer-typed phi instructions",
+DECLARE_IGC_REGKEY(bool, EnableLowerGEPPtrHoisting, false, "Enable hoisting loads past pointer-typed phi instructions",
                    true)
-DECLARE_IGC_REGKEY(bool, EnablePrivMemNewSOAForScalarArrays, true,
+DECLARE_IGC_REGKEY(bool, EnablePrivMemNewSOAForScalarArrays, false,
                    "Enables new SOA algorithm also for scalar float/int arrays.", true)
 DECLARE_IGC_REGKEY(bool, NewSOATransposeForOpenCL, true,
                    "If true, EnablePrivMemNewSOATranspose only applies to OpenCL kernels. For testing purpose", true)
-DECLARE_IGC_REGKEY(bool, EnableSelectOfAllocaPtrSplit, true,
+DECLARE_IGC_REGKEY(bool, EnableSelectOfAllocaPtrSplit, false,
                    "If true, pre-pass in PrivateMemoryResolution splits select-of-pointer "
                    "where one operand is alloca-derived (load duplication / store branching). "
                    "Enables SoA promotion for allocas otherwise blocked by SELECT pattern.",
