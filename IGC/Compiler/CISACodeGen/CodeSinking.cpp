d3311a7d4e005274324638027e39532a1bafdec2
sys-igc <IGC@intel.com>
2026-09-11 04:21:37 +0200

    [Autobackout][FunctionalRegression]Revert of change: 8491a92650885f07de865e5e1dd1d23f72997aa9: CodeSinking publishing proper RPE values
    Return back to proper functionality, after partial revert.

--- IGC/Compiler/CISACodeGen/CodeSinking.cpp
+++ IGC/Compiler/CISACodeGen/CodeSinking.cpp
@@ -951,9 +951,11 @@ bool CodeLoopSinking::loopSink(Function &F) {
       Changed |= loopSink(L, SinkMode);
   }
 
+  uint SIMD = numLanes(IGC::bestGuessSIMDSize(CTX, &F, FGA));
   PressurePair ExternalPair = FRPE->getExternalPressurePairForFunction(&F);
-  unsigned SIMD = numLanes(IGC::bestGuessSIMDSize(CTX, &F, FGA));
-  PressurePair Pair = RPE->getMaxPressurePairForFunction(F, SIMD, WI);
+  unsigned int MaxPressure = getMaxRegCountForFunction(&F);
+  unsigned RegSize = CTX->platform.getGRFSize();
+  PressurePair Pair = {MaxPressure * RegSize, 0};
   RPE->publishNormalizedPressurePair(F, Pair + ExternalPair, SIMD);
 
   return Changed;
