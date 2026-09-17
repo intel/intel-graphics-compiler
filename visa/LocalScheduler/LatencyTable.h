215a8ab133b3f08f8d5f8168592121c000f2c3b4
Cheng, Bu Qi <bu.qi.cheng@intel.com>
2026-09-17 20:19:41 +0000

    Update the cycle of AddrMov for indirect send payload
    Since the AddrMov instruction will be replaced with mov instruction.
    It's latency cycle in instruction scheduling shouldn't be 2, instead it
    should be same as instruction mov s0.0

--- visa/LocalScheduler/LatencyTable.h
+++ visa/LocalScheduler/LatencyTable.h
@@ -86,7 +86,7 @@ enum class XELatencyInfo : uint16_t {
   LSC_TYPED_L1 = 75,      // LSC typed L1 cache hit
   LSC_TYPED_L3 = 200,     // LSC typed L3 cache hit
   LSC_TYPED_FENCE = 60,   // LSC typed fence
-  ADDR_MOV = 2,
+  ADDR_MOV = 10,
   SEND_ARB = 8,          //The cycles for arbitration acquire and release of send
   UNKNOWN = std::numeric_limits<uint16_t>::max(),
 
